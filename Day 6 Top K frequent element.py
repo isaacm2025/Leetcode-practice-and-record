dbb40d8b1ce1a27204051c6c1f6a638f259b56f1
Isaac <zihao599@gmail.com>
2026-01-16 12:00:00 -0800

    Add Day 6 Top K frequent element

--- Day 6 Top K frequent element.py
+++ Day 6 Top K frequent element.py
@@ -40,3 +40,48 @@ class Solution:
         while len(res) < k:
             res.append(arr.pop()[1])
         return res
+#time complexity: O(N log N)
+#space complexity: O(N)
+
+#min-heap
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
+#time complexity: O(N log K)
+#space complexity: O(N + K)
+
+
+#bucket sort
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [ [] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) -1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+#time complexity: O(N)
+#space complexity: O(N)
+
