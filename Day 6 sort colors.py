8bd03d8162c436c8e6736469960865cb76cd9f77
Isaac <zihao599@gmail.com>
2026-02-04 14:30:34 -0800

    Day 6 sort colors - three pointers approach

--- Day 6 sort colors.py
+++ Day 6 sort colors.py
@@ -49,6 +49,68 @@ class Solution:
 #time complexity: O(n)
 #space complexity: O(1)
 
+#three pointers 1
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        l, r = 0, len(nums) - 1
+        i = 0
+
+        def swap(i, j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+
+        while i <= r:
+            if nums[i] == 0:
+                swap(l, i)
+                l += 1
+            elif nums[i] == 2:
+                swap(i, r)
+                r -= 1
+                i -= 1
+            i += 1
+#time complexity: O(n)
+# space complexity: O(1)
+
+#three pointers 2
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+
+        zero = one = two = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                nums[two] = 2
+                nums[one] = 1
+                nums[zero] = 0
+                zero += 1
+                one += 1
+                two += 1
+            elif nums[i] == 1:
+                nums[two] = 2
+                nums[one] = 1
+                one += 1
+                two += 1
+            else:
+                nums[two] = 2
+                two += 1
+#time complexity: O(n)
+#space complexity: O(1)
+
+#three pointers 3
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        zero = one = 0
+        for two in range(len(nums)):
+            temp = nums[two]
+            nums[two] = 2
+            if temp < 2:
+                nums[one] = 1
+                one += 1
+            if temp < 1:
+                nums[zero] = 0
+                zero += 1
+#time complexity: O(n)
+#space complexity: O(1)
 
 
 
