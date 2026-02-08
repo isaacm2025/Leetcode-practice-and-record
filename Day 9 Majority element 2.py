6ac72adf300a62ba75c401b7ce7139409bfa538d
Isaac <zihao599@gmail.com>
2026-02-07 21:29:54 -0800

    Day 9 majority element 2 with hash map approach

--- Day 9 Majority element 2.py
+++ Day 9 Majority element 2.py
@@ -52,3 +52,28 @@ class Solution:
 #time complexity: O(n log n)
 #space complexity: O(1) or O(n) depend on the sorting algorithm
 
+#boyer-moore voting algorithm (hash map)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count = defaultdict(int)
+
+        for num in nums:
+            count[num] += 1
+
+            if len(count) <= 2:
+                continue
+            new_count = defaultdict(int)
+            for num, c in count.item():
+                if c > 1:
+                    new_count[num] = c - 1
+            count = new_count
+
+        res = []
+        for num in count:
+            if nums.count(num) > len(nums) // 3:
+                res.append(num)
+        return res
+#time comlexity: O(n)
+#space complexity: O(1) since output array size will be at most 2
+
+
