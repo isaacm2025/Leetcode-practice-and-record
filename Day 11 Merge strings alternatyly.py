ce09e6b75870f88f37f1282ea3b8a602ab1eaf50
Isaac <zihao599@gmail.com>
2026-02-09 23:23:32 -0800

    Day 11 Merge strings alternatyly with one pointer

--- Day 11 Merge strings alternatyly.py
+++ Day 11 Merge strings alternatyly.py
@@ -35,3 +35,17 @@ class Solution:
 #time complexity: O(n+m)
 #space complexity: O(n+m)
 
+#one pointer
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        n, m = len(word1), len(word2)
+        res = []
+        for i in range(max(m,n)):
+            if i < n:
+                res.append(word1[i])
+            if i < m:
+                res.append(word2[i])
+        return "".join(res)
+#time complexity: O(n+m)
+#space complexity: O(n+m) for the output string
+#  
