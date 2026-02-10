e73d51f954e71215d74f6d131a67101b54c2d126
Isaac <zihao599@gmail.com>
2026-02-09 23:20:54 -0800

    Day 11 merge strings alternatly - two pointers

---
+++ Day 11 Merge strings alternatyly.py
@@ -0,0 +1,37 @@
+'''You are given two strings, word1 and word2. Construct a new string by merging them in alternating order, starting with word1 — take one character from word1, then one from word2, and repeat this process.
+
+If one string is longer than the other, append the remaining characters from the longer string to the end of the merged result.
+
+Return the final merged string.
+
+Example 1:
+
+Input: word1 = "abc", word2 = "xyz"
+
+Output: "axbycz"
+Example 2:
+
+Input: word1 = "ab", word2 = "abbxxc"
+
+Output: "aabbbxxc"
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.'''
+
+#two pointers
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
+        res = []
+        while i < len(word1) and j < len(word2):
+            res.append(word1[i])
+            res.append(word2[j])
+            i += 1
+            j += 1
+        res.append(word1[i:])
+        res.append(word2[j:])
+        return "".join(res)
+#time complexity: O(n+m)
+#space complexity: O(n+m)
+
