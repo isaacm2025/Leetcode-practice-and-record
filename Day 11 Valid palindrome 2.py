892700fa88038d98760e156824dcc7d7668c26c5
Isaac <zihao599@gmail.com>
2026-02-09 23:10:52 -0800

    Day 11 Valid palindrome 2 - brute force

---
+++ Day 11 Valid palindrome 2.py
@@ -0,0 +1,44 @@
+'''You are given a string s, return true if the s can be a palindrome after deleting at most one character from it.
+
+A palindrome is a string that reads the same forward and backward.
+
+Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+
+Example 1:
+
+Input: s = "aca"
+
+Output: true
+Explanation: "aca" is already a palindrome.
+
+Example 2:
+
+Input: s = "abbadc"
+
+Output: false
+Explanation: "abbadc" is not a palindrome and can't be made a palindrome after deleting at most one character.
+
+Example 3:
+
+Input: s = "abbda"
+
+Output: true
+Explanation: "We can delete the character 'd'.
+
+Constraints:
+
+1 <= s.length <= 100,000
+s is made up of only lowercase English letters.
+'''
+#brute force approach
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if s == s[::-1]:
+            return True
+        
+        for i in range(len(s)):
+            newS = s[:i] + s[i + 1:]]
+            if newS == newS[::-1]:
+                return True
+        return False
+    
