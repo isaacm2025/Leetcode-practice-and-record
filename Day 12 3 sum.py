dd265f4f53fa81e067affe929449f73603ab8ce6
Isaac <zihao599@gmail.com>
2026-02-10 21:43:44 -0800

    Day 12 3 sum with hashmap

--- Day 12 3 sum.py
+++ Day 12 3 sum.py
@@ -47,3 +47,31 @@ class Solution:
 #time c: O(n^3)
 #space c: O(m)
 
+#hashmap
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+        res = []
+        for i in range(len(nums)):
+            count[nums[i]] -= 1
+            if i and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, len(nums)):
+                count[nums[j]] -= 1
+                if j - 1 > i and nums[j] == nums[j - 1]:
+                    continue
+                target = -(nums[i] + nums[j])
+                if count[target] > 0:
+                    res.append([nums[i], nums[j], target])
+
+            for j in range(i + 1, len(nums)):
+                continue[nums[j]] += 1
+        return res
+#time c: O(n^2)
+#space c: O(n)
+
+
