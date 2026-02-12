b830b4852ff9850b04311a10aba99d9532425a76
Isaac <zihao599@gmail.com>
2026-02-11 23:03:53 -0800

    Day 13 rotate array with one liner and reverse

--- Day 13 rotate array.py
+++ Day 13 rotate array.py
@@ -47,3 +47,35 @@ class Solution:
 #space O(1)
 
 
+#one liner:
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        nums[:] = nums[-k % len(nums):] + nums[:-k % len(nums)]
+#time c: O(n)
+#space c: O(1)
+
+#using reverse:
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        n = len(nums)
+        k %= n
+
+        def reverse(l:int, r:int) -> None:
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l, r = l + 1, r - 1
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
+
+#time com: O(n)
+#space c: O(1)
+
