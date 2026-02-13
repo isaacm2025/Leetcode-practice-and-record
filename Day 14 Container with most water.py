8fb9b3c4124c0fe9ab8edb3818055c66f0c3459e
Isaac <zihao599@gmail.com>
2026-02-12 23:32:20 -0800

    Day 14 container with most water - two pinters

---
+++ Day 14 Container with most water.py
@@ -0,0 +1,48 @@
+'''You are given an integer array heights where heights[i] represents the height of the ith bar.
+
+You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+
+Input: height = [1,7,2,5,4,7,3,6]
+
+Output: 36
+Example 2:
+
+Input: height = [2,2,2]
+
+Output: 4
+Constraints:
+
+2 <= height.length <= 1000
+0 <= height[i] <= 1000
+
+'''
+from typing import List
+
+#brute force:
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        res = 0
+        for i in range(len(heights)):
+            for j in range(i + 1, len(heights)):
+                res = max(res, min(heights[i], heights[j]) * (j - i))
+        return res
+    
+#time complexity: O(n^2)
+#space com: O(1)
+
+#two pointers:
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l, r = 0, len(heights) - 1
+        res = 0
+
+        while l < r:
+            area = min(heights[], heights[r]) * (r - l)
+            res = max(res, area)
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+#time com: O(n)
+#space com: O(1)
