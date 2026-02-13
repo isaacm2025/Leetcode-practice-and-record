4c2b5a9aaab9666fbb9d79fe0930222f5f3e2f4f
Isaac <zihao599@gmail.com>
2026-02-12 23:58:10 -0800

    Day 14 Boats to save people - counting sort

--- Day 14 Boats to save people.py
+++ Day 14 Boats to save people.py
@@ -41,3 +41,29 @@ class Solution:
         return res
 #time complex: O(nlogn)
 #space: O(1) or O(n)
+
+#counting sort:
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        m = max[people]
+        count = [0] * (m + 1)
+        for p in people:
+            count[p] += 1
+        idx, i = 0, 1
+        while idx < len(people):
+            while count[i] == 0:
+                i += 1
+                people[idx] = 1
+                count[i] -= 1
+                idx += 1
+        res, l, r = 0, 0, len(people) - 1
+        while l <= r:
+            remain = limit - people[r]
+            r -= 1
+            res += 1
+            if l <= r and remain >= people[l]:
+                l += 1
+        return res
+    #time com:O(n)
+    #space com: O(m)
+    
