aa95fa0512346b855d1444287b307a7866530644
Isaac <zihao599@gmail.com>
2026-02-13 23:00:48 -0800

    contains dupilcate with hash map/set

--- Day 15 Contains duplicate 2.py
+++ Day 15 Contains duplicate 2.py
@@ -30,5 +30,30 @@ class Solution:
 #Time complexity: O(n * min(n,k))
 #space com: O(1)
 
+#Hash map
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        mp = {}
+        for i in range(len(nums)):
+            if nums[i] in mp and i - mp[nums[i]] <= k:
+                return True
+            mp[nums[i]] = i
+        return False
+#time complexity:O(n)
+#space complexity: O(n)
 
-                
+#Hash set
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window = set()
+        L = 0
+        for R in range(len(nums)):
+            if R - L > k:
+                window.remove(nums[L])
+                L += 1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
+        return False
+#time complexity:O(n)
+#space complexity: O(min(n,k))
