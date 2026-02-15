35cae1ddb4c59af5f206b481204fbd62aaef1b89
Isaac <zihao599@gmail.com>
2026-02-14 22:09:52 -0800

    Day 16 longest substring without repeating with sliding optimal

--- Day 16. Longest substring without repeating characters.py
+++ Day 16. Longest substring without repeating characters.py
@@ -34,3 +34,36 @@ class Solution:
 #time comple: O(n*m)
 #spce com: O(m)
 
+#sliding window
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[r])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+#time complexity: O(n)
+#space com: O(m)
+
+#slding window (optimal)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mp = {}
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            if s[r] in mp:
+                l = max(mp[s[r]] + 1, l)
+            mp[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
+#time com: O(n)
+#space com: O(m)
+  
