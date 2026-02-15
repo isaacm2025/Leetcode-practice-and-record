9fd9a9b3190c5e29699ff04b94763a06c947cf27
Isaac <zihao599@gmail.com>
2026-02-14 22:19:49 -0800

    longest repeating character with optimal sliding window

--- Day 16 Longest Repeating Character Replacement.py
+++ Day 16 Longest Repeating Character Replacement.py
@@ -32,4 +32,26 @@ class Solution:
                 if (j - i + 1) - maxf <= k:
                     res = max(res, j - i + 1)
         return res
+#time com: O(n^2)
+#space com: O(m)
     
+#optimal sliding window:
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        res = 0
+
+        l = 0
+        maxf =0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, couunt[s[r]])
+
+            while (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+
+        return res
+#time com: O(n)
+#space com: O(m)
