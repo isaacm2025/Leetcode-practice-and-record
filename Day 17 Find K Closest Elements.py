329d437d663f3da7790d23dd340fac08b66a431b
Isaac <zihao599@gmail.com>
2026-02-15 15:25:28 -0800

    find k closest elements with two pointers and binary search

--- Day 17 Find K Closest Elements.py
+++ Day 17 Find K Closest Elements.py
@@ -30,3 +30,29 @@ class Solution:
 #time com: O(n log n + k log k)
 #space com: O(1) or O(n) space depending on the sorting, O(k) for the output array.
 
+#two pointers:
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - 1
+        while r - l >= k:
+            if abs(x - arr[l]) <= abs(x - arr[r]):
+                r -= 1
+            else:
+                l += 1
+        return arr[l: r + 1]
+#time com: O(n - k)
+#space com: O(k)
+
+#binary search
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - k
+        while l < r:
+            m = (l + r) //2
+            if x - arr[m] > arr[m + k] - x:
+                l = m + 1
+            else:
+                r = m
+        return arr[l: l + k]
+#time com: O(log(n-k) + k)
+#space com: O(k) for the output array
