3650a75f63c4f72046853318fb19bc51329e5bb8
Isaac <zihao599@gmail.com>
2026-02-15 16:45:57 -0800

    Day 17 sliding window max with brute force

---
+++ Day 17 Sliding Window Maximum.py
@@ -0,0 +1,39 @@
+'''You are given an array of integers nums and an integer k. There is a sliding window of size k that starts at the left edge of the array. The window slides one position to the right until it reaches the right edge of the array.
+
+Return a list that contains the maximum element in the window at each step.
+
+Example 1:
+
+Input: nums = [1,2,1,0,4,2,6], k = 3
+
+Output: [2,2,4,4,6]
+
+Explanation:
+Window position            Max
+---------------           -----
+[1  2  1] 0  4  2  6        2
+ 1 [2  1  0] 4  2  6        2
+ 1  2 [1  0  4] 2  6        4
+ 1  2  1 [0  4  2] 6        4
+ 1  2  1  0 [4  2  6]       6
+Constraints:
+
+1 <= nums.length <= 1000
+-10,000 <= nums[i] <= 10,000
+1 <= k <= nums.length'''
+
+from typing import List
+#brute force:
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+
+        for i in range(len(nums) - k + 1):
+            maxi = nums[i]
+            for j in range(i, i + k):
+                maxi = max(maxi, nums[j])
+            output.append(maxi)
+        return output
+#time complexity: O(n * k)
+#space: O(1) extra space, O(n - k + 1), n is the length of array and k is the size of the window
+
