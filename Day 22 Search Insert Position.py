e30fce736fc2cc1af3d5d69c206574840eb4583c
Isaac <zihao599@gmail.com>
2026-02-20 22:39:03 -0800

    Day 22 search insert position with linear search

---
+++ Day 22 Search Insert Position.py
@@ -0,0 +1,32 @@
+'''You are given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [-1,0,2,4,6,8], target = 5
+
+Output: 4
+Example 2:
+
+Input: nums = [-1,0,2,4,6,8], target = 10
+
+Output: 6
+Constraints:
+
+1 <= nums.length <= 10,000.
+-10,000 < nums[i], target < 10,000
+nums contains distinct values sorted in ascending order.
+'''
+#linear search
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
+
+#time complexity: O(n)
+#space complexity: O(1)
+
