50eb5632d42c69fa4cb5eab3214480871ff7e672
Isaac <zihao599@gmail.com>
2026-02-24 22:43:34 -0800

    optimal binary search

--- Day 26 Median of Two Sorted Arrays.py
+++ Day 26 Median of Two Sorted Arrays.py
@@ -51,8 +51,8 @@ class Solution:
         i = j = 0
         median1 = median2 = 0
 
-        for count in range((len1 + len2) // 2 + 1):
-            median1 = median2
+        for count in range((len1 + len2) // 2 + 1): #we only need to iterate until the middle of the merged array
+            median2 = median1
             if i < len1 and j < len2:
                 if nums1[i] > nums2[j]:
                     median1 = nums2[j]
@@ -66,8 +66,42 @@ class Solution:
             else:
                 median1 = nums2[j]
                 j += 1
-        if (len1 + len2) % 2 == 1:
+        if (len1 + len2) % 2 == 1: #if the total length is odd, we return the middle element which is median1
             return float(median1)
         else:
             return (median1 + median2) / 2.0
-            
+        
+#time complexity O(n+m) where n and m are the lengths of the two arrays
+#space complexity O(1) since we are not using any extra space except for a few variables to keep track of the medians and pointers
+
+#binary search (optimal solution)
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2) #we need to find the total length of the merged array to determine the position of the median
+        half = total // 2
+        if len(B) < len(A):
+            A, B = B, A
+        l, r = 0, len(A) - 1
+        while True:
+            i = (l + r) // 2  # A
+            j = half - i - 2  # B
+
+            Aleft = A[i] if i >= 0 else float("-infinity") #we need to check if the index is out of bounds, if it is we can set it to infinity or negative infinity depending on whether we are looking at the left or right side of the partition
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity") #we need to check if the index is out of bounds, if it is we can set it to infinity or negative infinity depending on whether we are looking at the left or right side of the partition
+            Bleft = B[j] if j >= 0 else float("-infinity") #we need to check if the index is out of bounds, if it is we can set it to infinity or negative infinity depending on whether we are looking at the left or right side of the partition
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity") #we need to check if the index is out of bounds, if it is we can set it to infinity or negative infinity depending on whether we are looking at the left or right side of the partition
+
+            # partition is correct
+            if Aleft <= Bright and Bleft <= Aright: #if the left side of the partition is less than or equal to the right side of the partition, then we have found the correct partition
+                # odd
+                if total % 2:
+                    return min(Aright, Bright)
+                # even
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2 #if the total length is even, we need to return the average of the two middle elements which are the maximum of the left side of the partition and the minimum of the right side of the partition
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1 
+#time complexity O(log(min(n, m))) where n and m are the lengths of the two arrays
+#space complexity O(1) since we are not using any extra space except for a few variables to keep track of the medians and pointers
