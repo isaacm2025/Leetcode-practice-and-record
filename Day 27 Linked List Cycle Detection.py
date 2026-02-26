662554601c99067d681d58c5d002a56517a54ac5
Isaac <zihao599@gmail.com>
2026-02-25 23:47:53 -0800

    Day 27 linked list cycle detection with hash set

---
+++ Day 27 Linked List Cycle Detection.py
@@ -0,0 +1,49 @@
+'''Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return false.
+
+There is a cycle in a linked list if at least one node in the list can be visited again by following the next pointer.
+
+Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle exists.
+
+Note: index is not given to you as a parameter.
+
+Example 1:
+
+
+
+Input: head = [1,2,3,4], index = 1
+
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+
+
+
+Input: head = [1,2], index = -1
+
+Output: false
+Constraints:
+
+0 <= Length of the list <= 1000.
+-1000 <= Node.val <= 1000
+index is -1 or a valid index in the linked list.'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+#hash set
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set() #to store the nodes we have seen so far
+        cur = head #to traverse the linked list
+        while cur:
+            if cur in seen: #if we have seen this node before, then there is a cycle
+                return True
+            seen.add(cur) #add the current node to the set of seen nodes
+            cur = cur.next #move to the next node
+        return False
+#time complexity: O(n)
+#space complexity: O(n)
