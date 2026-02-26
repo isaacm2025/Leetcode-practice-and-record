aca471765dedc611bd0a6be98d480b3cb81b81d4
Isaac <zihao599@gmail.com>
2026-02-25 23:51:32 -0800

    Day 27 reorder linked list with brute force

---
+++ Day 27 Reorder Linked List.py
@@ -0,0 +1,60 @@
+'''You are given the head of a singly linked-list.
+
+The positions of a linked list of length = 7 for example, can intially be represented as:
+
+[0, 1, 2, 3, 4, 5, 6]
+
+Reorder the nodes of the linked list to be in the following order:
+
+[0, 6, 1, 5, 2, 4, 3]
+
+Notice that in the general case for a list of length = n the nodes are reordered to be in the following order:
+
+[0, n-1, 1, n-2, 2, n-3, ...]
+
+You may not modify the values in the list's nodes, but instead you must reorder the nodes themselves.
+
+Example 1:
+
+Input: head = [2,4,6,8]
+
+Output: [2,8,4,6]
+Example 2:
+
+Input: head = [2,4,6,8,10]
+
+Output: [2,10,4,8,6]
+Constraints:
+
+1 <= Length of the list <= 1000.
+1 <= Node.val <= 1000
+'''
+
+#brute force
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head:
+            return
+        
+        nodes = []
+        cur = head
+        while cur:
+            nodes.append(cur)
+            cur = cur.next
+        i, j = 0, len(nodes) - 1
+        while i < j:
+            nodes[i].next = nodes[j]
+            i += 1
+            if i == j:
+                break
+            nodes[j].next = nodes[i]
+            j -= 1
+        nodes[i].next = None
+#time complexity: O(n)
+#space complexity: O(n)
