c2ca1dcc719d998be3fa7d5031adb6dee6916371
Isaac <zihao599@gmail.com>
2026-02-26 21:32:30 -0800

    space optimization

--- Day 28 Copy Linked List with Random Pointer.py
+++ Day 28 Copy Linked List with Random Pointer.py
@@ -90,4 +90,34 @@ class Solution:
 #time complexity: O(n)
 #space complexity: O(n)
 
+#space optimization
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if head is None:
+            return None
+        l1 = head
+        while l1:
+            l2 = Node(l1.val)
+            l2.next = l1.random
+            l1.random = l2
+            l1 = l1.next
+
+        newHead = head.random
+        l1 = head
+        while l1:
+            l2 = l1.random
+            l2.random = l2.next.random if l2.next else None
+            l1 = l1.next
+        
+        l1 = head
+        while l1 is not None:
+            l2 = l1.random
+            l1.random = l2.next
+            l2.next = l1.next.random if l1.next else None
+            l1 = l1.next
+        return newHead
+#time complexity: O(n)
+#space complexity: O(1)
+
+
         
