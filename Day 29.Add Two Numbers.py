ff2136d044a9cf377822b83c2456f167e8045262
Isaac <zihao599@gmail.com>
2026-02-27 22:56:10 -0800

    Day 29 add two numbers with recursion

---
+++ Day 29.Add Two Numbers.py
@@ -0,0 +1,57 @@
+'''You are given two non-empty linked lists, l1 and l2, where each represents a non-negative integer.
+
+The digits are stored in reverse order, e.g. the number 321 is represented as 1 -> 2 -> 3 -> in the linked list.
+
+Each of the nodes contains a single digit. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Return the sum of the two numbers as a linked list.
+
+Example 1:
+
+
+
+Input: l1 = [1,2,3], l2 = [4,5,6]
+
+Output: [5,7,9]
+
+Explanation: 321 + 654 = 975.
+Example 2:
+
+Input: l1 = [9], l2 = [9]
+
+Output: [8,1]
+Constraints:
+
+1 <= l1.length, l2.length <= 100.
+0 <= Node.val <= 9'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+#recursion
+from typing import Optional
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1 and not l2 and carry == 0:
+            return None
+        
+        v1 = l1.val if l1 else 0
+        v2 = l2.val if l2 else 0
+        carry, val = divmod(v1 + v2 + carry, 10)
+        next_node = self.add(
+            l1.next if l1 else None,
+            l2.next if l2 else None,
+            carry
+        )
+        return ListNode(val, next_node)
+    
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        return self.add(l1, l2, 0)
+#time complexity: O(m+n) where m and n are the lengths of the two linked lists
+#space complexity: O(m + n) due to the recursive call stack and the new linked list created to store the result
+
