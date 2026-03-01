5060f1f00e1803ff7a0788c83c6a1f1256f0d23b
Isaac <zihao599@gmail.com>
2026-02-28 23:14:32 -0800

    Day 30 reverse nodes in K-group

---
+++ Day 30 Reverse Nodes in K-Group.py
@@ -0,0 +1,59 @@
+'''You are given the head of a singly linked list head and a positive integer k.
+
+You must reverse the first k nodes in the linked list, and then reverse the next k nodes, and so on. If there are fewer than k nodes left, leave the nodes as they are.
+
+Return the modified list after reversing the nodes in each group of k.
+
+You are only allowed to modify the nodes' next pointers, not the values of the nodes.
+
+Example 1:
+
+
+
+Input: head = [1,2,3,4,5,6], k = 3
+
+Output: [3,2,1,6,5,4]
+Example 2:
+
+
+
+Input: head = [1,2,3,4,5], k = 3
+
+Output: [3,2,1,4,5]
+Constraints:
+
+The length of the linked list is n.
+1 <= k <= n <= 100
+0 <= Node.val <= 100'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+#recursion
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        current = head
+        group = 0
+        while current and group < k:
+            current = current.next
+            group += 1
+        if group == k:
+            current = self.reverseKGroup(current, k)
+            while group > 0:
+                tmp = head.next
+                head.next = current
+                current = head
+                head = tmp
+                group -= 1
+            head = current
+        return head
+#time complexity: O(n)
+#space complexity: O(n/k) for the recursion stack, where n is the number of nodes in the linked list and k is the size of each group. 
+# In the worst case, when k is 1, the space complexity would be O(n) due to the recursion stack. 
+# However, when k is greater than 1, the space complexity is O(n/k) because each recursive call processes a group of k nodes.
+
+
