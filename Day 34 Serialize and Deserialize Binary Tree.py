09aa58c7cf4944602de3e0e7bc042a377433643a
Isaac <zihao599@gmail.com>
2026-03-04 22:08:57 -0800

    Day 34 Serialize and Deserialize Binary Tree with DFS

---
+++ Day 34 Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,64 @@
+'''Implement an algorithm to serialize and deserialize a binary tree.
+
+Serialization is the process of converting an in-memory structure into a sequence of bits so that it can be stored or sent across a network to be reconstructed later in another computer environment.
+
+You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure. There is no additional restriction on how your serialization/deserialization algorithm should work.
+
+Note: The input/output format in the examples is the same as how NeetCode serializes a binary tree. You do not necessarily need to follow this format.
+
+Example 1:
+
+
+
+Input: root = [1,2,3,null,null,4,5]
+
+Output: [1,2,3,null,null,4,5]
+Example 2:
+
+Input: root = []
+
+Output: []
+Constraints:
+
+0 <= The number of nodes in the tree <= 1000.
+-1000 <= Node.val <= 1000'''
+
+#DFS
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        def dfs(node):
+            if not node:
+                res.append('null')
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ','.join(res)
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        vals = data.split(",")
+        self.i = 0
+        def dfs():
+            if vals[self.i] == 'null':
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
+#time: O(n) for both serialize and deserialize
+#space: O(n) for both serialize and deserialize
