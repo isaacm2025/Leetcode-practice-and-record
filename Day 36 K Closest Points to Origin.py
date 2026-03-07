ec705f3b1b760405691f5b58d7dfbc37bca724f6
Isaac <zihao599@gmail.com>
2026-03-06 22:54:05 -0800

    max heap

--- Day 36 K Closest Points to Origin.py
+++ Day 36 K Closest Points to Origin.py
@@ -54,3 +54,21 @@ class Solution:
         return result
 #time complexity: O(nlogn)
 #space complexity: O(n)
+
+#max heap
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        max_heap = []
+        for x, y in points:
+            dist = -(x ** 2 + y ** 2)
+            heapq.heappush(max_heap, (dist, x, y))
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+        result = []
+        while max_heap:
+            dist, x, y = heapq.heappop(max_heap)
+            result.append([x, y])
+        return result
+#time complexity: O(nlogk)
+#space complexity: O(k)
