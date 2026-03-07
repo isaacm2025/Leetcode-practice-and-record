20987a175a9cc61ee63e2c6cff51d9d90ae443a8
Isaac <zihao599@gmail.com>
2026-03-06 22:53:10 -0800

    min heap

--- Day 36 K Closest Points to Origin.py
+++ Day 36 K Closest Points to Origin.py
@@ -36,3 +36,21 @@ class Solution:
         return points[:k]
 #time complexity: O(nlogn)
 #space complexity: O(1)
+
+#min heap
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        min_heap = []
+        for x, y in points:
+            dist = x ** 2 + y ** 2
+            min_heap.append((dist, x, y))
+        heapq.heapify(min_heap)
+        result = []
+        while k > 0:
+            dist, x, y = heapq.heappop(min_heap)
+            result.append([x, y])
+            k -= 1
+        return result
+#time complexity: O(nlogn)
+#space complexity: O(n)
