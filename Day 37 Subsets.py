d9a67767d8afe9e6b270f4d74222069dff0bcc60
Isaac <zihao599@gmail.com>
2026-03-07 17:36:55 -0800

    Day 37 subsets with iteration and backtracking

---
+++ Day 37 Subsets.py
@@ -0,0 +1,50 @@
+'''Given an array nums of unique integers, return all possible subsets of nums.
+
+The solution set must not contain duplicate subsets. You may return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [7]
+
+Output: [[],[7]]
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+'''
+
+#backtracking
+from ast import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+    
+    def dfs(i):
+        if i >= len(nums):
+            res.append(subset.copy())
+            return
+        subset.append(nums[i])
+        dfs(i + 1)
+        subset.pop()
+        dfs(i + 1)
+    dfs(0)
+    return res
+#time complexity: O(n*2^n) where n is the length of the input array. This is because there are 2^n subsets and each subset takes O(n) time to copy to the result list.
+#space complexity: O(n) for the recursion stack and O(n) for the subset list
+
+#iterative
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for num in nums:
+            res += [subset + [num] for subset in res]
+        return res
+#time complexity: O(n*2^n) where n is the length of the input array. This is because there are 2^n subsets and each subset takes O(n) time to copy to the result list.
+#space complexity: O(n*2^n) for the result list which contains all subsets
