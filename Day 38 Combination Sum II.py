2a6d8e5ecc09168b34735cdb4806746b5fcc5b9f
Isaac <zihao599@gmail.com>
2026-03-08 23:18:00 -0700

    hashmap

--- Day 38 Combination Sum II.py
+++ Day 38 Combination Sum II.py
@@ -28,6 +28,7 @@ Constraints:
 1 <= candidates[i] <= 50
 1 <= target <= 30'''
 
+from collections import defaultdict
 from typing import List, list
 
 #brute force
@@ -48,6 +49,36 @@ class Solution:
             generate_subsets(i + 1, cur, total)
         generate_subsets(0, [], 0)
         return [list(combination) for combination in res]
+#time complexity: O(n * 2^n) where n is the length of candidates
+#space complexity: O(n *2^n) for the recursion stack and O(k) for the result list
+
+#hashmap
+class Solution:
+    def combinationSum2(self, nums, candidates: List[int], target: int) -> List[List[int]]:
+        self.res = []
+        self.count = defaultdict(int)
+        cur = []
+        A = []
+
+        for num in nums:
+            if self.count[num] == 0:
+                A.append(num)
+            self.count[num] += 1
+        self.backtrack(A, target, 0, cur)
+        return self.res
+    def backtrack(self, A, target, i, cur):
+        if target == 0:
+            self.res.append(cur.copy())
+            return
+        if target < 0 or i >= len(nums):
+            return
+        if self.count[nums[i]] > 0:
+            cur.append(nums[i])
+            self.count[nums[i]] -= 1
+            self.backtrack(A, target - nums[i], i, cur)
+            cur.pop()
+            self.count[nums[i]] += 1
+        self.backtrack(nums, target, cur, i + 1)
 #time complexity: O(n * 2^n) where n is the length of candidates
 #space complexity: O(n *2^n) for the recursion stack and O(k) for the result list
 
