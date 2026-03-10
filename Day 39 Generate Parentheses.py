9d7f4fb89201c6c59c92f6a2c488c904b289cb22
Isaac <zihao599@gmail.com>
2026-03-09 20:09:25 -0700

    backtracking and dynamic programing

--- Day 39 Generate Parentheses.py
+++ Day 39 Generate Parentheses.py
@@ -42,4 +42,39 @@ class Solution:
 #time complexity: O(2^(2n) * n) where 2^(2n) is the number of possible combinations of parentheses and n is the time taken to validate each combination.
 #space complexity: O(2^(2n) * n) where 2^(2n) is the number of valid combinations of parentheses and n is the space taken to store each combination
 
+#backtracking
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        backtrack(0, 0)
+        return res
+#time complexity: O(4^n / sqrt(n)) where 4^n is the number of possible combinations of parentheses and sqrt(n) is the time taken to generate each combination.
+#space complexity: O(n) where n is the maximum depth of the recursion stack.
+
+#Dynamic programming
+class Solution:
+    def generateParenthesis(self, n):
+        res = [[] for _ in range(n + 1)]
+        res[0] = [""]
+        for k in range(n + 1):
+            for i in range(k):
+                for left in res[i]:
+                    for right in res[k - 1 - i]:
+                        res[k].append(f"({left}){right}")
+        return res[-1]
+#time complexity: O(4^n / sqrt(n)) where 4^n is the number of possible combinations of parentheses and sqrt(n) is the time taken to generate each combination.
+#space complexity: O(n) where n is the maximum depth of the recursion stack.
 
