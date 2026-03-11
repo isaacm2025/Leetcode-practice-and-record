55071aac9d27b1ac67cdd3d35fee68d4e69065fd
Isaac <zihao599@gmail.com>
2026-03-10 23:48:53 -0700

    hash set

--- Day 40 N-Queens.py
+++ Day 40 N-Queens.py
@@ -65,3 +65,33 @@ class Solution:
         return True
 #time complexity: O(n!)
 #space complexity: O(n^2) 
+
+#hash set
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDiag = set()
+        negDiag = set()
+        res = []
+        board = [["."] * n for i in range(n)]
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                backtrack(r + 1)
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+        backtrack(0)
+        return res
+#time complexity: O(n!)
+#space complexity: O(n^2)
