3b3967bbdf01678589d9ece216896a5b0ca9daf1
Isaac <zihao599@gmail.com>
2026-03-15 15:36:31 -0700

    Alien dictionary with DFS

---
+++ Day 45 Alien Dictionary.py
@@ -0,0 +1,72 @@
+'''There is a foreign language which uses the latin alphabet, but the order among letters is not "a", "b", "c" ... "z" as in English.
+
+You receive a list of non-empty strings words from the dictionary, where the words are sorted lexicographically based on the rules of this new language.
+
+Derive the order of letters in this language. If the order is invalid, return an empty string. If there are multiple valid order of letters, return any of them.
+
+A string a is lexicographically smaller than a string b if either of the following is true:
+
+The first letter where they differ is smaller in a than in b.
+a is a prefix of b and a.length < b.length.
+Example 1:
+
+Input: ["z","o"]
+
+Output: "zo"
+Explanation:
+From "z" and "o", we know 'z' < 'o', so return "zo".
+
+Example 2:
+
+Input: ["hrn","hrf","er","enn","rfnn"]
+
+Output: "hernf"
+Explanation:
+
+from "hrn" and "hrf", we know 'n' < 'f'
+from "hrf" and "er", we know 'h' < 'e'
+from "er" and "enn", we know get 'r' < 'n'
+from "enn" and "rfnn" we know 'e'<'r'
+so one possibile solution is "hernf"
+Constraints:
+
+The input words will contain characters only from lowercase 'a' to 'z'.
+1 <= words.length <= 100
+1 <= words[i].length <= 100'''
+
+from collections import defaultdict, deque
+from typing import List
+
+#DFS
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adj = {c: set() for w in words for c in w}
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+        visited = {}
+        res = []
+        def dfs(c):
+            if i in visited:
+                return visited[i]
+            visited[i] = True
+            for neighbor in adj[i]:
+                if dfs(neighbor):
+                    return True
+            visited[i] = False
+            res.append(i)
+        for i in adj:
+            if dfs(i):
+                return ""
+        res.reverse()
+        return "".join(res)
+#time complexity: O(N + V + E) where N is the total number of characters in the input, V is the number of unique characters and E is the number of edges in the graph.
+#space complexity: O(V + E) where V is the number of unique characters and E is the number of edges in the graph.
+            
+
