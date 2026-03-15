32ff0432ad5fc915e4cf04bc3a1a46e681327618
Isaac <zihao599@gmail.com>
2026-03-14 23:02:28 -0700

    DFS optimal

--- Day 44 Redundant Connection.py
+++ Day 44 Redundant Connection.py
@@ -52,4 +52,39 @@ class Solution:
         return []
 #time complexity: O(E *(V + E)) where V is the number of vertices and E is the number of edges
 #space complexity: O(V + E) for the adjacency list and visited array
+
+#DFS optimal
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        adj = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        visited = [False] * (n + 1)
+        cycle = set()
+        cycleStart = -1
+        def dfs(node, par):
+            nonlocal cycleStart
+            if visited[node]:
+                cycleStart = node
+                return True
+            visited[node] = True
+            for nei in adj[node]:
+                if nei == par:
+                    continue
+                if dfs(nei, node):
+                    if cycleStart != -1:
+                        cycle.add(node)
+                    if node == cycleStart:
+                        cycleStart = -1
+                    return True
+            return False
+        dfs(1, -1)
+        for u, v in reversed(edges):
+            if u in cycle and v in cycle:
+                return [u, v]
+        return []
+#time complexity: O(V + E) where V is the number of vertices and E is the number of edges
+#space complexity: O(V + E) for the adjacency list and visited array and cycle set
             
