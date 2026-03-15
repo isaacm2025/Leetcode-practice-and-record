6e0f24a8c0d768c62b915967cc32499597a6888f
Isaac <zihao599@gmail.com>
2026-03-15 15:15:13 -0700

    binary + DFS

--- Day 45 Swim in Rising Water.py
+++ Day 45 Swim in Rising Water.py
@@ -61,3 +61,69 @@ class Solution:
         return dfs((0, 0), 0)
 #time complexity: O(4^(n^2))
 #space complexity: O(n^2)
+
+#DFS
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        visit = [[False] * n for _ in range(n)]
+        minH = maxH = grid[0][0]
+        for row in range(n):
+            maxH = max(maxH, max(grid[row]))
+            minH = min(minH, min(grid[row]))
+        def dfs(node, t):
+            r, c = node
+            if (min(r, c) < 0 or max(r, c) >= n or visit[r][c] or grid[r][c] > t):
+                return False
+            if r == (n - 1) and c == (n - 1):
+                return True
+            visit[r][c] = True
+            return (dfs((r + 1, c), t) or
+                    dfs((r - 1, c), t) or
+                    dfs((r, c + 1), t) or
+                    dfs((r, c - 1), t))
+        for t in range(minH, maxH):
+            if dfs((0, 0), t):
+                return t
+            for row in range(n):
+                for col in range(n):
+                    visit[row][col] = False
+        return maxH
+#time complexity: O(n^4)
+#space complexity: O(n^2)
+
+
+#binary search + DFS
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        visit = [[False] * n for _ in range(n)]
+        minH = maxH = grid[0][0]
+        for row in range(n):
+            maxH = max(maxH, max(grid[row]))
+            minH = min(minH,min(grid[row]))
+        def dfs(node, t):
+            r, c = node
+            if (min(r, c) < 0 or max(r, c) >= n or visit[r][c] or grid[r][c] > t):
+                return False
+            if r == (n - 1) and c == (n - 1):
+                return True
+            visit[r][c] = True
+            return (dfs((r + 1, c), t) or
+                    dfs((r - 1, c), t) or
+                    dfs((r, c + 1), t) or
+                    dfs((r, c - 1), t))
+        l, r = minH, maxH
+        while l < r:
+            m = (l + r) // 2
+            if dfs((0, 0), m):
+                r = m
+            else:
+                l = m + 1
+            for row in range(n):
+                for col in range(n):
+                    visit[row][col] = False
+        return r
+#time complexity: O(n^2 logn)
+#space complexity: O(n^2)
+                       
