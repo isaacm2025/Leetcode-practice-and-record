c5ca8e1ac26df994bcc90fcc6deac80cbeeb85b9
Isaac <zihao599@gmail.com>
2026-03-16 22:40:36 -0700

    dynamic programming

--- Day 46 Min Cost Climbing Stairs.py
+++ Day 46 Min Cost Climbing Stairs.py
@@ -40,3 +40,28 @@ class Solution:
         return min(dfs(0), dfs(1))
 #time complexity: O(2^n)
 #space complexity: O(n)
+
+
+#dynamic programming
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = [-1] * len(cost)
+        def dfs(i):
+            if i >= len(cost):
+                return 0 
+            if memo[i] != -1:
+                return memo[i]
+            memo[i] = cost[i] + min(dfs(i + 1), dfs(i + 2))
+            return memo[i]
+        return min(dfs(0), dfs(1))
+#time complexity: O(n)
+#space complexity: O(n)
+
+#dynamic programming with space optimization
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cost[1])
+#time complexity: O(n)
+#space complexity: O(1)
