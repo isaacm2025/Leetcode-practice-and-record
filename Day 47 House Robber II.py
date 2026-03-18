126464e2c7216f0e67fd6e432ed9a03bd39f5bf4
Isaac <zihao599@gmail.com>
2026-03-17 22:55:34 -0700

    optimized dynamic

--- Day 47 House Robber II.py
+++ Day 47 House Robber II.py
@@ -53,3 +53,17 @@ class Solution:
         return max(dfs(0, True), dfs(1, False))
 #time complexity: O(n)
 #space complexity: O(n)
+
+#optimized dynamic programming
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+        for num in nums:
+            newRob = max(rob1 + num, rob2)
+            rob1 = rob2
+            rob2 = newRob
+        return rob2
+#time complexity: O(n)
+#space complexity: O(1)
