8f647aab446700261982b8d976bfaf8dfac483d3
Isaac <zihao599@gmail.com>
2026-03-17 23:30:43 -0700

    manacher's algorithm

--- Day 47 Palindromic Substrings.py
+++ Day 47 Palindromic Substrings.py
@@ -50,3 +50,25 @@ class Solution:
 #time complexity: O(n^2)
 #space complexity: O(n^2)
 
+#manacher's algorithm
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        def manacher(s):
+            t = '#' + '#'.join(s) + '#'
+            n = len(t)
+            p = [0] * n
+            l, r = 0, 0
+            for i in range(n):
+                p[i] = min(r - i, p[l + r - i]) if i < r else 0
+                while (i + p[i] + 1 < n and i - p[i] - 1 >= 0 and t[i + p[i] + 1] == t[i - p[i] - 1]):
+                    p[i] += 1
+                if i + p[i] > r:
+                    l, r = i - p[i], i + p[i]
+            return p
+        p = manacher(s)
+        res = 0
+        for i in range(len(p)):
+            res += (p[i] + 1) // 2
+        return res
+#time complexity: O(n)
+#space complexity: O(n)
