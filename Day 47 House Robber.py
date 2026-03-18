b54b8571802307688a68423c23f306edb4f696b6
Isaac <zihao599@gmail.com>
2026-03-17 22:43:48 -0700

    optimal dynamic programming

--- Day 47 House Robber.py
+++ Day 47 House Robber.py
@@ -65,3 +65,15 @@ class Solution:
         return dp[-1]
 #time complexity: O(n)
 #space complexity: O(n)
+
+#optimized dynamic programming
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for num in nums:
+            temp = max(rob1 + num, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+#time complexity: O(n)
+#space complexity: O(1)
