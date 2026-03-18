89e770145a63cb6781a603cf07b44327ba082dcc
Isaac <zihao599@gmail.com>
2026-03-17 23:27:58 -0700

    dynamic progm

--- Day 47 Palindromic Substrings.py
+++ Day 47 Palindromic Substrings.py
@@ -35,3 +35,18 @@ class Solution:
         return res
 #time complexity: O(n^3)
 #space complexity: O(1)
+
+#dynamic programming
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n, res = len(s), 0
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    res += 1
+        return res
+#time complexity: O(n^2)
+#space complexity: O(n^2)
+
