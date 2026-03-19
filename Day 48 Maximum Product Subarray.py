e9bc8b291ce07f27115f15ea579e129c5edf7c27
Isaac <zihao599@gmail.com>
2026-03-18 22:58:32 -0700

    prefiix & suffix

--- Day 48 Maximum Product Subarray.py
+++ Day 48 Maximum Product Subarray.py
@@ -72,4 +72,17 @@ class Solution:
 #time complexity: O(n)
 #space complexity: O(n)
 
+#prefix & suffix
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        n, res = len(nums), nums[0]
+        prefix, suffix = 0, 0
+        for i in range(n):
+            prefix = nums[i] * (prefix or 1)
+            suffix = nums[n - 1 - i] * (suffix or 1)
+            res = max(res, max(prefix, suffix))
+        return res
+#time complexity: O(n)
+#space complexity: O(1)
+
                 
