c6dacf7e8b13ce19de7e27cba3b0adb83aa190c7
Isaac <zihao599@gmail.com>
2026-03-19 23:02:49 -0700

    math and optimized dp

--- Day 49 Unique Paths.py
+++ Day 49 Unique Paths.py
@@ -62,3 +62,30 @@ class Solution:
 #time complexity: O(m * n)
 #space complexity: O(m * n)
 
+#optimal
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(m - 2, -1, -1):
+            for j in range(n - 2, -1, -1):
+                dp[j] += dp[j + 1]
+        return dp[0]
+#time complexity: O(m * n)
+#space complexity: O(n)
+
+#math
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+        if m < n:
+            m, n = n, m
+        res = j = 1
+        for i in range(m, m + n - 1):
+            res *= i
+            res //= j
+            j += 1
+        return res
+#time complexity: O(m + n)
+#space complexity: O(1)
+
