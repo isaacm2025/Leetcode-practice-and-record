34d6272237ece77c2ff6abd383f76a60af3bf410
Isaac <zihao599@gmail.com>
2026-03-19 22:20:27 -0700

    dp and binary search

--- Day 49 Longest Increasing Subsequence.py
+++ Day 49 Longest Increasing Subsequence.py
@@ -20,6 +20,7 @@ Constraints:
 1 <= nums.length <= 1000
 -1000 <= nums[i] <= 1000
 '''
+from bisect import bisect_left
 from typing import List
 #recursion
 class Solution:
@@ -72,3 +73,21 @@ class Solution:
 #time complexity: O(n^2)
 #space complexity: O(n)
 
+#dynamic programming with binary search
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = []
+        dp.append(nums[0])
+
+        LIS = 1
+        for i in range(1, len(nums)):
+            if dp[-1] < nums[i]:
+                dp.append(nums[i])
+                LIS += 1
+                continue
+            idx = bisect_left(dp, nums[i])
+            dp[idx] = nums[i]
+        return LIS
+#time complexity: O(nlogn)
+#space complexity: O(n)
+            
