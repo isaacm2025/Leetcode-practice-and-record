d3b7955f9deb92fc93a332825e304b88415e1c3b
Isaac <zihao599@gmail.com>
2026-03-19 23:00:07 -0700

    dp

--- Day 49 Unique Paths.py
+++ Day 49 Unique Paths.py
@@ -33,4 +33,32 @@ class Solution:
 #time complexity: O(2^(m + n))
 #space complexity: O(m + n)
 
+#dp with top down
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = [[-1] * n for _ in range(m)]
+        def dfs(i, j):
+            if i == (m - 1) and j ==(n - 1):
+                return 1
+            if i >= m or j >= n:
+                return 0
+            if memo[i][j] != -1:
+                return memo[i][j]
+            memo[i][j] = dfs(i, j + 1) + dfs(i + 1, j)
+            return memo[i][j]
+        return dfs(0, 0)
+#time complexity: O(m * n)
+#space complexity: O(m * n)
+
+#dp with bottom up
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * (n+1) for _ in range(m + 1)]
+        dp[m - 1][n] = 1
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                dp[i][j] = dp[i][j + 1] + dp[i + 1][j]
+        return dp[0][0]
+#time complexity: O(m * n)
+#space complexity: O(m * n)
 
