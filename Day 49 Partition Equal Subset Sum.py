2071635b45bc1a4c7cc75ee1490163fd8eb1891b
Isaac <zihao599@gmail.com>
2026-03-19 22:44:59 -0700

    dp and space optimization

--- Day 49 Partition Equal Subset Sum.py
+++ Day 49 Partition Equal Subset Sum.py
@@ -38,4 +38,45 @@ class Solution:
 #DP with top down
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2:
+            return False
+        target = total // 2
+        n = len(nums)
+        memo = [[-1] * (target + 1) for _ in range(len)]
+
+        def dfs(i, target):
+            if target == 0:
+                return True
+            if i >= n or target < 0:
+                return False
+            if memo[i][target] != -1:
+                return memo[i][target]
+            memo[i][target] = (dfs(i + 1, target) or dfs(i + 1, target - nums[i]))
+            return memo[i][target]
+        return dfs(0, target)
+#time complexity: O(n * target)
+#space complexity: O(n * target)
+
+#dp with space optimization
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+        target = sum(nums) // 2
+        dp = [False] * (target + 1)
+        nextDp = [False] * (target + 1)
+        dp[0] = True
+        for i in range(len(nums)):
+            for j in range(target + 1):
+                if j >= nums[i]:
+                    nextDp[j] = dp[j] or dp[j - nums[i]]
+                else:
+                    nextDp[j] = dp[j]
+            dp, nextDp = nextDp, dp
+        return dp[target]
+#time complexity: O(n * target)
+#space complexity: O(target)
+
+
         
