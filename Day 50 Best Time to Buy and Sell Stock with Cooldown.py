8bee71bfd9114c724e4b301dc4e4813b6022ccff
Isaac <zihao599@gmail.com>
2026-03-20 22:53:21 -0700

    dp

--- Day 50 Best Time to Buy and Sell Stock with Cooldown.py
+++ Day 50 Best Time to Buy and Sell Stock with Cooldown.py
@@ -43,3 +43,45 @@ class Solution:
         return dfs(0, True)
 #time complexity: O(2^n)
 #space complexity: O(n)
+
+#dp with top down
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {}
+        def dfs(i, buying):
+            if i >= len(prices):
+                return 0
+            if (i, buying) in dp:
+                return dp[(i, buying)]
+            cooldown = dfs(i + 1, buying)
+            if buying:
+                buy = dfs(i + 1, not buying) - prices[i]
+                dp[(i, buying)] = max(cooldown, buy)
+            else:
+                sell = dfs(i + 2, not buying) + prices[i]
+                dp[(i, buying)] = max(cooldown, sell)
+            return dp[(i, buying)]
+        return dfs(0, True)
+#time complexity: O(n)
+#space complexity: O(n)
+
+#dp with bottom up
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n + 1)]
+
+        for i in range(n - 1, -1, -1):
+            for buying in [True, False]:
+                if buying:
+                    buy = dp[i + 1][False] - prices[i] if i + 1 < n else - prices[i]
+                    cooldown = dp[i + 1][True] if i + 1 < n else 0
+                    dp[i][1] = max(cooldown, buy)
+                else:
+                    sell = dp[i + 2][True] + prices[i] if i + 2 < n else prices[i]
+                    cooldown = dp[i + 1][False] if i + 1 < n else 0
+                    dp[i][0] = max(cooldown, sell)
+        return dp[0][1]
+#time complexity: O(n)
+#space complexity: O(n)
+
