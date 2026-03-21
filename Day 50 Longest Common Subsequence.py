9df494bb64d5773d1733c215a8568ac1da094289
Isaac <zihao599@gmail.com>
2026-03-20 22:41:43 -0700

    dp

--- Day 50 Longest Common Subsequence.py
+++ Day 50 Longest Common Subsequence.py
@@ -39,3 +39,37 @@ class Solution:
         return dfs(0, 0)
 #time complexity: O(2^(m + n))
 #space complexity: O(m + n)
+
+#dp with top down
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = {}
+        def dfs(i, j):
+            if i == len(text1) or j == len(text2):
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if text1[i] == text2[j]:
+                memo[(i, j)] = 1 + dfs(i + 1, j + 1)
+                return memo[(i, j)]
+            else:
+                memo[(i, j)] = max(dfs(i + 1, j), dfs(i, j + 1))
+                return memo[(i, j)]
+        return dfs(0, 0)
+#time complexity: O(m * n)
+#space complexity: O(m * n)
+
+#dp with bottom up
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+        return dp[0][0]
+#time complexity: O(m * n)
+#space complexity: O(m * n)
