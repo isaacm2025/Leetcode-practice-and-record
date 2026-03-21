dde80f265d131a96faa93b04325d8437af3c5eac
Isaac <zihao599@gmail.com>
2026-03-20 23:03:31 -0700

    optimal

--- Day 50 Coin Change II.py
+++ Day 50 Coin Change II.py
@@ -63,3 +63,14 @@ class Solution:
 #time complexity: O(n * a)
 #space complexity: O(n * a)
 
+#space optimized dp
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for a in range(coin, amount + 1):
+                dp[a] += dp[a - coin]
+        return dp[amount]
+#time complexity: O(n * a)
+#space complexity: O(a)
