e4730c6e11acdcec0b1eccc9d0de09debc89497b
Isaac <zihao599@gmail.com>
2026-03-21 22:37:06 -0700

    dp optimized space

--- Day 51 Target Sum.py
+++ Day 51 Target Sum.py
@@ -68,4 +68,19 @@ class Solution:
 #time complexity: O(n * m)
 #space complexity: O(n * m) where m is the range of possible sums (from -1000 to 1000)
 
+#dp (optimized space)
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1
+        for num in nums:
+            next_dp = defaultdict(int)
+            for total, count in dp.items():
+                next_dp[total + num] += count
+                next_dp[total - num] += count
+            dp = next_dp
+        return dp[target]
+#time complexity: O(n * m)
+#space complexity: O(m) where m is the range of possible sums (from -1000 to 1000)
+
 
