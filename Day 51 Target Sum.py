00371c7b2a2abf4d4a1e4166bacafec9802e8895
Isaac <zihao599@gmail.com>
2026-03-21 22:21:26 -0700

    recursion

---
+++ Day 51 Target Sum.py
@@ -0,0 +1,40 @@
+'''You are given an array of integers nums and an integer target.
+
+For each number in the array, you can choose to either add or subtract it to a total sum.
+
+For example, if nums = [1, 2], one possible sum would be "+1-2=-1".
+If nums=[1,1], there are two different ways to sum the input numbers to get a sum of 0: "+1-1" and "-1+1".
+
+Return the number of different ways that you can build the expression such that the total sum equals target.
+
+Example 1:
+
+Input: nums = [2,2,2], target = 2
+
+Output: 3
+Explanation: There are 3 different ways to sum the input numbers to get a sum of 2.
++2 +2 -2 = 2
++2 -2 +2 = 2
+-2 +2 +2 = 2
+
+Constraints:
+
+1 <= nums.length <= 20
+0 <= nums[i] <= 1000
+-1000 <= target <= 1000'''
+
+
+from typing import List
+#recursion
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        def backtrack(i, total):
+            if i == len(nums):
+                return total == target
+            return (backtrack(i + 1, total + nums[i]) + backtrack(i + 1, total - nums[i]))
+        return backtrack(0, 0)
+
+#time complexity: O(2^n)
+#space complexity: O(n) for the recursion stack
+
+
