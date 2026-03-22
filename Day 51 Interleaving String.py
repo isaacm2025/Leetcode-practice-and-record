9fc64d35e5a43c91936bf56fabab37e05c508f6b
Isaac <zihao599@gmail.com>
2026-03-21 22:58:32 -0700

    dp

--- Day 51 Interleaving String.py
+++ Day 51 Interleaving String.py
@@ -51,3 +51,43 @@ class Solution:
 #time complexity: O(2^(m+n)) where m and n are the lengths of s1 and s2
 #space complexity: O(m+n) for the recursion stack
 
+#dp (top-down)
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        dp = {}
+        def dfs(i, j, k):
+            if k == len(s3):
+                return (i == len(s1) and (j == len(s2)))
+            if (i, j) in dp:
+                return dp[(i, j)]
+            res = False
+            if i < len(s1) and s1[i] == s3[k]:
+                res = dfs(i + 1, j, k + 1)
+            if not res and j < len(s2) and s2[j] == s3[k]:
+                res = dfs(i, j + 1, k + 1)
+            dp[(i, j)] = res
+            return res
+        return dfs(0, 0, 0)
+#time complexity: O(m*n) where m and n are the lengths of s1 and s2
+#space complexity: O(m*n) for the dp dictionary
+
+#dp (bottom-up)
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        m, n = len(s1), len(s2)
+        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+        dp[len(s1)][len(s2)] = True
+        for i in range(len(s1), -1, -1):
+            for j in range(len(s2), -1, -1):
+                if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
+                    dp[i][j] = True
+                if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
+                    dp[i][j] = True
+        return dp[0][0]
+#time complexity: O(m*n) where m and n are the lengths of s1 and s2
+#space complexity: O(m*n) for the dp table
+
