480f87c8bd92a893c98a01266fce7e315f8742b5
Isaac <zihao599@gmail.com>
2026-03-23 22:58:33 -0700

    recursion for jump game

---
+++ Day 53 Jump Game.py
@@ -0,0 +1,38 @@
+'''You are given an integer array nums where each element nums[i] indicates your maximum jump length at that position.
+
+Return true if you can reach the last index starting from index 0, or false otherwise.
+
+Example 1:
+
+Input: nums = [1,2,0,1,0]
+
+Output: true
+Explanation: First jump from index 0 to 1, then from index 1 to 3, and lastly from index 3 to 4.
+
+Example 2:
+
+Input: nums = [1,2,1,0,1]
+
+Output: false
+Constraints:
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 1000
+'''
+
+
+from typing import List
+#recursion
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        def dfs(i):
+            if i == len(nums) -1:
+                return True
+            end = min(len(nums) - 1, i + nums[i])
+            for j in range(i + 1, end + 1):
+                if dfs(j):
+                    return True
+            return False
+        return dfs(0)
+#time complexity: O(n!)
+#space complexity: O(n)
