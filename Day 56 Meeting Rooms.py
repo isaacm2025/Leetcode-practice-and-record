e21f3c869765ad07d62388e5a09b93db4c4ef1a2
Isaac <zihao599@gmail.com>
2026-03-26 23:01:25 -0700

    sorting

--- Day 56 Meeting Rooms.py
+++ Day 56 Meeting Rooms.py
@@ -50,3 +50,12 @@ class Solution:
 
 class Solution:
     def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda i: i.start)
+        for i in range(1, len(intervals)):
+            i1 = intervals[i - 1]
+            i2 = intervals[i]
+            if i1.end > i2.start:
+                return False
+        return True
+#time complexity: O(nlogn) due to sorting
+#space complexity: O(1) if we ignore the space used by sorting, otherwise O(n)
