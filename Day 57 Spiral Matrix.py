7abd730467f9eb3c26842f155a2b3f928faa76eb
Isaac <zihao599@gmail.com>
2026-03-27 22:42:09 -0700

    optimal iteration

--- Day 57 Spiral Matrix.py
+++ Day 57 Spiral Matrix.py
@@ -68,3 +68,22 @@ class Solution:
         return res
 #time complexity O(m*n)
 #space complexity O(m*n) for the result list
+
+#iteration optimized
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        steps = [len(matrix[0]), len(matrix) - 1]
+        r, c, d = 0, -1, 0
+        while steps[d & 1]:
+            for i in range(steps[d & 1]):
+                r += directions[d][0]
+                c += directions[d][1]
+                res.append(matrix[r][c])
+            steps[d & 1] -= 1
+            d += 1
+            d %= 4
+        return res
+#time complexity O(m*n)
+#space complexity O(m*n) for the result list
