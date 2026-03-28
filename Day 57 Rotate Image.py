70c57a48f3985883ed8225646aa08f6e07fb576f
Isaac <zihao599@gmail.com>
2026-03-27 22:28:33 -0700

    reverse and transposee & rotate by four cells

--- Day 57 Rotate Image.py
+++ Day 57 Rotate Image.py
@@ -53,4 +53,34 @@ class Solution:
 #time complexity O(n^2)
 
 
+#rotate by four cells
 class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        l, r = 0, len(matrix) - 1
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+                # save the top left
+                top_left = matrix[top][l + i]
+                # move bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+                # move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+                # move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+                # move top left into top right
+                matrix[top + i][r] = top_left
+            l += 1
+            r -= 1
+#space complexity O(1)
+#time complexity O(n^2)
+
+#transpose and reverse
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+#space complexity O(1)
+#time complexity O(n^2)
