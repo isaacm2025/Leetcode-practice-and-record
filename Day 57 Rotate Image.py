4f677c4e394702ff34b6ed63dc9f8e17fb0fdf8c
Isaac <zihao599@gmail.com>
2026-03-27 22:09:15 -0700

    rotate image brute force

---
+++ Day 57 Rotate Image.py
@@ -0,0 +1,56 @@
+'''Given a square n x n matrix of integers matrix, rotate it by 90 degrees clockwise.
+
+You must rotate the matrix in-place. Do not allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+
+
+Input: matrix = [
+  [1,2],
+  [3,4]
+]
+
+Output: [
+  [3,1],
+  [4,2]
+]
+Example 2:
+
+
+
+Input: matrix = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+
+Output: [
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000'''
+
+from typing import List
+
+#brute force
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        rotated = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                rotated[j][n - 1 - i] = matrix[i][j]
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = rotated[i][j]
+#space complexity O(n^2)
+#time complexity O(n^2)
+
+
+class Solution:
