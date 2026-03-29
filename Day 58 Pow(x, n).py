60392c44ebc8c8ad8dde819f6e1d90a7071ec02f
Isaac <zihao599@gmail.com>
2026-03-28 23:18:26 -0700

    iteration

--- Day 58 Pow(x, n).py
+++ Day 58 Pow(x, n).py
@@ -55,3 +55,21 @@ class Solution:
         return res if n >= 0 else 1 / res
 #time complexity O(log n) where n is the absolute value of the exponent
 #space complexity O(log n) for the recursive call stack
+
+#iteration
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+        if n == 0:
+            return 1
+        res = 1
+        power = abs(n)
+        while power:
+            if power & 1:
+                res * = x
+            x *= x
+            power >>= 1
+        return res if n >= 0 else 1 / res
+#time complexity O(log n) where n is the absolute value of the exponent
+#space complexity O(1) since we are using a constant amount of space to store the result
