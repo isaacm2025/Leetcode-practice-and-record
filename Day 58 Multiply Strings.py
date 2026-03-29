e6e7b2510935186a2c7f41c073cde86ba2a1b6b4
Isaac <zihao599@gmail.com>
2026-03-28 23:21:34 -0700

    multiplication

--- Day 58 Multiply Strings.py
+++ Day 58 Multiply Strings.py
@@ -34,3 +34,23 @@ class Solution:
         return str(res)
 #time complexity O(m * n) where m and n are the lengths of num1 and num2 respectively
 #space complexity O(1) since we are using a constant amount of space
+
+
+#multiplication using an array to store the intermediate results
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if "0" in [num1, num2]:
+            return "0"
+        res = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                digit = int(num1[i]) * int(num2[j])
+                res[i + j] += int(num1[i]) * int(num2[j])
+                res[i + j + 1] += res[i + j] // 10
+                res[i + j] %= 10
+        while res[-1] == 0:
+            res.pop()
+        return "".join(str(x) for x in res[::-1])
+#time complexity O(m * n) where m and n are the lengths of num1 and num2 respectively
+#space complexity O(m + n) for the array to store the intermediate results
