1805cf7ae4ca2390ef5dd8b4db4811f8e4a26d70
Isaac <zihao599@gmail.com>
2026-03-29 22:56:37 -0700

    hashset

--- Day 59 Single Number.py
+++ Day 59 Single Number.py
@@ -47,3 +47,14 @@ class Solution:
 #space complexity: O(1)
 
 #Hash set
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+        return list(seen)[0]
+#time complexity: O(n)
+#space complexity: O(n)
