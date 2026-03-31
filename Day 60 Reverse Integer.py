33faa9829fec0ffdacfad45286d21a7c4bc4fb8b
Isaac <zihao599@gmail.com>
2026-03-30 23:18:14 -0700

    reverse int with brute force

---
+++ Day 60 Reverse Integer.py
@@ -0,0 +1,38 @@
+'''You are given a signed 32-bit integer x.
+
+Return x after reversing each of its digits. After reversing, if x goes outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0 instead.
+
+Solve the problem without using integers that are outside the signed 32-bit integer range.
+
+Example 1:
+
+Input: x = 1234
+
+Output: 4321
+Example 2:
+
+Input: x = -1234
+
+Output: -4321
+Example 3:
+
+Input: x = 1234236467
+
+Output: 0
+Constraints:
+
+-2^31 <= x <= 2^31 - 1'''
+
+#brute force
+class Solution:
+    def reverse(self, x: int) -> int:
+        org = x
+        x = abs(x)
+        res = int(str(x)[::-1])
+        if org < 0:
+            res *= -1
+        if res < -(1 << 31) or res > (1 << 31) - 1:
+            return 0
+        return res
+#time complexity: O(n)
+#space complexity: O(n)
