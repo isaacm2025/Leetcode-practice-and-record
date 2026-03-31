d170abb2d75d5e7294a9f22c5601a55115bf1e0f
Isaac <zihao599@gmail.com>
2026-03-30 23:15:25 -0700

    bit manipulate

--- Day 60 Sum of Two Integers.py
+++ Day 60 Sum of Two Integers.py
@@ -21,3 +21,16 @@ class Solution:
 #time complexity: O(1)
 #space complexity: O(1)
 
+#bit manipulation
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xffffffff
+        max_int = 0x7fffffff
+        while b!=0:
+            carry = (a & b) << 1
+            a = (a ^ b) & mask
+            b = carry & mask
+        return a if a <= max_int else ~(a ^ mask)
+#time complexity: O(1)
+#space complexity: O(1)
+    
