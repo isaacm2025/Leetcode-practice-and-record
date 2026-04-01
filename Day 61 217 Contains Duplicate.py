69242df82995f12489b756c296ff6a420a06234a
Isaac <zihao599@gmail.com>
2026-03-31 23:29:32 -0700

    Brute force and sorting for Contains Duplicate

---
+++ Day 61 217 Contains Duplicate.py
@@ -0,0 +1,38 @@
+'''Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false'''
+
+
+from typing import List
+#brute force
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+#time complexity: O(n^2)
+#space complexity: O(1)
+
+#sorting
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+#time complexity: O(n log n)
+#space complexity: O(1)
+        
