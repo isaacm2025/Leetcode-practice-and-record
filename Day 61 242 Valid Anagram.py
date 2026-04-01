91590cdd8f09088ce09c4816061b810a01e7f4b7
Isaac <zihao599@gmail.com>
2026-03-31 23:36:46 -0700

    sorting for valid anagram

---
+++ Day 61 242 Valid Anagram.py
@@ -0,0 +1,26 @@
+'''Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+
+Input: s = "racecar", t = "carrace"
+
+Output: true
+Example 2:
+
+Input: s = "jar", t = "jam"
+
+Output: false
+Constraints:
+
+s and t consist of lowercase English letters.'''
+
+#sorting
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
+#time complexity: O(n log n + m log m) where n and m are the lengths of s and t respectively
+#space complexity: O(n + m) or O(1) if we consider the space used by the sorting algorithm as negligible
