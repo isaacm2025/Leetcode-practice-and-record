02443219ae9857099fe5a50e762806366d6fe242
Isaac <zihao599@gmail.com>
2026-04-01 23:42:33 -0700

    optimal and division

--- Day 62 238 Products of Array Except Self.py
+++ Day 62 238 Products of Array Except Self.py
@@ -40,5 +40,39 @@ class Solution:
         return res
 #time complexity is O(n^2) and space complexity is O(n)
 
+#division
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod, zero_count = 1, 0
+        for num in nums:
+            if num:
+                prod *= num
+            else:
+                zero_count += 1
+        if zero_count > 1:
+            return [0] * len(nums)
+        res = [0] * len(nums)
+        for i, c in enumerate(nums):
+            if zero_count:
+                res[i] = 0 if c else prod
+            else:
+                res[i] = prod // c
+        return res
+#time complexity is O(n) and space complexity is O(n), O(1) if we don't count the output array.
+
+#prefix and suffix optimal
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] *= prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+#time complexity is O(n) and space complexity is O(n), O(1) if we don't count the output array.
 
 
