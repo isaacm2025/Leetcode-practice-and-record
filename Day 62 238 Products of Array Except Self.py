d15fcb4a1bf1808e2bfa975b281629694da0aa79
Isaac <zihao599@gmail.com>
2026-04-01 23:34:28 -0700

    brute force for Products of Array Except Self

---
+++ Day 62 238 Products of Array Except Self.py
@@ -0,0 +1,44 @@
+'''Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+
+Each product is guaranteed to fit in a 32-bit integer.
+
+Follow-up: Could you solve it in 
+O
+(
+n
+)
+O(n) time without using the division operation?
+
+Example 1:
+
+Input: nums = [1,2,4,6]
+
+Output: [48,24,12,8]
+Example 2:
+
+Input: nums = [-1,0,1,2,3]
+
+Output: [0,-6,0,0,0]
+Constraints:
+
+2 <= nums.length <= 1000
+-20 <= nums[i] <= 20
+'''
+from typing import List
+#brute force
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n
+        for i in range(n):
+            prod = 1
+            for j in range(n):
+                if i == j:
+                    continue
+                prod *= nums[j]
+            res[i] = prod
+        return res
+#time complexity is O(n^2) and space complexity is O(n)
+
+
+
