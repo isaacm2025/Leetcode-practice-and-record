7a9ad81f11699cb68bcec283cfd2d3b1a9540a7b
Isaac <zihao599@gmail.com>
2026-04-02 22:25:53 -0700

    two pointer

--- Day 63 11 Container With Most Water.py
+++ Day 63 11 Container With Most Water.py
@@ -38,3 +38,18 @@ class Solution:
 #time complexity: O(n^2)
 #space complexity: O(1)
         
+#two pointers
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l, r = 0, len(heights) - 1
+        res = 0
+        while l < r:
+            area = min(heights[l], heights[r]) * (r - l)
+            res = max(res, area)
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+#time complexity: O(n)
+#space complexity: O(1)
