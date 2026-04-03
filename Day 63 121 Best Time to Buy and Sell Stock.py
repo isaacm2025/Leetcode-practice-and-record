191549bd856e72384adb65c87a5bde3511ac2daa
Isaac <zihao599@gmail.com>
2026-04-02 22:46:00 -0700

    dp

--- Day 63 121 Best Time to Buy and Sell Stock.py
+++ Day 63 121 Best Time to Buy and Sell Stock.py
@@ -53,3 +53,14 @@ class Solution:
         return maxProfit
 #time complexity: O(n)
 #space complexity: O(1)
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+        minBuy = prices[0]
+        for sell in prices:
+            maxP = max(maxP, sell - minBuy)
+            minBuy = min(minBuy, sell)
+        return maxP
+#time complexity: O(n)
+#space complexity: O(1)
