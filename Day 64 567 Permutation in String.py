0c0b717faa6fd7650eaa5dfe7bb0fe07dc2c651c
Isaac <zihao599@gmail.com>
2026-04-03 22:49:59 -0700

    567 hash table

--- Day 64 567 Permutation in String.py
+++ Day 64 567 Permutation in String.py
@@ -35,3 +35,24 @@ class Solution:
 #space complexity: O(n) where n is the length of s1
 
                        
+#Hash table
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count1 = {}
+        for c in s1:
+            count1[c] = 1 + count1.get(c, 0)
+        need = len(count1)
+        for i in range(len(s2)):
+            count2, cur = {}, 0
+            for j in range(i, len(s2)):
+                count2[s2[j]] = 1 + count2.get(s2[j], 0)
+                if count2[s2[j]] == count1.get(s2[j], 0):
+                    break
+                if count2[s2[j]] == count1.get(s2[j], 0):
+                    cur += 1
+                if cur == need:
+                    return True
+        return False
+#time complexity: O(n^2) where n is the length of s2
+#space complexity: O(1) since the count dictionaries will have at most 26 entries for lowercase letters 
+
