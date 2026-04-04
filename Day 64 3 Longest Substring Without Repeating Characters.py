2b3d76d0ff2e578d44ca1827dc242465e7b92e3c
Isaac <zihao599@gmail.com>
2026-04-03 22:36:19 -0700

    optimal sliding window

--- Day 64 3 Longest Substring Without Repeating Characters.py
+++ Day 64 3 Longest Substring Without Repeating Characters.py
@@ -34,3 +34,18 @@ class Solution:
         return res
 #time complexity: O(n * m) where n is the length of the string and m is the length of the longest substring without duplicate characters
 #space complexity: O(m) where m is the length of the longest substring without duplicate characters
+
+#optimal sliding window
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mp = {}
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            if s[r] in mp:
+                l = max(mp[s[r]] + 1, l)
+            mp[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
+#time complexity: O(n) where n is the length of the string
+#space complexity: O(m) where m is the length of the longest substring without duplicate characters
