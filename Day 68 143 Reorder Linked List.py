61a9c4b396d81425bd808fe85ffe63aa2e6daba0
Isaac <zihao599@gmail.com>
2026-04-07 23:00:33 -0700

    reverse and merge

--- Day 68 143 Reorder Linked List.py
+++ Day 68 143 Reorder Linked List.py
@@ -84,3 +84,34 @@ class Solution:
         head = rec(head, head.next)
 #time complexity: O(n) where n is the length of the linked list
 #space complexity: O(n) where n is the length of the linked list due to recursion
+
+
+#reverse and merge
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        second = slow.next
+        slow.next = None
+        prev = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next =second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+#time complexity: O(n) where n is the length of the linked list
+#space complexity: O(1)
