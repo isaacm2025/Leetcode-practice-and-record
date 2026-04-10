124d03a5dcd3c121ffa7c2d9422168ab43aa1813
Isaac <zihao599@gmail.com>
2026-04-09 21:58:34 -0700

    DFS 226

--- Day 69 226 Invert Binary Tree.py
+++ Day 69 226 Invert Binary Tree.py
@@ -53,3 +53,19 @@ class Solution:
             
 
 
+#DFS
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: 
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+#time complexity:O(n) where n is the number of nodes in the binary tree.
+#space complexity:O(n) in the worst case, when the binary tree is completely unbalanced and all nodes are on one side, the recursion stack will hold all nodes at once. In the best case, when the binary tree is completely balanced, the recursion stack will hold at most log(n) nodes at once, resulting in O(log(n)) space complexity.
