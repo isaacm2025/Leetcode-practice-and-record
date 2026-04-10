b0922d8f73182aa79a47f55485935682b3c3d9f3
Isaac <zihao599@gmail.com>
2026-04-09 21:48:32 -0700

    Add reverse nodes in k-group solution

---
+++ Day 69 25 Reverse Nodes in K-Group.py
@@ -0,0 +1,92 @@
+'''You are given the head of a singly linked list head and a positive integer k.
+
+You must reverse the first k nodes in the linked list, and then reverse the next k nodes, and so on. If there are fewer than k nodes left, leave the nodes as they are.
+
+Return the modified list after reversing the nodes in each group of k.
+
+You are only allowed to modify the nodes' next pointers, not the values of the nodes.
+
+Example 1:
+
+
+
+Input: head = [1,2,3,4,5,6], k = 3
+
+Output: [3,2,1,6,5,4]
+Example 2:
+
+
+
+Input: head = [1,2,3,4,5], k = 3
+
+Output: [3,2,1,4,5]
+Constraints:
+
+The length of the linked list is n.
+1 <= k <= n <= 100
+0 <= Node.val <= 100'''
+
+
+
+from typing import Optional
+
+
+#recursion
+class ListNode:
+    def __int__(self, val = 0, next = None):
+        self.val =val
+        self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> Optional[ListNode]:
+        cur = head
+        group = 0
+        while cur and group < k:
+            cur = cur.next
+            group += 1
+        if group == k:
+            cur = self.reverseKGroup(cur, k)
+            while group > 0:
+                tmp = head.next
+                head.next = cur
+                cur = head
+                head = tmp
+                group -= 1
+            head = cur
+        return head
+    
+#time complexity:O(n)
+#space complexity:O(n/k) for recursion stack, where n is the number of nodes in the linked list and k is the size of the groups being reversed.
+
+#iteration
+class ListNode:
+    def __int__(self, val = 0, next = None):
+        self.val =val
+        self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                currr = tmp
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
+#time complexity:O(n)
+#space complexity:O(1)
