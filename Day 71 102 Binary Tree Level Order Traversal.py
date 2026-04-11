ddfe49f9908f2d0c737d907ff45d7ff4a71ec641
Isaac <zihao599@gmail.com>
2026-04-10 22:41:25 -0700

    bfs 102

--- Day 71 102 Binary Tree Level Order Traversal.py
+++ Day 71 102 Binary Tree Level Order Traversal.py
@@ -48,3 +48,27 @@ class Solution:
         return res
 #time complexity: O(n) where n is the number of nodes in the tree.
 #space complexity: O(n) in worst case when the tree is skewed, O(logn) in best case when the tree is balanced.
+
+#BFS
+from collections import deque
+import collections
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        q = collections.deque()
+        q.append(root)
+        while q:
+            qLen = len(q)
+            level = []
+            for _ in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
+#time complexity: O(n) where n is the number of nodes in the tree.
+#space complexity: O(n) in worst case when the tree is skewed, O(logn) in best case when the tree is balanced.
+
