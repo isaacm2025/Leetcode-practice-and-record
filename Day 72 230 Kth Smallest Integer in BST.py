01128aeefe8b726f273ab78cd8fca36e11608864
Isaac <zihao599@gmail.com>
2026-04-11 20:50:43 -0700

    230 DFS and iteration

--- Day 72 230 Kth Smallest Integer in BST.py
+++ Day 72 230 Kth Smallest Integer in BST.py
@@ -26,6 +26,9 @@ Constraints:
 '''
 
 #brute force
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val = 0, left = None, right = None):
         self.val = val
@@ -45,3 +48,43 @@ class Solution:
         return arr[k - 1]
 #time complexity: O(nlogn)
 #space complexity: O(n)
+
+
+#DFS
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        cnt = k
+        res = root.val
+        def dfs(node):
+            nonlocal cnt, res
+            if not node:
+                return
+            dfs(node.left)
+            if cnt == 0:
+                return
+            cnt -= 1
+            if cnt == 0:
+                res = node.val
+                return
+            dfs(node.right)
+        dfs(root)
+        return res
+#time complexity: O(n)
+#space complexity: O(n) in worst case, O(logn) in average case
+
+#iterative dfs
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        curr = root
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
+#time complexity: O(n)
+#space complexity: O(n) in worst case, O(logn) in average case
