5fd952638d8f40bf5d2893b77cd6bbedab8df02b
Isaac <zihao599@gmail.com>
2026-04-13 21:27:41 -0700

    optimal backtrack 76

--- Day 74 76 Permutations.py
+++ Day 74 76 Permutations.py
@@ -35,3 +35,22 @@ class Solution:
 #time: O(n^2 * n!)
 #space: O(n * n!)
 
+#optimal backtracking
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.backtrack(nums, 0)
+        return self.res
+    
+
+    def backtrack(self, nums: List[int], idx: int):
+        if idx == len(nums):
+            self.res.append(nums[:])
+            return
+        for i in range(idx, len(nums)):
+            nums[idx], nums[i] = nums[i], nums[idx]
+            self.backtrack(nums, idx + 1)
+            nums[idx], nums[i] = nums[i], nums[idx]
+#time: O(n * n!)
+#space: O(n * n!)
+            
