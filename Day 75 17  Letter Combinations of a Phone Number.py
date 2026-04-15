5c2f1d2f19aabcec50daf5dd59690f6924a45ddb
Isaac <zihao599@gmail.com>
2026-04-14 21:33:22 -0700

    17 iteration

---
+++ Day 75 17  Letter Combinations of a Phone Number.py
@@ -0,0 +1,81 @@
+'''You are given a string digits made up of digits from 2 through 9 inclusive.
+
+Each digit (not including 1) is mapped to a set of characters as shown below:
+
+A digit could represent any one of the characters it maps to.
+
+Return all possible letter combinations that digits could represent. You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: digits = "34"
+
+Output: ["dg","dh","di","eg","eh","ei","fg","fh","fi"]
+Example 2:
+
+Input: digits = ""
+
+Output: []
+Constraints:
+
+0 <= digits.length <= 4
+2 <= digits[i] <= 9'''
+
+from typing import List
+from unicodedata import digit
+
+
+#backtracking
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+        if digits:
+                backtrack(0, "")
+        return res
+#time complexity: O(n * 4^n) where n is the length of the string digits
+#space complexity: O(n) where n is the length of the string digits
+
+
+#iteration
+class Solution:
+     def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        res = [""]
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+         }
+        for digit in digits:
+            tmp = []
+            for curStr in res:
+                for c in digitToChar[digit]:
+                    tmp.append(curStr + c)
+            res = tmp
+        return res
+#time complexity: O(n * 4^n) where n is the length of the string digits
+#space complexity: O(n * 4^n) where n is the length of the string digits
