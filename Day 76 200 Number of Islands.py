025d3a2708dfc60ac74007d4c92ffd4dde3fce26
Isaac <zihao599@gmail.com>
2026-04-15 23:01:40 -0700

    200 bfs

--- Day 76 200 Number of Islands.py
+++ Day 76 200 Number of Islands.py
@@ -26,6 +26,7 @@ Constraints:
 grid[i][j] is '0' or '1'.'''
 
 
+from collections import deque
 from typing import List
 #DFS
 class Solution:
@@ -48,3 +49,32 @@ class Solution:
         return islands
 #time O(m * n) where m and n are the number of rows and columns in the grid
 #space O(m * n) in the worst case when the grid is filled with lands, O(1) in the best case when the grid is filled with water
+
+#BFS
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = "0"
+            q.append((r, c))
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + row, dc + col
+                    if(nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] == "0"):
+                        continue
+                    q.append((nr, nc))
+                    grid[nr[nc]] = "0"
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    bfs(r,c)
+                    islands += 1
+        return islands
+#time O(m * n) where m and n are the number of rows and columns in the grid
+#space O(m, n) in the worst case when the grid is filled with lands, O(1) in the best case when the grid is filled with water
+            
