96128af514229636c1a28a1054eb7ae19b51eb5c
Isaac <zihao599@gmail.com>
2026-04-15 22:24:07 -0700

    211 dfs

--- Day 76 211 Design Add and Search Word Data Structure.py
+++ Day 76 211 Design Add and Search Word Data Structure.py
@@ -58,6 +58,38 @@ class WordDictionary:
 #time O(m * n) for search, O(1) for addWord
 #space O(m * n) for storing the words
 
+#DFS
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
 
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == '.':
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+        return dfs(0, self.root)
+    
             
         
