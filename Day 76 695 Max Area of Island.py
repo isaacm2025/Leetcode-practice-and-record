dfae7c119d3f59806431a330b198d609ac626046
Isaac <zihao599@gmail.com>
2026-04-15 23:13:55 -0700

    695 bfs

--- Day 76 695 Max Area of Island.py
+++ Day 76 695 Max Area of Island.py
@@ -44,3 +44,37 @@ class Solution:
         return area
 #time O(m * n) where m and n are the number of rows and columns in the grid
 #space O(m * n) in the worst case when the grid is filled with lands, O(1) in the best case when the grid is filled with water
+
+
+#BFS
+from collections import deque
+from typing import List
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        area = 0
+
+        def bfs(r, c):
+            q = deque()
+            q.append((r, c))
+            grid[r][c] = 0
+            res = 1
+
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + row, dc + col
+                    if (nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] ==0):
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = 0
+                    res += 1
+            return res
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    area = max(area, bfs(r, c))
+        return area
+#time O(m * n) where m and n are the number of rows and columns in the grid
+#space O(m * n) in the worst case when the grid is filled with lands, O(1) in the best case when the grid is filled with water
