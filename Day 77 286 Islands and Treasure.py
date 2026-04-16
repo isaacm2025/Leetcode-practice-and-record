32ba31f1e06b4c7ba4ee276355a03abdf691a314
Isaac <zihao599@gmail.com>
2026-04-16 13:07:14 -0700

    286 multi bfs

--- Day 77 286 Islands and Treasure.py
+++ Day 77 286 Islands and Treasure.py
@@ -106,3 +106,34 @@ class Solution:
                     grid[r][c] = bfs(r, c)
 #time complexity O(m * n * m * n) where m is the number of rows and n is the number of columns in the grid
 #space complexity O(m * n) where m is the number of rows and n is the number of columns in the grid
+
+#multi source bfs
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+        q = deque()
+
+        def addCell(r, c):
+            if (min(r, c) < 0 or r == ROWS or c == COLS or (r, c) in visit or grid[r][c] == -1):
+                return
+            visit.add((r, c))
+            q.append((r, c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    q.append([r, c])
+                    visit.add((r, c))
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                grid[r][c] = dist
+                addCell(r + 1, c)
+                addCell(r - 1, c)
+                addCell(r, c + 1)
+                addCell(r, c - 1)
+            dist += 1
+#time complexity O(m * n) where m is the number of rows and n is the number of columns in the grid
+#space complexity O(m * n) where m is the number of rows and n is the number of columns in the grid
