b751ec1e6fb3864921a672ab9e381b34d86bcb26
Isaac <zihao599@gmail.com>
2026-04-16 16:54:37 -0700

    BFS 130

--- Day 77 130 Surrounded Regions.py
+++ Day 77 130 Surrounded Regions.py
@@ -28,6 +28,7 @@ Constraints:
 1 <= board.length, board[i].length <= 200
 board[i][j] is 'X' or 'O'.'''
 
+from collections import deque
 from typing import List
 #DFS
 class Solution:
@@ -62,4 +63,34 @@ class Solution:
                     board[r][c] = '0'
 #time complexity O(m * n) where m is the number of rows and n is the number of columns in the grid
 #space complexity O(m * n) where m is the number of rows and n is the number of columns in the grid
-        
+
+#BFS
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        def capture():
+            q = deque()
+            for r in range(ROWS):
+                for c in range(COLS):
+                    if (r == 0 or r == ROWS - 1 or c == 0 or c == COLS - 1) and board[r][c] == '0':
+                        q.append((r, c))
+            while q:
+                r, c = q.popleft()
+                if board[r][c] == "0":
+                    board[r][c] = 'T'
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if 0 <= nr < ROWS and 0 <= nc < COLS:
+                            q.append((nr, nc))
+        capture()
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == '0':
+                    board[r][c] = 'x'
+                elif board[r][c] == 'T':
+                    board[r][c] = '0'
+#time complexity O(m * n) where m is the number of rows and n is the number of columns in the grid
+#space complexity O(m * n) where m is the number of rows and n is the number of columns in the grid
+                    
