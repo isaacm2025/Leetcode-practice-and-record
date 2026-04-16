0973df12013b45f914f5eca96e59dc0218a90f99
Isaac <zihao599@gmail.com>
2026-04-16 13:45:48 -0700

    417 dfs

---
+++ Day 77 417 Pacific Atlantic Water Flow.py
@@ -0,0 +1,59 @@
+'''You are given a rectangular island heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+
+The islands borders the Pacific Ocean from the top and left sides, and borders the Atlantic Ocean from the bottom and right sides.
+
+Water can flow in four directions (up, down, left, or right) from a cell to a neighboring cell with height equal or lower. Water can also flow into the ocean from cells adjacent to the ocean.
+
+Find all cells where water can flow from that cell to both the Pacific and Atlantic oceans. Return it as a 2D list where each element is a list [r, c] representing the row and column of the cell. You may return the answer in any order.
+
+Example 1:
+
+
+
+Input: heights = [
+  [4,2,7,3,4],
+  [7,4,6,4,7],
+  [6,3,5,3,6]
+]
+
+Output: [[0,2],[0,4],[1,0],[1,1],[1,2],[1,3],[1,4],[2,0]]
+Example 2:
+
+Input: heights = [[1],[1]]
+
+Output: [[0,0],[1,0]]
+Constraints:
+
+1 <= heights.length, heights[r].length <= 100
+0 <= heights[r][c] <= 1000'''
+
+from typing import List
+#dfs
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pacific, atlantic = set(), set()
+        def dfs(r, c, visit, prevHeight):
+            if ((r, c) in visit or r < 0 or c < 0 or r == ROWS or c == COLS or heights[r][c] < prevHeight):
+                return
+            visit.add((r, c))
+
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
+
+        for c in range(COLS):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(ROWS - 1, c, atlantic, heights[ROWS - 1][c])
+        for r in range(ROWS):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, COLS - 1, atlantic, heights[r][COLS - 1])
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pacific and (r, c) in atlantic:
+                    res.append([r, c])
+        return res
+#time complexity O(m * n) where m is the number of rows and n is the number of columns in the grid
+#space complexity O(m * n) where m is the number of rows and n is the number of columns in the grid
