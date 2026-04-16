6ab6f682185a9078ad74db0f5432af1e59dedc73
Isaac <zihao599@gmail.com>
2026-04-15 23:28:17 -0700

    bfs 133

--- Day 76 133 Clone Graph.py
+++ Day 76 133 Clone Graph.py
@@ -68,3 +68,29 @@ class Solution:
         return dfs(node) if node else None
 #time O(n + m) where n is the number of nodes and m is the number of edges in the graph
 #space O(n) where n is the number of nodes in the graph
+
+#BFS
+from collections import defaultdict, deque
+from typing import Optional
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        oldToNew = {}
+        q = deque([node])
+        oldToNew[node] = Node(node.val)
+        while q:
+            cur = q.popleft()
+            for nei in cur.neighbors:
+                if nei not in oldToNew:
+                    oldToNew[nei] = Node(nei.val)
+                    q.append(nei)
+                oldToNew[cur].neighbors.append(oldToNew[nei])
+        return oldToNew[node]
+#time O(n + m) where n is the number of nodes and m is the number of edges in the graph
+#space O(n) where n is the number of nodes in the graph
