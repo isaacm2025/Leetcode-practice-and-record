39a3a3477ba87dd0abca953ce8f328fb6ea08dfb
Isaac <zihao599@gmail.com>
2026-04-16 17:48:35 -0700

    210 topological sort

--- Day 77 210 Course Schedule II.py
+++ Day 77 210 Course Schedule II.py
@@ -57,3 +57,28 @@ class Solution:
         return output
 #time complexity: O(E + V) where E is the number of edges and V is the number of vertices in the graph
 #space complexity: O(E + V) where E is the number of edges and V is the number of vertices in the graph
+
+
+#Topological Sort
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = [[] for i in range(numCourses)]
+        indegree = [0] * numCourses
+        for nxt, pre in prerequisites:
+            indegree[nxt] += 1
+            adj[pre].append(nxt)
+        output = []
+
+        def dfs(node):
+            output.append(node)
+            indegree[node] = -1
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    dfs(nei)
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                dfs(i)
+        return output if len(output) == numCourses else[]
+#time complexity: O(E + V) where E is the number of edges and V is the number of vertices in the graph
+#space complexity: O(E + V) where E is the number of edges and V is the number of vertices in the graph
