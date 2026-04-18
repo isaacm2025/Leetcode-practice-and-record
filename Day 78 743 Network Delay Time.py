fb80f818e39af7f7bcf61a8811f880bfb524d338
Isaac <zihao599@gmail.com>
2026-04-17 18:14:53 -0700

    743 bellman ford

--- Day 78 743 Network Delay Time.py
+++ Day 78 743 Network Delay Time.py
@@ -46,3 +46,17 @@ class Solution:
         return res if res < float('inf') else - 1
 #time complexity: O(n + m) where n is the number of nodes and m is the number of edges
 #space complexity: O(n + m) where n is the number of nodes and m is the number of edges
+
+#bellman ford
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        dist = [float('inf')] * n
+        dist[k - 1] = 0
+        for _ in range(n - 1):
+            for u, v, w in times:
+                if dist[u - 1] + w < dist[v - 1]:
+                    dist[v - 1] = dist[u - 1] + w
+        max_dist = max(dist)
+        return max_dist if max_dist < float('inf') else - 1
+#time complexity: O(n * m) where n is the number of nodes and m is the number of edges
+#space complexity: O(n) where n is the number of nodes
