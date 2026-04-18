636229067948f3c6627d521f6be4ccd23d44f6d8
Isaac <zihao599@gmail.com>
2026-04-17 18:52:29 -0700

    optimal prims 1584

--- Day 78 1584 Min Cost to Connect Points.py
+++ Day 78 1584 Min Cost to Connect Points.py
@@ -49,3 +49,28 @@ class Solution:
         return res
 #time complexity: O(n^2 log n) where n is the number of points
 #space complexity: O(n^2) where n is the number of points
+
+#optimal
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n, node = len(points), 0
+        dist = [100000000] * n
+        visit = [False] * n
+        edges, res = 0 , 0
+
+        while edges < n - 1:
+            visit[node] = True
+            nextNode = -1
+            for i in range(n):
+                if  visit[i]:
+                    continue
+                curDist = (abs(points[i][0] - points[node][0]) + abs(points[i][1] - points[node][1]))
+                dist[i] = min(dist[i], curDist)
+                if nextNode == -1 or dist[i] < dist[nextNode]:
+                    nextNode = i
+            res += dist[nextNode]
+            node = nextNode
+            edges += 1
+        return res
+#time complexity: O(n^2) where n is the number of points
+#space complexity: O(n) where n is the number of points
