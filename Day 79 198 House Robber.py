5c1ad9fbefdc32c79d4c3224d9a2c3b5bfa60ac4
Isaac <zihao599@gmail.com>
2026-04-18 22:43:47 -0700

    198 dp

--- Day 79 198 House Robber.py
+++ Day 79 198 House Robber.py
@@ -37,3 +37,18 @@ class Solution:
 #time complexity: O(2^n)
 #space complexity: O(n) due to the recursion stack
 
+
+#dp
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums)
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if memo[i] != -1:
+                return memo[i]
+            memo[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            return memo[i]
+        return dfs(0)
+#time complexity: O(n) where n is the length of the input array nums
+#space complexity: O(n) due to the recursion stack and the memoization array
