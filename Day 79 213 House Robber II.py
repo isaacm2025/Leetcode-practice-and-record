fb3a2501890c1c2930e40ba181c13df7280471c5
Isaac <zihao599@gmail.com>
2026-04-18 22:51:48 -0700

    213 recursion

---
+++ Day 79 213 House Robber II.py
@@ -0,0 +1,38 @@
+'''You are given an integer array nums where nums[i] represents the amount of money the ith house has. The houses are arranged in a circle, i.e. the first house and the last house are neighbors.
+
+You are planning to rob money from the houses, but you cannot rob two adjacent houses because the security system will automatically alert the police if two adjacent houses were both broken into.
+
+Return the maximum amount of money you can rob without alerting the police.
+
+Example 1:
+
+Input: nums = [3,4,3]
+
+Output: 4
+Explanation: You cannot rob nums[0] + nums[2] = 6 because nums[0] and nums[2] are adjacent houses. The maximum you can rob is nums[1] = 4.
+
+Example 2:
+
+Input: nums = [2,9,8,3,6]
+
+Output: 15
+Explanation: You cannot rob nums[0] + nums[2] + nums[4] = 16 because nums[0] and nums[4] are adjacent houses. The maximum you can rob is nums[1] + nums[4] = 15.
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 200'''
+from typing import List
+
+#recurisive
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        def dfs(i, flag):
+            if i >= len(nums) or (flag and i == len(nums) - 1):
+                return 0
+            return max(dfs(i + 1, flag), nums[i] + dfs(i + 2, flag or i == 0))
+        return max(dfs(0, False), dfs(1, False))
+#time complexity: O(2^n)
+#space complexity: O(n) due to the recursion stack
