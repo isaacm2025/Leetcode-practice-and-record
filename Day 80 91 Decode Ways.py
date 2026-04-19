adf9732d9d0e7bffcf437c60f6043d011cc3c3d7
Isaac <zihao599@gmail.com>
2026-04-19 11:24:35 -0700

    dp space optimized 91

--- Day 80 91 Decode Ways.py
+++ Day 80 91 Decode Ways.py
@@ -48,3 +48,20 @@ class Solution:
         return dfs(0)
 #time complexity: O(2^n) in the worst case
 #space complexity: O(n) for the recursion stack
+
+#dp space optimized
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = dp2 = 0
+        dp1 = 1
+        for i in range(len(s) -1, -1, -1):
+            if s[i] == '0':
+                dp = 0
+            else:
+                dp = dp1
+            if i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"):
+                dp += dp2
+            dp, dp1, dp2 = 0, dp, dp1
+        return dp1
+#time complexity: O(n)
+#space complexity: O(1)
