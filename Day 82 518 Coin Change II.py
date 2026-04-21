9387c4d7c8e70c5d7c0da252516946a10b23cf1e
Isaac <zihao599@gmail.com>
2026-04-21 01:05:40 -0700

    space optimized 518

--- Day 82 518 Coin Change II.py
+++ Day 82 518 Coin Change II.py
@@ -62,4 +62,21 @@ class Solution:
         return dp[0][amount]
 #time complexity: O(n * a) where n is the number of coins and a is the amount
 #space complexity: O(n * a) where n is the number of coins and a is the amount
+
+#space optimized dp
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+            for a in range(1, amount + 1):
+                nextDP[a] = dp[a]
+                if a - coins[i] >= 0:
+                    nextDP[a] += nextDP[a - coins[i]]
+            dp = nextDP
+        return dp[amount]
+#time complexity: O(n * a) where n is the number of coins and a is the amount
+#space complexity: O(a) where a is the amount
     
