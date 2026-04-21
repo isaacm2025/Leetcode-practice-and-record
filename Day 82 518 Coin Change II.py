3828a2d21456a8ad9c27f4da06f4c020c145d081
Isaac <zihao599@gmail.com>
2026-04-21 01:00:31 -0700

    dp 518

--- Day 82 518 Coin Change II.py
+++ Day 82 518 Coin Change II.py
@@ -45,4 +45,21 @@ class Solution:
         return dfs(0, amount)
 #time complexity: O(2^(max(n, a/m)) where n is the number of coins, a is the amount and m is the smallest coin denomination
 #space complexity: O(max(n, a/m)) where n is the number of coins, a is the amount and m is the smallest coin denomination
+
+#dp
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        coins.sort()
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
+        for i in range(n + 1):
+            dp[i][0] = 1
+        for i in range(n - 1, -1, -1):
+            for a in range(1, amount + 1):
+                if a >= coins[i]:
+                    dp[i][a] = dp[i + 1][a]
+                    dp[i][a] += dp[i][a - coins[i]]
+        return dp[0][amount]
+#time complexity: O(n * a) where n is the number of coins and a is the amount
+#space complexity: O(n * a) where n is the number of coins and a is the amount
     
