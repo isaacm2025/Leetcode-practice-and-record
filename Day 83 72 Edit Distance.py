68cb7be7e14ea723d33f42337690f094f97dae74
Isaac <zihao599@gmail.com>
2026-04-22 15:27:01 -0700

    dp optimized 72

--- Day 83 72 Edit Distance.py
+++ Day 83 72 Edit Distance.py
@@ -49,3 +49,24 @@ class Solution:
 #time complexity: O(3^(m+n))
 #space complexity: O(m+n)
 
+#dp (optimal)
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        if m < n:
+            m, n = n, m
+            word1, word2 = word2, word1
+        dp = [n - i for i in range(n + 1)]
+        for i in range(m - 1, -1, -1):
+            nextDp = dp[n]
+            dp[n] = m - i
+            for j in range(n - 1, -1, -1):
+                temp = dp[j]
+                if word1[i] == word2[j]:
+                    dp[j] = nextDp
+                else:
+                    dp[j] = 1 + min(dp[j], dp[j + 1], nextDp)
+                nexDp = temp
+        return dp[0]
+#time complexity: O(m*n)
+#space complexity: O(n)
