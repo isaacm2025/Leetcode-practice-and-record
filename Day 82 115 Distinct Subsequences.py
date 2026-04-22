0c1fa5cf0ec90bb55451e3f039b85d5814da1423
Isaac <zihao599@gmail.com>
2026-04-22 14:54:00 -0700

    dp 115

--- Day 82 115 Distinct Subsequences.py
+++ Day 82 115 Distinct Subsequences.py
@@ -47,3 +47,20 @@ class Solution:
         return dfs(0, 0)
 #time complexity: O(2^n)
 #space complexity: O(n)
+
+
+#dp
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = 1
+        for i in range(m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i + 1][j]
+                if s[i] == t[j]:
+                    dp[i][j] += dp[i + 1][j + 1]
+        return dp[0][0]
+#time complexity: O(m*n)
+#space complexity: O(m*n)
