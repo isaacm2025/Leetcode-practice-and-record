d1bc7c989dedc7a36e0abadc36d43a93bde2e74c
Isaac <zihao599@gmail.com>
2026-04-22 16:22:08 -0700

    10 dp

--- Day 83 10 Regular Expression Matching.py
+++ Day 83 10 Regular Expression Matching.py
@@ -47,3 +47,21 @@ class Solution:
         return dfs(0, 0)
 #time complexity: O(2^(m + n))
 #space complexity: O(m + n)
+
+#dp
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+        dp[len(s)][len(p)] = True
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) -1, -1, -1):
+                match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+                if (j + 1) < len(p) and p[j + 1] == "*":
+                    dp[i][j] = dp[i][j + 2]
+                    if match:
+                        dp[i][j] = dp[i + 1][j] or dp[i][j]
+                elif match:
+                    dp[i][j] = dp[i + 1][j + 1]
+        return dp[0][0]
+#time complexity: O(m * n)
+#space complexity: O(m * n)
