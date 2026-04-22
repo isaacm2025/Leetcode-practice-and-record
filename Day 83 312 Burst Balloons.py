09cb37adfaf42ffacbb616a190b45aff1888e23a
Isaac <zihao599@gmail.com>
2026-04-22 15:56:39 -0700

    dp 312

--- Day 83 312 Burst Balloons.py
+++ Day 83 312 Burst Balloons.py
@@ -37,3 +37,19 @@ class Solution:
         return dfs(nums)
 #time complexity: O(n * 2^n)
 #space complexity: O(n * 2^n)
+
+#dp
+class Solution:
+    def maxCoins(self, nums):
+        n = len(nums)
+        nums = [1] + nums + [1]
+        dp = [[0] * (n + 2) for _ in range(n + 2)]
+        for l in range(n, 0, -1):
+            for r in range(l, n + 1):
+                for i in range(l, r + 1):
+                    coins = nums[l - 1] * nums[i] * nums[r + 1]
+                    coins += dp[l][i - 1] + dp[i + 1][r]
+                    dp[l][r] = max(dp[l][r], coins)
+        return dp[1][n]
+#time complexity: O(n^3)
+#space complexity: O(n^2)
