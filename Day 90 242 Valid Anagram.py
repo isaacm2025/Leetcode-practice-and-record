ebc34dfad1dd1c13777d487f1bce2841ddfe7155
Isaac <zihao599@gmail.com>
2026-04-29 22:10:57 -0700

    hashmap 242

--- Day 90 242 Valid Anagram.py
+++ Day 90 242 Valid Anagram.py
@@ -25,3 +25,18 @@ class Solution:
         return sorted(s) == sorted(t)
 #time complexity: O(nlogn + mlogm) where n and m are the lengths of s and t respectively
 #space complexity: O(n + m) for the sorted lists or O(1) if we consider the sorting in place
+
+#hash map
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+#time complexity: O(n + m) where n and m are the lengths of s and t respectively
+#space complexity: O(1) since the hash maps will at most contain 26 key-value pairs for lowercase English letters
+
