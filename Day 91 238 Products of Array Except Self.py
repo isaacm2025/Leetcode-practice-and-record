02517f2e89e9fc3d94b8e26719c9473f03c5e39d
Isaac <zihao599@gmail.com>
2026-04-30 21:44:04 -0700

    division 238

--- Day 91 238 Products of Array Except Self.py
+++ Day 91 238 Products of Array Except Self.py
@@ -40,3 +40,24 @@ class Solution:
         return res
 #time: O(n^2)
 #space: O(n), O(1) if we don't count the output array
+
+#division
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod, zero_cnt = 1, 0
+        for num in nums:
+            if num:
+                prod *= num
+            else:
+                zero_cnt += 1
+        if zero_cnt > 1: return [0] * len(nums)
+        res = [0] * len(nums)
+        for i, c in enumerate(nums):
+            if zero_cnt == 1:
+                res[i] = 0 if c else prod
+            else:
+                res[i] = prod // c
+        return res
+#time: O(n)
+#space: O(n), O(1) if we don't count the output array
