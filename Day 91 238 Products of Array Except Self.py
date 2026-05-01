8ae3b30970fabe4cc087e08bd531b80f4854d192
Isaac <zihao599@gmail.com>
2026-04-30 21:46:41 -0700

    238 prefix and suffix

--- Day 91 238 Products of Array Except Self.py
+++ Day 91 238 Products of Array Except Self.py
@@ -61,3 +61,20 @@ class Solution:
         return res
 #time: O(n)
 #space: O(n), O(1) if we don't count the output array
+
+#prefix and suffix optimal
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] *= prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+#time: O(n)
+#space: O(n), O(1) if we don't count the output array
