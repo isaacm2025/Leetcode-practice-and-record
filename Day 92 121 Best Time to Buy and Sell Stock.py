c6b825d5b0a5bec24645bb6ae8253e1c7b48932a
Isaac <zihao599@gmail.com>
2026-05-01 23:14:52 -0700

    two pointer 121

--- Day 92 121 Best Time to Buy and Sell Stock.py
+++ Day 92 121 Best Time to Buy and Sell Stock.py
@@ -41,3 +41,15 @@ class Solution:
 #two pointers
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
+#time: O(n)
+#space: O(1)
