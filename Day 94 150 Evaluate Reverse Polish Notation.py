3519ba37e1a68bd95844e5db857fd36d2df45fd2
Isaac <zihao599@gmail.com>
2026-05-03 13:00:12 -0700

    recurssion 150

--- Day 94 150 Evaluate Reverse Polish Notation.py
+++ Day 94 150 Evaluate Reverse Polish Notation.py
@@ -39,3 +39,24 @@ class Solution:
         return int(tokens[0])
 #time complexity: O(n^2) because we are iterating through the tokens list multiple times
 #space complexity: O(n) because we are creating a new list of tokens each time
+
+#recursive
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        def dfs():
+            token = tokens.pop()
+            if token not in "+-*/":
+                return int(token)
+            right = dfs()
+            left = dfs()
+            if token =='+':
+                return left + right
+            elif token == '-':
+                return left - right
+            elif token == '*':
+                return left * right
+            elif token == '/':
+                return int(left / right)
+        return dfs()
+#time complexity: O(n) because we are iterating through the tokens list once
+#space complexity: O(n) because we are using a recursive stack
