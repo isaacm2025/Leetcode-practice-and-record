9dabb38d8e842dd388ee1f414bba3aa01be0fcde
Isaac <zihao599@gmail.com>
2026-05-03 13:39:50 -0700

    84 stack

--- Day 94 84 Largest Rectangle In Histogram.py
+++ Day 94 84 Largest Rectangle In Histogram.py
@@ -39,3 +39,19 @@ class Solution:
         return maxArea
 #time complexity: O(n^2)
 #space complexity: O(1)
+
+#stack optimal
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        maxArea = 0
+        stack = []
+        for i in range(n + 1):
+            while stack and (i == n or heights[stack[-1]] >= heights[i]):
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                maxArea = max(maxArea, height * width)
+            stack.append(i)
+        return maxArea
+#time complexity: O(n)
+#space complexity: O(n)
