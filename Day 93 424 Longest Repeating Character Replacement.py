706554439c82c361ae4a6ca232fece97bcd26778
Isaac <zihao599@gmail.com>
2026-05-02 22:47:36 -0700

    optimal 424

--- Day 93 424 Longest Repeating Character Replacement.py
+++ Day 93 424 Longest Repeating Character Replacement.py
@@ -53,3 +53,22 @@ class Solution:
         return res
 #time complexity: O(n*m) where m is the number of distinct characters in the string
 #space complexity: O(m) where m is the number of distinct characters in the string
+
+
+#optimized sliding window
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        count = {}
+        l = 0
+        maxf = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+            while (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
+#time complexity: O(n)
+#space complexity: O(m) where m is the number of distinct characters in the string
