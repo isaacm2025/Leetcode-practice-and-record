d5d8f90ab5ee3f35b8e981a463dced753cc37c84
Isaac <zihao599@gmail.com>
2026-05-04 12:15:22 -0700

    iterative bs 704

--- Day 95 704 Binary Search.py
+++ Day 95 704 Binary Search.py
@@ -46,3 +46,19 @@ class Solution:
         return self.binary_search(0, len(nums) - 1, nums, target)
 #time complexity: O(log n)
 #space complxity: O(log n)
+
+#iterative
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = l + ((r - l) // 2)
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+#time complexity: O(log n)
+#space complexity: O(1)
