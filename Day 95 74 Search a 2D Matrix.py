8d96198f69e28184eff711782ba1d4db0c80e94c
Isaac <zihao599@gmail.com>
2026-05-04 13:21:26 -0700

    bs 74

---
+++ Day 95 74 Search a 2D Matrix.py
@@ -0,0 +1,59 @@
+'''You are given an m x n 2-D integer array matrix and an integer target.
+
+Each row in matrix is sorted in non-decreasing order.
+The first integer of every row is greater than the last integer of the previous row.
+Return true if target exists within matrix or false otherwise.
+
+Can you write a solution that runs in O(log(m * n)) time?
+
+Example 1:
+
+
+
+Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
+
+Output: true
+Example 2:
+
+
+
+Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 15
+
+Output: false
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-10000 <= matrix[i][j], target <= 10000'''
+
+#bf
+from ast import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == target:
+                    return True
+        return False
+#time complexity: O(m * n)
+#space complexity: O(1)
+
+#binary search(one pass)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        l, r = 0, ROWS * COLS - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            row, col = m // COLS, m % COLS
+            if target > matrix[row][col]:
+                l = m + 1
+            elif target < matrix[row][col]:
+                r = m - 1
+            else:
+                return True
+        return False
+#time complexity: O(log(m * n))
+#space complexity: O(1)
+
