4aa9f0825134368670851e6cdecd2d6750fafd4b
Isaac <zihao599@gmail.com>
2026-05-04 13:56:50 -0700

    bs one pass 33

--- Day 95 33 Search in Rotated Sorted Array.py
+++ Day 95 33 Search in Rotated Sorted Array.py
@@ -65,3 +65,25 @@ class Solution:
         return binary_search(pivot, len(nums) - 1)
 #time complexity: O(log n)
 #space complexity: O(1)
+
+#bs (one pass)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target == nums[mid]:
+                return mid
+            if nums[left] <= nums[mid]:
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
+#time complexity: O(log n)
+#space complexity: O(1)
