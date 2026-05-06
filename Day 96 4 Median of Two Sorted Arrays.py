88d462c7cdea31f86eec1facfb98dff7d335e91b
Isaac <zihao599@gmail.com>
2026-05-05 23:45:08 -0700

    bf 4

---
+++ Day 96 4 Median of Two Sorted Arrays.py
@@ -0,0 +1,53 @@
+'''You are given two integer arrays nums1 and nums2 of size m and n respectively, where each is sorted in ascending order. Return the median value among all elements of the two arrays.
+
+Your solution must run in 
+O
+(
+l
+o
+g
+(
+m
++
+n
+)
+)
+O(log(m+n)) time.
+
+Example 1:
+
+Input: nums1 = [1,2], nums2 = [3]
+
+Output: 2.0
+Explanation: Among [1, 2, 3] the median is 2.
+
+Example 2:
+
+Input: nums1 = [1,3], nums2 = [2,4]
+
+Output: 2.5
+Explanation: Among [1, 2, 3, 4] the median is (2 + 3) / 2 = 2.5.
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+-10^6 <= nums1[i], nums2[i] <= 10^6'''
+
+#bf
+from typing import List
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        len1, len2 = len(nums1), len(nums2)
+        merged = nums1 + nums2
+        merged.sort()
+
+        totalLen = len(merged)
+        if totalLen % 2 == 0:
+            return (merged[totalLen // 2 - 1] + merged[totalLen // 2]) / 2.0
+        else:
+            return merged[totalLen // 2]
+#time complexity O((m+n)log(m+n)) where m and n are the lengths of nums1 and nums2 respectively
+#space complexity O(m+n) where m and n are the lengths of nums1 and nums
