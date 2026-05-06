84ccd9304cf192663b4d1ac4a2918b47b33f57c3
Isaac <zihao599@gmail.com>
2026-05-05 23:59:41 -0700

    iteration 21

--- Day 96 21 Merge Two Sorted Linked Lists.py
+++ Day 96 21 Merge Two Sorted Linked Lists.py
@@ -47,3 +47,28 @@ class Solution:
             return list2
 #time complexity O(m+n) where m and n are the number of nodes in list1 and list2 respectively
 #space complexity O(m+n) where m and n are the number of nodes in list1 and list2 respectively due to the recursive call stack
+
+#iterative
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val= val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+        return dummy.next
+#time complexity O(m+n) where m and n are the number of nodes in list1 and list2 respectively
+#space complexity O(1)
