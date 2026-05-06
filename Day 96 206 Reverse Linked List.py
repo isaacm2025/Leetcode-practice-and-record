db8912cae5f347e72682a69b5abbe41ced1d8ec6
Isaac <zihao599@gmail.com>
2026-05-05 23:54:28 -0700

    recursion 206

---
+++ Day 96 206 Reverse Linked List.py
@@ -0,0 +1,37 @@
+'''Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+Example 1:
+
+Input: head = [0,1,2,3]
+
+Output: [3,2,1,0]
+Example 2:
+
+Input: head = []
+
+Output: []
+Constraints:
+
+0 <= The length of the list <= 1000.
+-1000 <= Node.val <= 1000'''
+
+# Definition for singly-linked list.
+from typing import Optional
+#recursive
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val= val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+        return newHead
+#time complexity O(n) where n is the number of nodes in the linked list
+#space complexity O(n) where n is the number of nodes in the linked list due to the recursive call stack
+
