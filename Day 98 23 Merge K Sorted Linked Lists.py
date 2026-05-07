7c160377057d953bbc6fb5981cb49bfd90b77d45
Isaac <zihao599@gmail.com>
2026-05-07 10:23:37 -0700

    iteration 23

--- Day 98 23 Merge K Sorted Linked Lists.py
+++ Day 98 23 Merge K Sorted Linked Lists.py
@@ -47,3 +47,30 @@ class Solution:
         return res.next
 #time complexity: O(N log N) where N is the total number of nodes across all linked lists, due to the sorting step.
 #space complexity: O(N) for storing the values of all nodes in the list before sorting.
+
+#iteration
+from typing import Optional
+from typing import List
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        res = ListNode()
+        cur = res
+        while True:
+            minNode = -1
+            for i in range(len(lists)):
+                if not lists[i]:
+                    continue
+                if minNode == -1 or lists[minNode].val > lists[i].val:
+                    minNode = i
+            if minNode == -1:
+                break
+            cur.next = lists[minNode]
+            lists[minNode] = lists[minNode].next
+            cur = cur.next
+        return res.next
+#time complexity: O(N * k) where N is the total number of nodes across all linked lists and k is the number of linked lists, due to the nested loop.
+#space complexity: O(1) for the iterative approach, as we are not using any additional data structures to store the nodes.
