a3e979ff68d47e32b464277c34a09ca9c4f9714c
Isaac <zihao599@gmail.com>
2026-05-06 21:24:38 -0700

    recursion 143

--- Day 97 143 Reorder Linked List.py
+++ Day 97 143 Reorder Linked List.py
@@ -57,3 +57,30 @@ class Solution:
         nodes[i].next = None
 #time complexity: O(n)
 #space complexity: O(n)
+
+#recursive
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        def rec(root: ListNode, cur: ListNode) -> ListNode:
+            if not cur:
+                return root
+            root = rec(root, cur.next)
+            if not root:
+                return None
+            tmp = None
+            if root == cur or root.next == cur:
+                cur.next = None
+            else:
+                tmp = root.next
+                root.next = cur
+                cur.next = tmp
+            return tmp
+        head = rec(head, head.next)
+#time complexity: O(n)
+#space complexity: O(n)
