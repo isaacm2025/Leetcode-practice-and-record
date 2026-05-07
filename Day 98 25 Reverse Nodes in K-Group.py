ce8a3202793986ef171cf293254695a2b61675b2
Isaac <zihao599@gmail.com>
2026-05-07 10:35:24 -0700

    iteration 25

--- Day 98 25 Reverse Nodes in K-Group.py
+++ Day 98 25 Reverse Nodes in K-Group.py
@@ -51,3 +51,36 @@ class Solution:
         return head
 #time complexity: O(N) where N is the number of nodes in the linked list, since we visit each node at most once.
 #space complexity: O(N/k) for the recursive call stack, where N is the total number of nodes and k is the size of each group.
+
+#iteration
+from typing import Optional
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+            prev, cur = kth.next, groupPrev.next
+            while cur != groupNext:
+                tmp = cur.next
+                cur.next = prev
+                prev = cur
+                cur = tmp
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+    def getKth(self, cur, k):
+        while cur and k>0:
+            cur = cur.next
+            k -= 1
+        return cur
+#time complexity: O(N) where N is the number of nodes in the linked list, since we visit each node at most once.
+#space complexity: O(1) for the iterative approach, as we are only using a constant amount of extra space for pointers.
