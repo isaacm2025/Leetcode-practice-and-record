a43d27b340375e8785a306d3c626221fb0e11950
Isaac <zihao599@gmail.com>
2026-05-06 21:36:09 -0700

    iteration 19

--- Day 97 19 Remove Node From End of Linked List.py
+++ Day 97 19 Remove Node From End of Linked List.py
@@ -45,3 +45,30 @@ class Solution:
         return head
 #time complexity: O(n)
 #space complexity: O(n)
+
+#iteration
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        N = 0
+        cur = head
+        while cur:
+            N += 1
+            cur = cur.next
+        removeIndex = N - n
+        if removeIndex == 0:
+            return head.next
+        cur = head
+        for i in range(N - 1):
+            if (i + 1) == removeIndex:
+                cur.next = cur.next.next
+                break
+            cur = cur.next
+        return head
+#time complexity: O(N)
+#space complexity: O(1)
