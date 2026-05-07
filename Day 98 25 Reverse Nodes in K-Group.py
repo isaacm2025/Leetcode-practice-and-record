04456507f3496ee9f4ee599c0bcbd247fcec329e
Isaac <zihao599@gmail.com>
2026-05-07 10:30:56 -0700

    25 recursion

---
+++ Day 98 25 Reverse Nodes in K-Group.py
@@ -0,0 +1,53 @@
+'''You are given the head of a singly linked list head and a positive integer k.
+
+You must reverse the first k nodes in the linked list, and then reverse the next k nodes, and so on. If there are fewer than k nodes left, leave the nodes as they are.
+
+Return the modified list after reversing the nodes in each group of k.
+
+You are only allowed to modify the nodes' next pointers, not the values of the nodes.
+
+Example 1:
+
+
+
+Input: head = [1,2,3,4,5,6], k = 3
+
+Output: [3,2,1,6,5,4]
+Example 2:
+
+
+
+Input: head = [1,2,3,4,5], k = 3
+
+Output: [3,2,1,4,5]
+Constraints:
+
+The length of the linked list is n.
+1 <= k <= n <= 100
+0 <= Node.val <= 100'''
+
+#recursion
+from typing import Optional
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        cur = head
+        group = 0
+        while cur and group < k:
+            cur = cur.next
+            group += 1
+        if group == k:
+            cur = self.reverseKGroup(cur, k)
+            while group > 0:
+                tmp = head.next
+                head.next = cur
+                cur = head
+                head = tmp
+                group -= 1
+            head = cur
+        return head
+#time complexity: O(N) where N is the number of nodes in the linked list, since we visit each node at most once.
+#space complexity: O(N/k) for the recursive call stack, where N is the total number of nodes and k is the size of each group.
