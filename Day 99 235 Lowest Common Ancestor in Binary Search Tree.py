1c1d348c381e869f622cf791114562e520cc7b59
Isaac <zihao599@gmail.com>
2026-05-08 20:59:39 -0700

    235 iteration

--- Day 99 235 Lowest Common Ancestor in Binary Search Tree.py
+++ Day 99 235 Lowest Common Ancestor in Binary Search Tree.py
@@ -43,3 +43,18 @@ class Solution:
             return root
 #time complexity: O(h) where h is the height of the tree. In the worst case, the tree is skewed and the height is equal to the number of nodes in the tree, resulting in O(n) time complexity. In the best case, the tree is balanced and the height is log(n), resulting in O(log n) time complexity.
 #space complexity: O(h) in the worst case when the tree is skewed, and O(log n) in the best case when the tree is balanced. This is because the maximum depth of the recursion will be equal to the height of the tree, which can be O(n) in the worst case and O(log n) in the best case.
+
+
+#iteration
+class Solution:
+    def lowestCommonAncestor(self, root: Optional[TreeNode], p: Optional[TreeNode], q: TreeNode) -> TreeNode:
+        cur = root
+        while cur:
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            else:
+                return cur
+#time complexity: O(h) where h is the height of the tree. In the worst case, the tree is skewed and the height is equal to the number of nodes in the tree, resulting in O(n) time complexity. In the best case, the tree is balanced and the height is log(n), resulting in O(log n) time complexity.
+#space complexity: O(1) because we are using a constant amount of space to store the current node and the input parameters. We are not using any additional data structures that grow with the size of the input, and we are not using recursion, so the space complexity is constant.
