430161f29fab9b46c99e13a53fd24eff9206071f
Isaac <zihao599@gmail.com>
2026-05-09 23:16:35 -0700

    2021 dfs

---
+++ Day 100 2021 Count Good Nodes in Binary Tree.py
@@ -0,0 +1,43 @@
+'''Within a binary tree, a node x is considered good if the path from the root of the tree to the node x contains no nodes with a value greater than the value of node x
+
+Given the root of a binary tree root, return the number of good nodes within the tree.
+
+Example 1:
+
+
+
+Input: root = [2,1,1,3,null,1,5]
+
+Output: 3
+Example 2:
+
+Input: root = [1,2,-1,3,4]
+
+Output: 4
+Constraints:
+
+1 <= number of nodes in the tree <= 100
+-100 <= Node.val <= 100
+'''
+
+#dfs
+# Definition for a binary tree node.
+from typing import List, Optional
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+        return dfs(root, root.val)
+#time complexity: O(n)
+#space complexity: O(n) in worst case when the tree is skewed, O(log n) in best case when the tree is balanced.
