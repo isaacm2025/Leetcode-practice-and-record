28e714364e53d8cf7b34af5f271d45195a1c719e
Isaac <zihao599@gmail.com>
2026-05-09 23:07:47 -0700

    dfs 199

---
+++ Day 100 199 Binary Tree Right Side View.py
@@ -0,0 +1,56 @@
+'''You are given the root of a binary tree. Return only the values of the nodes that are visible from the right side of the tree, ordered from top to bottom.
+
+Example 1:
+
+
+
+Input: root = [1,2,3,null,4,null,5]
+
+Output: [1,3,5]
+
+Example 2:
+
+
+
+Input: root = [1,2,3,4,null,null,null,5]
+
+Output: [1,3,4,5]
+Example 3:
+
+Input: root = [1,null,2]
+
+Output: [1,2]
+Example 4:
+
+Input: root = []
+
+Output: []
+
+Constraints:
+
+0 <= number of nodes in the tree <= 100
+-100 <= Node.val <= 100
+
+'''
+#dfs
+# Definition for a binary tree node.
+from typing import List, Optional
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(node, level):
+            if not node:
+                return None
+            if level == len(res):
+                res.append(node.val)
+            dfs(node.right, level + 1)
+            dfs(node.left, level + 1)
+        dfs(root, 0)
+        return res
+#time complexity: O(n)
+#space complexity: O(n) in worst case when the tree is skewed, O(log n) in best case when the tree is balanced.
