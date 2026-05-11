cb339e1d45c47c344873ad2d4df539bca5b301c6
Isaac <zihao599@gmail.com>
2026-05-10 22:46:37 -0700

    min heap 703

--- Day 101 703 Kth Largest Element in a Stream.py
+++ Day 101 703 Kth Largest Element in a Stream.py
@@ -39,3 +39,19 @@ class KthLargest:
         return self.arr[len(self.arr) - self.k]
 #time complexity: O(m * nlogn)
 #space complexity: O(n)
+
+#min heap
+import heapq
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+#time complexity: O(m * logn)
+#space complexity: O(n)
