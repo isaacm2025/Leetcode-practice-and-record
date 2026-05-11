ddb2edf17d1e1422d30fa57716cbdc3e2389b81a
Isaac <zihao599@gmail.com>
2026-05-10 22:18:39 -0700

    dfs 124

--- Day 101 124 Binary Tree Maximum Path Sum.py
+++ Day 101 124 Binary Tree Maximum Path Sum.py
@@ -59,3 +59,21 @@ class Solution:
         return max(path, 0)
 #time complexity: O(n^2)
 #space complexity: O(n)
+
+#dfs optimal
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+        def dfs(root):
+            if not root:
+                return 0
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+            return root.val + max(leftMax, rightMax)
+        dfs(root)
+        return res[0]
+#time complexity: O(n)
+#space complexity: O(n)
