37450aa8ee21fb5a4aac1a59b40657db0e5e5064
Isaac <zihao599@gmail.com>
2026-05-10 22:15:44 -0700

    dfs 124

---
+++ Day 101 124 Binary Tree Maximum Path Sum.py
@@ -0,0 +1,61 @@
+'''Given the root of a non-empty binary tree, return the maximum path sum of any non-empty path.
+
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes has an edge connecting them. A node can not appear in the sequence more than once. The path does not necessarily need to include the root.
+
+The path sum of a path is the sum of the node's values in the path.
+
+Example 1:
+
+
+
+Input: root = [1,2,3]
+
+Output: 6
+Explanation: The path is 2 -> 1 -> 3 with a sum of 2 + 1 + 3 = 6.
+
+Example 2:
+
+
+
+Input: root = [-15,10,20,null,null,15,5,-5]
+
+Output: 40
+Explanation: The path is 15 -> 20 -> 5 with a sum of 15 + 20 + 5 = 40.
+
+Constraints:
+
+1 <= The number of nodes in the tree <= 1000.
+-1000 <= Node.val <= 1000
+'''
+
+# Definition for a binary tree node.
+#dfs
+from typing import Optional
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = -float('inf')
+        def dfs(root):
+            nonlocal res
+            if not root:
+                return
+            left = self.getMax(root.left)
+            right = self.getMax(root.right)
+            res = max(res, left + right + root.val)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return res
+    def getMax(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left = self.getMax(root.left)
+        right = self.getMax(root.right)
+        path = root.val + max(left, right)
+        return max(path, 0)
+#time complexity: O(n^2)
+#space complexity: O(n)
