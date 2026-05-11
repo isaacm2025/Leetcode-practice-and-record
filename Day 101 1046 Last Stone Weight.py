2cef3654d2dacbca7beb1966ec164dd002fae3ff
Isaac <zihao599@gmail.com>
2026-05-10 22:59:11 -0700

    bs 1046

--- Day 101 1046 Last Stone Weight.py
+++ Day 101 1046 Last Stone Weight.py
@@ -41,3 +41,30 @@ class Solution:
         return stones[0] if stones else 0
 #time complexity: O(n^2logn)
 #space complexity: O(1) or O(n) depending on the sorting algorithm 
+
+#bs
+import heapq
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones.sort()
+        n = len(stones)
+        while n > 1:
+            cur = stones.pop() - stones.pop()
+            n -= 2
+            if cur > 0:
+                l, r = 0, n
+                while l < r:
+                    mid = (l + r) // 2
+                    if stones[mid] < cur:
+                        l = mid + 1
+                    else:
+                        r = mid
+                pos = l
+                n += 1
+                stones.append(0)
+                for i in range(n - 1, pos, -1):
+                    stones[i] = stones[i - 1]
+                stones[pos] = cur
+        return stones[0] if n > 0 else 0
+#time complexity: O(n^2)
+#space complexity: O(1) or O(n) depending on the sorting algorithm
