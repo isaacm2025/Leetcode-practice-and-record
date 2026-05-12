9a37b9dac66ec33859f1d42e7923911e74c6f7af
Isaac <zihao599@gmail.com>
2026-05-11 23:20:10 -0700

    bf 621

---
+++ Day 102 621 Task Scheduler.py
@@ -0,0 +1,60 @@
+'''You are given an array of CPU tasks tasks, where tasks[i] is an uppercase english character from A to Z. You are also given an integer n.
+
+Each CPU cycle allows the completion of a single task, and tasks may be completed in any order.
+
+The only constraint is that identical tasks must be separated by at least n CPU cycles, to cooldown the CPU.
+
+Return the minimum number of CPU cycles required to complete all tasks.
+
+Example 1:
+
+Input: tasks = ["X","X","Y","Y"], n = 2
+
+Output: 5
+Explanation: A possible sequence is: X -> Y -> idle -> X -> Y.
+
+Example 2:
+
+Input: tasks = ["A","A","A","B","C"], n = 3
+
+Output: 9
+Explanation: A possible sequence is: A -> B -> C -> Idle -> A -> Idle -> Idle -> Idle -> A.
+
+Constraints:
+
+1 <= tasks.length <= 1000
+0 <= n <= 100
+'''
+
+#bf
+
+from ast import List
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = [0] * 26
+        for task in tasks:
+            count[ord(task) - ord('A')] += 1
+        arr = []
+        for i in range(26):
+            if count[i] > 0:
+                arr.append([count[i], i])
+        time = 0
+        processed = []
+        while arr:
+            maxi = -1
+            for i in range(len(arr)):
+                if all(processed[j] != arr[i][1] for j in range(max(0, time - n), time)):
+                    if maxi == -1 or arr[maxi][0] < arr[i][0]:
+                        maxi = i
+            time += 1
+            cur = -1
+            if maxi != -1:
+                cur = arr[maxi][1]
+                arr[maxi][0] -= 1
+                if arr[maxi][0] == 0:
+                    arr.pop(maxi)
+            processed.append(cur)
+        return time
+#time complexity: O(n^2) in worst case
+#space complexity: O(n) in worst case due to processed list
+
