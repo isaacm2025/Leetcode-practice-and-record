f8d8d0218dbcbdd57600353dd1dc8e594753e714
Isaac <zihao599@gmail.com>
2026-05-11 22:47:02 -0700

    quick select 215

--- Day 102 215 Kth Largest Element in an Array.py
+++ Day 102 215 Kth Largest Element in an Array.py
@@ -36,3 +36,24 @@ class Solution:
         return heapq.nlargest(k, nums)[-1]
 #time complexity: O(nlogk)
 #space complexity: O(k)
+
+#quick select
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+            if p > k:
+                return quickSelect(l, p - 1)
+            elif p < k:
+                return quickSelect(p + 1, r)
+            else:
+                return nums[p]
+        return quickSelect(0, len(nums) - 1)
+#time complexity: O(n) on average, O(n^2) in worst case
+#space complexity: O(1) on average, O(n) in worst case due to recursion stack
