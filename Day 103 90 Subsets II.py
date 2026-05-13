14f0ae944acb2a2209576e26824bbb4b8b7885bd
Isaac <zihao599@gmail.com>
2026-05-12 22:56:39 -0700

    iteration 90

--- Day 103 90 Subsets II.py
+++ Day 103 90 Subsets II.py
@@ -53,3 +53,20 @@ class Solution:
         return res
 #time complexity: O(n * 2^n) where n is the length of nums.
 #space complexity: O(2^n) for the result list and the recursion stack, O(n) for the subset list.
+
+#iteration
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = [[]]
+        prev_idx = idx = 0
+        for i in range(len(nums)):
+            idx = prev_idx if i >= 1 and nums[i] == nums[i - 1] else 0
+            prev_idx = len(res)
+            for j in range(idx, prev_idx):
+                tmp = res[j].copy()
+                tmp.append(nums[i])
+                res.append(tmp)
+        return res
+#time complexity: O(n * 2^n) where n is the length of nums.
+#space complexity: O(2^n) for the result list, O(1) for the temporary list.
