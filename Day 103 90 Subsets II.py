8fa7f288c087028938f6f5fec18aab7133767bc8
Isaac <zihao599@gmail.com>
2026-05-12 22:50:07 -0700

    bf 90

---
+++ Day 103 90 Subsets II.py
@@ -0,0 +1,37 @@
+'''You are given an array nums of integers, which may contain duplicates. Return all possible subsets.
+
+The solution must not contain duplicate subsets. You may return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,1]
+
+Output: [[],[1],[1,2],[1,1],[1,2,1],[2]]
+Example 2:
+
+Input: nums = [7,7]
+
+Output: [[],[7], [7,7]]
+Constraints:
+
+1 <= nums.length <= 11
+-20 <= nums[i] <= 20'''
+
+#bf
+from typing import List
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.add(tuple(subset))
+                return
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+            backtrack(i + 1, subset)
+        nums.sort()
+        backtrack(0, [])
+        return [list(s) for s in res]
+#time complexity: O(n * 2^n) where n is the length of nums.
+#space complexity: O(2^n) for the result set and the recursion stack.
