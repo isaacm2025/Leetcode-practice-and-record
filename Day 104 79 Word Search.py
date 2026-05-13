4a0abf567656adfa4390dfc09cbfed16a0916ffe
Isaac <zihao599@gmail.com>
2026-05-13 16:43:47 -0700

    optimal 79

--- Day 104 79 Word Search.py
+++ Day 104 79 Word Search.py
@@ -56,3 +56,27 @@ class Solution:
                 if dfs(r, c, 0):
                     return True
         return False
+#tiime complexity: O(m * 4^n) where m is the number of cells in the board and n is the length of the word
+#space complexity: O(n) where n is the length of the word, which is the maximum depth of the recursion stack. The path set can also take up to O(n) space in the worst case, but it is not counted towards the overall space complexity since it is not part of the recursion stack.
+
+#optimal
+from typing import List
+class Solution:
+    def exist(self, board: List[List[str]], word:str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (min(r, c) < 0 or r >= ROWS or c >= COLS or word[i] != board[r][c] or board[r][c] == '#'):
+                return False
+            board[r][c] = '#'
+            res = (dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1))
+            board[r][c] = word[i]
+            return res
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+        return False
+#time complexity: O(m * 4^n) where m is the number of cells in the board and n is the length of the word
+#space complexity: O(n) where n is the length of the word, which is the maximum depth of the recursion stack. The board is modified in place to mark visited cells, so it does not require additional space for a path set
