3f20832b21e26cde24ad3447f902b0a758ab61f0
Isaac <zihao599@gmail.com>
2026-05-13 19:37:11 -0700

    hashmap 208

---
+++ Day 104 208 Implement Trie (Prefix Tree).py
@@ -0,0 +1,60 @@
+'''A prefix tree (also known as a trie) is a tree data structure used to efficiently store and retrieve keys in a set of strings. Some applications of this data structure include auto-complete and spell checker systems.
+
+Implement the PrefixTree class:
+
+PrefixTree() Initializes the prefix tree object.
+void insert(String word) Inserts the string word into the prefix tree.
+boolean search(String word) Returns true if the string word is in the prefix tree (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+Example 1:
+
+Input:
+["Trie", "insert", "dog", "search", "dog", "search", "do", "startsWith", "do", "insert", "do", "search", "do"]
+
+Output:
+[null, null, true, false, true, null, true]
+
+Explanation:
+PrefixTree prefixTree = new PrefixTree();
+prefixTree.insert("dog");
+prefixTree.search("dog");    // return true
+prefixTree.search("do");     // return false
+prefixTree.startsWith("do"); // return true
+prefixTree.insert("do");
+prefixTree.search("do");     // return true
+Constraints:
+
+1 <= word.length, prefix.length <= 1000
+word and prefix are made up of lowercase English letters.'''
+
+#hashmap
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+#time complexity: O(n) where n is the length of the word or prefix
+#space complexity: O(t) where t is the total number of characters in all the words inserted into the trie
