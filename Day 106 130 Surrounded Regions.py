c5b1c9c008e67f32f81c3a3a8f42ec87bff3918c
Isaac <zihao599@gmail.com>
2026-05-15 23:41:55 -0700

    bfs 130

--- Day 106 130 Surrounded Regions.py
+++ Day 106 130 Surrounded Regions.py
@@ -59,3 +59,39 @@ class Solution:
                     board[r][c] = 'O'
 #time complexity: O(M*N)
 #space complexity: O(M*N)
+
+#bfs
+from collections import deque
+from typing import List
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        def bfs(r, c):
+            queue = deque([(r, c)])
+            while queue:
+                row, col = queue.popleft()
+                if (row < 0 or col < 0 or row == ROWS or col == COLS or board[row][col] != 'O'):
+                    continue
+                board[row][col] = 'T'
+                queue.append((row + 1, col))
+                queue.append((row - 1, col))
+                queue.append((row, col + 1))
+                queue.append((row, col - 1))
+        for r in range(ROWS):
+            if board[r][0] == 'O':
+                bfs(r, 0)
+            if board[r][COLS - 1] == 'O':
+                bfs(r, COLS - 1)
+        for c in range(COLS):
+            if board[0][c] == 'O':
+                bfs(0, c)
+            if board[ROWS - 1][c] == 'O':
+                bfs(ROWS - 1, c)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
+#time complexity: O(M*N)
+#space complexity: O(M*N)
