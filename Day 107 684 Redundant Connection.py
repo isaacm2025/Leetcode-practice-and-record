c11770a1ce4807d04d6f88e042450b238cbd72ed
Isaac <zihao599@gmail.com>
2026-05-16 23:11:14 -0700

    dfs optimized 684

--- Day 107 684 Redundant Connection.py
+++ Day 107 684 Redundant Connection.py
@@ -53,3 +53,40 @@ class Solution:
         return []
 #time complexity: O(E * (V + E))
 #space complexity: O(V + E)
+
+#dfs(optimized)
+from typing import List
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        adj = [[] for _ in range(n + 1)]
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+        visit = [False] * (n + 1)
+        cycle = set()
+        cycleStart = -1
+        def dfs(node, parent):
+            nonlocal cycleStart
+            if visit[node]:
+                cycleStart = node
+                return True
+            visit[node] = True
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if dfs(nei, node):
+                    if cycleStart != -1:
+                        cycle.add(node)
+                    if node == cycleStart:
+                        cycleStart = -1
+                    return True
+            return False
+        dfs(1, -1)
+        for a, b in reversed(edges):
+            if a in cycle and b in cycle:
+                return [a, b]
+        return []
+#time complexity: O(V + E)
+#space complexity: O(V + E)
+            
