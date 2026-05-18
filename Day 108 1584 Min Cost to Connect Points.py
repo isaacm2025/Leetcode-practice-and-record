ce6b7fdd5a0846df4fd28b37cc87b6932932514d
Isaac <zihao599@gmail.com>
2026-05-17 23:04:38 -0700

    prims 1584

---
+++ Day 108 1584 Min Cost to Connect Points.py
@@ -0,0 +1,50 @@
+'''You are given a 2-D integer array points, where points[i] = [xi, yi]. Each points[i] represents a distinct point on a 2-D plane.
+
+The cost of connecting two points [xi, yi] and [xj, yj] is the manhattan distance between the two points, i.e. |xi - xj| + |yi - yj|.
+
+Return the minimum cost to connect all points together, such that there exists exactly one path between each pair of points.
+
+Example 1:
+
+
+
+Input: points = [[0,0],[2,2],[3,3],[2,4],[4,2]]
+
+Output: 10
+Constraints:
+
+1 <= points.length <= 1000
+-1,000,000 <= xi, yi <= 1,000,000
+All pairs (xi, yi) are distinct.
+'''
+
+#prims algorithm
+
+import heapq
+from typing import List
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        N = len(points)
+        adj = {i: [] for i in range(N)}
+        for i in range(N):
+            x1, y1 = points[i]
+            for j in range(i + 1, N):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append((dist, j))
+                adj[j].append((dist, i))
+        res = 0
+        visit = set()
+        minHeap = [[0, 0]]
+        while len(visit) < N:
+            cost, i = heapq.heappop(minHeap)
+            if i in visit:
+                continue
+            res += cost
+            visit.add(i)
+            for neiCost, nei in adj[i]:
+                if nei not in visit:
+                    heapq.heappush(minHeap, [neiCost, nei])
+        return res
+#time complexity is O(N^2 log N) because we are building the adjacency list with O(N^2) and then we are using a min heap to get the minimum cost edge which takes O(log N) time. The space complexity is O(N^2) because we are storing the adjacency list.
+#space complexity is O(N^2) because we are storing the adjacency list.
