dd81103cdf3b9c1cf8e24e7bd5a9c7a3322a7fbc
Isaac <zihao599@gmail.com>
2026-05-18 16:14:02 -0700

    dp 746

--- Day 109 746 Min Cost Climbing Stairs.py
+++ Day 109 746 Min Cost Climbing Stairs.py
@@ -39,3 +39,19 @@ class Solution:
         return min(dfs(0), dfs(1))
 #time complexity: O(2^n)
 #space complexity: O(n)
+
+#dp top down
+from typing import List
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = [-1] * len(cost)
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+            if memo[i] != -1:
+                return memo[i]
+            memo[i] = cost[i] + min(dfs(i + 1), dfs(i + 2))
+            return memo[i]
+        return min(dfs(0), dfs(1))
+#time complexity: O(n)
+#space complexity: O(n)
