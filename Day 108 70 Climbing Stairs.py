d0289ab8b72b0020a218f6e1bed5ba35e5b9ccd9
Isaac <zihao599@gmail.com>
2026-05-17 23:53:41 -0700

    dp 70

--- Day 108 70 Climbing Stairs.py
+++ Day 108 70 Climbing Stairs.py
@@ -36,3 +36,18 @@ class Solution:
         return dfs(0)
 #time complexity: O(2^n)
 #space complexity: O(n)
+
+#dp
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = [-1] * n
+        def dfs(i):
+            if i >= n:
+                return i == n
+            if cache[i] != -1:
+                return cache[i]
+            cache[i] = dfs(i + 1) + dfs(i + 2)
+            return cache[i]
+        return dfs(0)
+#time complexity: O(n)
+#space complexity: O(n)
