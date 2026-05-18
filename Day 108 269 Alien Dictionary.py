7ae9a2459345e7c4ceda751e9703b24be3a1aeff
Isaac <zihao599@gmail.com>
2026-05-17 23:38:02 -0700

    269 topological sort

--- Day 108 269 Alien Dictionary.py
+++ Day 108 269 Alien Dictionary.py
@@ -38,6 +38,9 @@ The input words will contain characters only from lowercase 'a' to 'z'.
 '''
 
 #dfs
+from collections import deque
+
+
 class Solution:
     def foreignDictionary(self, words: List[str]) -> str:
         adj = {c: set() for w in words for c in w}
@@ -66,6 +69,37 @@ class Solution:
             if dfs(char):
                 return ""
         res.reverse()
+        return "".join(res)
+#time complexity: O(N + V + E) where N is the total number of characters in all words, V is the number of unique characters and E is the number of edges in the graph.
+#space complexity: O(V + E) where V is the number of unique characters and E is the number of edges in the graph.
+
+#topological sort
+class Solution:
+    def foreignDictionary(self, words):
+        adj = {c: set() for w in words for c in w}
+        indegree = {c: 0 for w in words for c in w}
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    if w2[j] not in adj[w1[j]]:
+                        adj[w1[j]].add(w2[j])
+                        indegree[w2[j]] += 1
+                    break
+        q = deque([c for c in indegree if indegree[c] == 0])
+        res = []
+        while q:
+            char = q.popleft()
+            res.append(char)
+            for neighbor in adj[char]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+        if len(res) < len(indegree):
+            return ""
         return "".join(res)
 #time complexity: O(N + V + E) where N is the total number of characters in all words, V is the number of unique characters and E is the number of edges in the graph.
 #space complexity: O(V + E) where V is the number of unique characters and E is the number of edges in the graph.
