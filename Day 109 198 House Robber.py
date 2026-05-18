7cb38eb223f6cbe5769fd5e9498486280f0934cf
Isaac <zihao599@gmail.com>
2026-05-18 16:28:44 -0700

    space optimized 198

--- Day 109 198 House Robber.py
+++ Day 109 198 House Robber.py
@@ -51,3 +51,16 @@ class Solution:
         return dfs(0)
 #time complexity: O(n)
 #space complexity: O(n)
+
+#dp space optimized
+from typing import List
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for num in nums:
+            temp = max(num + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+#time complexity: O(n)
+#space complexity: O(1)
