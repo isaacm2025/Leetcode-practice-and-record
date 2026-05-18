0934ee79b2d33c2f9aef5ab1f5ae8c3d3379a826
Isaac <zihao599@gmail.com>
2026-05-18 16:27:17 -0700

    dp top down 198

--- Day 109 198 House Robber.py
+++ Day 109 198 House Robber.py
@@ -35,3 +35,19 @@ class Solution:
         return dfs(0)
 #time complexity: O(2^n)
 #space complexity: O(n)
+
+#dp top down
+from typing import List
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums)
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if memo[i] != -1:
+                return memo[i]
+            memo[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            return memo[i]
+        return dfs(0)
+#time complexity: O(n)
+#space complexity: O(n)
