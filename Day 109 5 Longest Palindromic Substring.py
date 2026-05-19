6987d1759765967ad41e63bd1d2378cbfec11f81
Isaac <zihao599@gmail.com>
2026-05-18 17:46:25 -0700

    dp 5

--- Day 109 5 Longest Palindromic Substring.py
+++ Day 109 5 Longest Palindromic Substring.py
@@ -37,3 +37,20 @@ class Solution:
         return res
 #time: O(n^3)
 #space: O(n)
+
+#dp
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        resIdx, resLen = 0, 1
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n -1, -1, -1):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    if resLen < j - i + 1:
+                        resIdx = i
+                        resLen = j - i + 1
+        return s[resIdx: resIdx + resLen]
+#time: O(n^2)
+#space: O(n^2)
