463bb5e49cd1821a547aca4b541d1f9003190945
Isaac <zihao599@gmail.com>
2026-05-21 16:32:28 -0700

    recursion 72

---
+++ Day 112 72 Edit Distance.py
@@ -0,0 +1,52 @@
+'''You are given two strings word1 and word2, each consisting of lowercase English letters.
+
+You are allowed to perform three operations on word1 an unlimited number of times:
+
+Insert a character at any position
+Delete a character at any position
+Replace a character at any position
+Return the minimum number of operations to make word1 equal word2.
+
+Example 1:
+
+Input: word1 = "monkeys", word2 = "money"
+
+Output: 2
+Explanation:
+monkeys -> monkey (remove s)
+monkey -> money (remove k)
+
+Example 2:
+
+Input: word1 = "neatcdee", word2 = "neetcode"
+
+Output: 3
+Explanation:
+neatcdee -> neetcdee (replace a with e)
+neetcdee -> neetcde (remove last e)
+neetcde -> neetcode (insert o)
+
+Constraints:
+
+0 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.'''
+
+#recursive solution
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        def dfs(i, j):
+            if i == m:
+                return n - j
+            if j == n:
+                return m - i
+            if word1[i] == word2[j]:
+                return dfs(i + 1, j + 1)
+            res = min(dfs(i + 1, j), dfs(i, j + 1))
+            res = min(res, dfs(i + 1, j + 1))
+            return res + 1
+        return dfs(0, 0)
+#time complexity: O(3^(m+n))
+#space complexity: O(m+n)
+
+#
