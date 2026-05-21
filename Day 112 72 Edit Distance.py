23673679764255812b7a25eada317eab1a55cdfd
Isaac <zihao599@gmail.com>
2026-05-21 16:35:41 -0700

    dp 72

--- Day 112 72 Edit Distance.py
+++ Day 112 72 Edit Distance.py
@@ -49,4 +49,25 @@ class Solution:
 #time complexity: O(3^(m+n))
 #space complexity: O(m+n)
 
-#
+#dp
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        dp = {}
+        def dfs(i, j):
+            if i == m:
+                return n - j
+            if j == n:
+                return m - i
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if word1[i] == word2[j]:
+                dp[(i, j)] = dfs(i + 1, j+ 1)
+            else:
+                res = min(dfs(i + 1, j), dfs(i, j + 1))
+                res = min(res, dfs(i + 1, j + 1))
+                dp[(i, j)] = res + 1
+            return dp[(i, j)]
+        return dfs(0, 0)
+#time complexity: O(m*n)
+#space complexity: O(m*n)
