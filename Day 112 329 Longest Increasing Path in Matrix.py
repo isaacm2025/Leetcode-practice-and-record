af4239942e8df21bc4c9c8dea8f38cc066636713
Isaac <zihao599@gmail.com>
2026-05-21 16:17:06 -0700

    329 dp

--- Day 112 329 Longest Increasing Path in Matrix.py
+++ Day 112 329 Longest Increasing Path in Matrix.py
@@ -45,3 +45,28 @@ class Solution:
         return LIP
 #time complexity: O(m * n* 4^(n*m)) where n and m are the dimensions of the matrix
 #space complexity: O(n*m) where n and m are the dimensions of the matrix
+
+#dp
+from typing import List
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        dp = {}
+        def dfs(r, c, prevVal):
+            if (r < 0 or r == ROWS or c < 0 or c == COLS or matrix[r][c] <= prevVal):
+                return 0
+            if (r, c) in dp:
+                return dp[(r, c)]
+            res = 1
+            res = max(res, 1 + dfs(r - 1, c, matrix[r][c]))
+            res = max(res, 1 + dfs(r + 1, c, matrix[r][c]))
+            res = max(res, 1 + dfs(r, c - 1, matrix[r][c]))
+            res = max(res, 1 + dfs(r, c + 1, matrix[r][c]))
+            dp[(r, c)] = res
+            return res
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, -1)
+        return max(dp.values())
+#time complexity: O(m * n) where n and m are the dimensions of the matrix
+#space complexity: O(m * n) where n and m are the dimensions of the matrix
