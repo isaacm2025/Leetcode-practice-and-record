bce642047dffca8e3a3cbe3badbbe9e658a82441
Isaac <zihao599@gmail.com>
2026-05-22 22:31:22 -0700

    recursion 53

--- Day 113 53 Maximum Subarray.py
+++ Day 113 53 Maximum Subarray.py
@@ -32,3 +32,17 @@ class Solution:
         return res
 #time complexity: O(n^2)
 #space complexity: O(1)
+
+#recursion
+from ast import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        def dfs(i, flag):
+            if i == len(nums) - 1:
+                return max(0, nums[i]) if flag else nums[i]
+            if flag:
+                return max(0, nums[i] + dfs(i + 1, True))
+            return max(dfs(i + 1, False), nums[i] + dfs(i + 1, True))
+        return dfs(0, False)
+#time complexity: O(2^n)
+#space complexity: O(n)
