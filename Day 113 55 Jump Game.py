4f8e73585d775f7d8d5c9375562e65a341defb6a
Isaac <zihao599@gmail.com>
2026-05-22 22:45:01 -0700

    greedy 55

--- Day 113 55 Jump Game.py
+++ Day 113 55 Jump Game.py
@@ -72,3 +72,15 @@ class Solution:
         return dp[0]
 #time complexity: O(n^2)
 #space complexity: O(n)
+
+#greedy
+from ast import List
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return goal == 0
+#time complexity: O(n)
+#space complexity: O(1)
