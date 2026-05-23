85c0e02393e5769722aac841df9af4622c9dbbd3
Isaac <zihao599@gmail.com>
2026-05-22 22:26:30 -0700

    space optimized 10

--- Day 113 10 Regular Expression Matching.py
+++ Day 113 10 Regular Expression Matching.py
@@ -72,4 +72,26 @@ class Solution:
         return dfs(0, 0)
 #time complexity: O(m * n)
 #space complexity: O(m * n)
+
+#dp space optimized
+from ast import List
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [False] * (len(p) + 1)
+        dp[0] = True
+        for i in range(len(s), -1, -1):
+            nextDp = [False] * (len(p) + 1)
+            nextDp[len(p)] = (i == len(s))
+            for j in range(len(p) - 1, -1, -1):
+                match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+                if (j + 1) < len(p) and p[j + 1] == "*":
+                    nextDp[j] = nextDp[j + 2]
+                    if match:
+                        nextDp[j] |= dp[j]
+                elif match:
+                    nextDp[j] = dp[j + 1]
+            dp = nextDp
+        return dp[0]
+#time complexity: O(m * n)
+#space complexity: O(n)
             
