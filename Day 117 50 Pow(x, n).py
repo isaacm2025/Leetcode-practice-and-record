af5a403c833563d2501e40381d60ea5115731dfd
Isaac <zihao599@gmail.com>
2026-05-26 18:49:31 -0700

    recursion 50

--- Day 117 50 Pow(x, n).py
+++ Day 117 50 Pow(x, n).py
@@ -38,3 +38,18 @@ class Solution:
         for i in range(abs(n)):
             res *= x
         return res if n >= 0 else 1 /res
+
+#recursive
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+            res = helper(x * x, n // 2)
+            return x * res if n % 2 else res
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1/res
+#time complexity: O(log n)
+#space complexity: O(log n) due to recursive stack space
