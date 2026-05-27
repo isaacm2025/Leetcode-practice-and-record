860fd80b214cb737c74f1a795c6f787b2117d691
Isaac <zihao599@gmail.com>
2026-05-27 11:39:27 -0700

    bf 371

---
+++ Day 118 371 Sum of Two Integers.py
@@ -0,0 +1,24 @@
+'''Given two integers a and b, return the sum of the two integers without using the + and - operators.
+
+Example 1:
+
+Input: a = 1, b = 1
+
+Output: 2
+Example 2:
+
+Input: a = 4, b = 7
+
+Output: 11
+Constraints:
+
+-1000 <= a, b <= 1000'''
+
+
+#bf
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return a + b
+#time complexity: O(1) since we are performing a constant number of operations
+#space complexity: O(1) since we are using a constant amount of space for the result
+
