5a28ec0cbb470f22f07efd1227e726c01b4a54e9
Isaac <zihao599@gmail.com>
2026-06-01 00:21:34 -0700

    stack 42

--- Day 122 42 Trapping Rain Water.py
+++ Day 122 42 Trapping Rain Water.py
@@ -33,3 +33,26 @@ class Solution:
         return res
 #time O(n^2) because we have two nested loops that iterate through the input array, where n is the length of the input array.
 #space O(1) because we are using a constant amount of extra space to store the maximum area found so far and the variables used in the loops.
+
+#stack
+from ast import List
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        stack = []
+        res= 0
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                mid = height[stack.pop()]
+                if stack:
+                    right = height[i]
+                    left = height[stack[-1]]
+                    h = min(left, right) - mid
+                    w = i - stack[-1] - 1
+                    res += h * w
+            stack.append(i)
+        return res
+#time O(n) because we have a single loop that iterates through the input array, where n is the length of the input array. Each element is pushed and popped from the stack at most once, which takes O(1) time on average.
+#space O(n) because in the worst case, we could have all the bars in the input array being of increasing height, which would require us to store all n indices in the stack, where n is the length of the input array. 
+# Each entry in the stack takes O(1) space, so the total space used by the stack can be O(n) in the worst case.
