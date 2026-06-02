9a0592f31d709a2647b6e85d22288d1d9cf2af75
Isaac <zihao599@gmail.com>
2026-06-01 23:37:00 -0700

    sliding window 76

--- Day 123 567 Permutation in String.py
+++ Day 123 567 Permutation in String.py
@@ -45,7 +45,7 @@ class Solution:
             count2, cur = {}, 0
             for j in range(i, len(s2)):
                 count2[s2[j]] = 1 + count2.get(s2[j], 0)
-                if count1.get(s2[j], 0) == count2[s2[j]]:
+                if count1.get(s2[j], 0) < count2[s2[j]]:
                     break
                 if count1.get(s2[j], 0) == count2[s2[j]]:
                     cur += 1
