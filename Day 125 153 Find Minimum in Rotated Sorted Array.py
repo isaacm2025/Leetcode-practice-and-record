085bada6af751bc02c1d35222b977dd7337cbaaa
Isaac <zihao599@gmail.com>
2026-06-04 16:25:08 -0700

    bf 33

--- Day 125 153 Find Minimum in Rotated Sorted Array.py
+++ Day 125 153 Find Minimum in Rotated Sorted Array.py
@@ -42,7 +42,7 @@ class Solution:
         res = nums[0]
         l, r = 0, len(nums) - 1
         while l <= r:
-            if nums[l] < nums[r];
+            if nums[l] < nums[r]:
                 res = min(res, nums[l])
                 break
             mid = l + (r - l) // 2
