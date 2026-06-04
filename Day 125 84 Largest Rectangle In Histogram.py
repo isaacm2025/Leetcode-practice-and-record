f7eb9b09fe48e114a3d6936d192cde159d365608
Isaac <zihao599@gmail.com>
2026-06-03 23:25:49 -0700

    125 stack

---
+++ Day 125 84 Largest Rectangle In Histogram.py
@@ -0,0 +1,50 @@
+'''You are given an array of integers heights where heights[i] represents the height of a bar. The width of each bar is 1.
+
+Return the area of the largest rectangle that can be formed among the bars.
+
+Note: This chart is known as a histogram.
+
+Example 1:
+
+Input: heights = [7,1,7,2,2,4]
+
+Output: 8
+Example 2:
+
+Input: heights = [1,3,7]
+
+Output: 7
+Constraints:
+
+1 <= heights.length <= 1000.
+0 <= heights[i] <= 1000'''
+
+#stack
+from typing import List
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        stack = []
+        leftMost = [-1] * n
+        for i in range(n):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if stack:
+                leftMost[i] = stack[-1]
+            stack.append(i)
+        stack = []
+        rightMost = [n] * n
+        for i in range(n - 1, -1, -1):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if stack:
+                rightMost[i] = stack[-1]
+            stack.append(i)
+        maxArea = 0
+        for i in range(n):
+            leftMost[i] += 1
+            rightMost[i] -= 1
+            maxArea = max(maxArea, heights[i] * (rightMost[i] - leftMost[i] + 1))
+        return maxArea
+#time O(n) because we are iterating through the heights array three times, and each element is pushed and popped at most once
+#space O(n) because of the stack and the leftMost and rightMost arrays
