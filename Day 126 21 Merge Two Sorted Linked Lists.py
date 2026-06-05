27b37fabe6f96eb268bfda350b426c2c74d8c559
Isaac <zihao599@gmail.com>
2026-06-04 17:25:34 -0700

    recursion 21

---
+++ Day 126 21 Merge Two Sorted Linked Lists.py
@@ -0,0 +1,50 @@
+'''You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+
+The new list should be made up of nodes from list1 and list2.
+
+Example 1:
+
+
+
+Input: list1 = [1,2,4], list2 = [1,3,5]
+
+Output: [1,1,2,3,4,5]
+Example 2:
+
+Input: list1 = [], list2 = [1,2]
+
+Output: [1,2]
+Example 3:
+
+Input: list1 = [], list2 = []
+
+Output: []
+Constraints:
+
+0 <= The length of the each list <= 100.
+-100 <= Node.val <= 100'''
+
+#recursion
+# Definition for singly-linked list.
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+#time O(n + m) where n and m are the lengths of list1 and list2 respectively, because we are traversing both lists once
+#space O(n + m) because of the recursive call stack, in the worst case we could have n + m recursive calls if all nodes of one list are smaller than the other list
