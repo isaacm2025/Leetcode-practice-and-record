58acfa017d7944be29ae59d590972cd37935b6d4
Isaac <zihao599@gmail.com>
2026-06-05 23:28:21 -0700

    iteratiion 19

---
+++ Day 127 19 Remove Nth Node From End of List.py
@@ -0,0 +1,49 @@
+'''Given the head of a linked list and an integer n, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+Input: head = [1,2,3,4], n = 2
+
+Output: [1,2,4]
+Example 2:
+
+Input: head = [5], n = 1
+
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 2
+
+Output: [2]
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz'''
+
+#iterative
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        N = 0
+        cur = head
+        while cur:
+            N += 1
+            cur = cur.next
+        removeIndex = N - n
+        if removeIndex == 0:
+            return head.next
+        cur = head
+        for i in range(N - 1):
+            if (i + 1) == removeIndex:
+                cur.next = cur.next.next
+                break
+            cur = cur.next
+        return head
+#time O(n) because we are iterating through the linked list twice, once to count the number of nodes, and once to remove the nth node from the end
+#space O(1) because we are using a constant amount of space to store the count of nodes and the current node
