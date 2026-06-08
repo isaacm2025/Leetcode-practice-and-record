a8d3bc4545879dbafff4e18324627320dc708941
Isaac <zihao599@gmail.com>
2026-06-07 22:46:36 -0700

    dfs 100

---
+++ Day 129 100 Same Binary Tree.py
@@ -0,0 +1,57 @@
+'''Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+
+Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+
+Example 1:
+
+
+
+Input: p = [1,2,3], q = [1,2,3]
+
+Output: true
+Example 2:
+
+
+
+Input: p = [4,7], q = [4,null,7]
+
+Output: false
+Example 3:
+
+
+
+Input: p = [1,2,3], q = [1,3,2]
+
+Output: false
+Constraints:
+
+0 <= The number of nodes in both trees <= 100.
+-100 <= Node.val <= 100
+'''
+
+#dfs
+# Definition for a binary tree node.
+from typing import Optional
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return True
+        if not root:
+            return False
+        if self.sameTree(root, subRoot):
+            return True
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+    def sameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root and not subRoot:
+            return True
+        if root and subRoot and root.val == subRoot.val:
+            return (self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right))
+        return False
+#time complexity: O(m*n) where m and n are the number of nodes in the two trees. In the worst case, we will compare each node of the first tree with each node of the second tree.
+#space complexity: O(m+n)
