ddc4f2a4170d62f8a7b6ad898421c09423320b62
Isaac <zihao599@gmail.com>
2026-06-08 20:51:37 -0700

    iteration 235

--- Day 130 235 Lowest Common Ancestor in Binary Search Tree.py
+++ Day 130 235 Lowest Common Ancestor in Binary Search Tree.py
@@ -1,69 +1,65 @@
-'''Given the root of a binary tree, return true if it is a valid binary search tree, otherwise return false.
+'''Given a binary search tree (BST) where all node values are unique, and two nodes from the tree p and q, return the lowest common ancestor (LCA) of the two nodes.
 
-A valid binary search tree satisfies the following constraints:
+The lowest common ancestor between two nodes p and q is the lowest node in a tree T such that both p and q as descendants. The ancestor is allowed to be a descendant of itself.
 
-The left subtree of every node contains only nodes with keys less than the node's key.
-The right subtree of every node contains only nodes with keys greater than the node's key.
-Both the left and right subtrees are also binary search trees.
 Example 1:
 
 
 
-Input: root = [2,1,3]
+Input: root = [5,3,8,1,4,7,9,null,2], p = 3, q = 8
 
-Output: true
+Output: 5
+
 Example 2:
 
 
 
-Input: root = [1,2,3]
+Input: root = [5,3,8,1,4,7,9,null,2], p = 3, q = 4
 
-Output: false
-Explanation: The root node's value is 1 but its left child's value is 2 which is greater than 1.
+Output: 3
+Explanation: The LCA of nodes 3 and 4 is 3, since a node can be a descendant of itself.
 
 Constraints:
 
-1 <= The number of nodes in the tree <= 1000.
--1000 <= Node.val <= 1000'''
+2 <= The number of nodes in the tree <= 100.
+-100 <= Node.val <= 100
+p != q
+p and q will both exist in the BST.
+'''
 
-#dfs
-from collections import deque
-from typing import Optional
+#recursive
 
-
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val = 0, left = None, right = None):
         self.val = val
         self.left = left
         self.right = right
-    
 class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
-        def valid(node, left, right):
-            if not node:
-                return True
-            if node.val <= left or node.val >= right:
-                return False
-            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
-        return valid(root, float('-inf'), float('inf'))
-#time complexity: O(n)
-#space complexity: O(n) in worst case, O(log n) in best case
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or not p or not q: 
+            return None #base case
+        if (max(p.val, q.val) < root.val): #if both p and q are smaller than root, then LCA lies in left
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif (min(p.val, q.val) > root.val): #if both p and q are greater than root, then LCA lies in right
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+#time complexity: O(h) where h is the height of the tree
+#space complexity: O(h) in worst case, O(log n) in best case
 
-#bfs
+#iteration
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
-        queue = deque([(root, float('-inf'), float('inf'))])
-        while queue:
-            node, left, right = queue.popleft()
-            if not (left < node.val < right):
-                return False
-            if node.left:
-                queue.append((node.left, left, node.val))
-            if node.right:
-                queue.append((node.right, node.val, right))
-        return True
-#time complexity: O(n)
-#space complexity: O(n) in worst case, O(log n) in best case
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        current = root
+        while current:
+            if p.val > current.val and q.val > current.val:
+                current = current.right
+            elif p.val < current.val and q.val < current.val:
+                current = current.left
+            else:
+                return current
+#time complexity: O(h) where h is the height of the tree
+#space complexity: O(1)
+
 
