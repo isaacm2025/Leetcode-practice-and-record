feefb2ea2205b4f11f817560eca74ce983784b73
Isaac <zihao599@gmail.com>
2026-06-08 20:27:25 -0700

    bfs 235

--- Day 130 235 Lowest Common Ancestor in Binary Search Tree.py
+++ Day 130 235 Lowest Common Ancestor in Binary Search Tree.py
@@ -27,6 +27,10 @@ Constraints:
 -1000 <= Node.val <= 1000'''
 
 #dfs
+from collections import deque
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val = 0, left = None, right = None):
         self.val = val
@@ -44,3 +48,22 @@ class Solution:
         return valid(root, float('-inf'), float('inf'))
 #time complexity: O(n)
 #space complexity: O(n) in worst case, O(log n) in best case
+
+#bfs
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        queue = deque([(root, float('-inf'), float('inf'))])
+        while queue:
+            node, left, right = queue.popleft()
+            if not (left < node.val < right):
+                return False
+            if node.left:
+                queue.append((node.left, left, node.val))
+            if node.right:
+                queue.append((node.right, node.val, right))
+        return True
+#time complexity: O(n)
+#space complexity: O(n) in worst case, O(log n) in best case
+
