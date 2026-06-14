3df43aa8ae1f40d46e6f9d441353fcfdc72d2339
Isaac <zihao599@gmail.com>
2026-06-13 23:21:14 -0700

    211 bf

---
+++ Day 135 211 Design Add and Search Word Data Structure.py
@@ -0,0 +1,54 @@
+'''Design a data structure that supports adding new words and searching for existing words.
+
+Implement the WordDictionary class:
+
+void addWord(word) Adds word to the data structure.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+Example 1:
+
+Input:
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["day"],["bay"],["may"],["say"],["day"],[".ay"],["b.."]]
+
+Output:
+[null, null, null, null, false, true, true, true]
+
+Explanation:
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("day");
+wordDictionary.addWord("bay");
+wordDictionary.addWord("may");
+wordDictionary.search("say"); // return false
+wordDictionary.search("day"); // return true
+wordDictionary.search(".ay"); // return true
+wordDictionary.search("b.."); // return true
+Constraints:
+
+1 <= word.length <= 20
+word in addWord consists of lowercase English letters.
+word in search consist of '.' or lowercase English letters.
+There will be at most 2 dots in word for search queries.
+At most 10,000 calls will be made to addWord and search.'''
+
+#bf
+class WordDictionary:
+    def __init__(self):
+        self.store = []
+    
+    def addWord(self, word: str) -> None:
+        self.store.append(word)
+    def search(self, word: str) -> bool:
+        for w in self.store:
+            if len(w) != len(word):
+                continue
+            i = 0
+            while i < len(w):
+                if word[i] == '.' or w[i] == word[i]:
+                    i += 1
+                else:
+                    break
+            if i == len(w):
+                return True
+        return False
+#time complexity: O(1) for addWord, O(n*m) for search where n is the number of words and m is the length of the word to search
+#space complexity: O(n*m) where n is the number of words and m is the average length of the words
