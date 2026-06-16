6acd281b4745e9446ef89413e3b39a5d617484c5
Isaac <zihao599@gmail.com>
2026-06-15 22:38:41 -0700

    topological sort 684

--- Day 137 684 Redundant Connection.py
+++ Day 137 684 Redundant Connection.py
@@ -28,6 +28,7 @@ n == edges.length
 There are no repeated edges and no self-loops in the input.'''
 
 #dfs
+from collections import deque
 from typing import List
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
@@ -52,3 +53,33 @@ class Solution:
         return []
 #time complexity: O(E*(V+E)) where E is the number of edges and V is the number of vertices
 #space complexity: O(V+E) where V is the number of vertices and E is the number of edges due to the adjacency list and the visited array
+
+#topological sort
+from typing import List
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        indegree = [0] * (n + 1)
+        adj = [[] for _ in range(n + 1)]
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+            indegree[a] += 1
+            indegree[b] += 1
+        q = deque()
+        for i in range(1, n + 1):
+            if indegree[i] == 1:
+                q.append(i)
+        while q:
+            node = q.popleft()
+            indegree[node] -= 1
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 1:
+                    q.append(nei)
+        for a, b in reversed(edges):
+            if indegree[a] == 2 and indegree[b]: # both nodes have indegree of 2, meaning they are part of the cycle
+                return [a,b]
+        return []
+#time complexity: O(V+E) where V is the number of vertices and E is the number of edges
+#space complexity: O(V+E) where V is the number of vertices and E is the number of edges due to the adjacency list and the indegree array
