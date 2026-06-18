d0bf5ad0e4c1e7d14f8b3bbb6eb393abe37c842b
Isaac <zihao599@gmail.com>
2026-06-17 22:03:33 -0700

    shortest path 787

--- Day 139 787 Cheapest Flights Within K Stops.py
+++ Day 139 787 Cheapest Flights Within K Stops.py
@@ -37,6 +37,7 @@ fromi != toi
 0 <= src, dst, k < n
 '''
 
+from collections import deque
 from typing import List
 #bellman-ford
 class Solution:
@@ -54,3 +55,27 @@ class Solution:
         return -1 if prices[dst] == float('inf') else prices[dst]
 #time complexity: O(n + (m * k)) where n is the number of airports, m is the number of flights and k is the maximum number of stops.
 #space complexity: O(n) where n is the number of airports.
+
+#shortest path with Dijkstra's algorithm
+import heapq
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float("inf")] * n
+        prices[src] = 0
+        adj = [[] for _ in range(n)]
+        for u, v, cst in flights:
+            adj[u].append([v, cst])
+        q = deque([(0, src, 0)])
+        while q:
+            cst, node, stops = q.popleft()
+            if stops > k:
+                continue
+            for nei, w in adj[node]:
+                nextCost = cst + w
+                if nextCost < prices[nei]:
+                    prices[nei] = nextCost
+                    q.append((nextCost, nei, stops + 1))
+        return prices[dst] if prices[dst] != float("inf") else -1
+#time complexity: O(n * k) where n is the number of airports and k is the maximum number of stops.
+#space complexity: O(n + m) where n is the number of airports and m is the number of flights.
