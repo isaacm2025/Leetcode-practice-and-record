4287075f2cc62208e27f27cb6baaf0363e4eb360
Isaac <zihao599@gmail.com>
2026-06-18 23:48:08 -0700

    dp 322

--- Day 140 322 Coin Change.py
+++ Day 140 322 Coin Change.py
@@ -47,3 +47,17 @@ class Solution:
         return -1 if minCoins >= 1e9 else minCoins
 #time complexity: O(n^t)
 #space complexity: O(t)
+
+#dp
+from typing import List
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if a - coin >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - coin])
+        return dp[amount] if dp[amount] != amount + 1 else -1
+#time complexity: O(n*t)
+#space complexity: O(t)
