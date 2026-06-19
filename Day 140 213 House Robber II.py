27e3cd7e7287416dbb8181129ba774b6b59a8e9c
Isaac <zihao599@gmail.com>
2026-06-18 23:11:20 -0700

    dp 213

--- Day 140 213 House Robber II.py
+++ Day 140 213 House Robber II.py
@@ -37,4 +37,22 @@ class Solution:
 #time complexity: O(2^n)
 #space complexity: O(n)
 
-
+#dp
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        return max(self.helper(nums[1:]), self.helper(nums[:-1]))
+    def helper(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) ==1:
+            return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        return dp[-1]
+#time complexity: O(n)
+#space complexity: O(n)
