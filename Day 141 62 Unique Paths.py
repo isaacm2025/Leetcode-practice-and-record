43dd893e664f54cdb88c6ff283868b212177d88b
Isaac <zihao599@gmail.com>
2026-06-19 23:31:07 -0700

    space optimized 62

--- Day 141 62 Unique Paths.py
+++ Day 141 62 Unique Paths.py
@@ -48,11 +48,12 @@ class Solution:
 #space optimization dp
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        dp[m - 1][n - 1] = 1
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
-        return dp[0][0]
+        row = [1] * n
+        for i in range(m -1):
+            nextRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                nextRow[j] = nextRow[j + 1] + row[j]
+            row = nextRow
+        return row[0]
 #time complexity: O(m*n)
 #space complexity: O(n)
