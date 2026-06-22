d61c5cf292467d2584ad27109c5ef589e2c42979
Isaac <zihao599@gmail.com>
2026-06-21 23:43:18 -0700

    dp optimal 72

--- Day 143 72 Edit Distance.py
+++ Day 143 72 Edit Distance.py
@@ -57,3 +57,28 @@ class Solution:
 # The algorithm iterates through each character of both strings, resulting in a time complexity of O(m*n).
 #space complexity: O(n) where n is the length of the shorter string (after the optimization). 
 # The space complexity is reduced to O(n) because we only need to store the current and next row of the DP table, which has a length of n (the length of the shorter string).
+
+#dp optimal
+from typing import List
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        if m < n:
+            m, n = n, m
+            word1, word2 = word2, word1
+        dp = [n - i for i in range(n + 1)]
+        for i in range(m - 1, -1, -1):
+            nextDp = dp[n]
+            dp[n] = m - i
+            for j in range(n - 1, -1, -1):
+                temp = dp[j]
+                if word1[i] == word2[j]:
+                    dp[j] = nextDp
+                else:
+                    dp[j] = 1 + min(dp[j], dp[j + 1], nextDp)
+                nextDp = temp
+        return dp[0]
+#time complexity: O(m*n) where m and n are the lengths of word1 and word2, respectively. 
+# The algorithm iterates through each character of both strings, resulting in a time complexity of O(m*n).
+#space complexity: O(n) where n is the length of the shorter string (after the optimization). 
+# The space complexity is reduced to O(n) because we only need to store the current row of the DP table, which has a length of n (the length of the shorter string).
