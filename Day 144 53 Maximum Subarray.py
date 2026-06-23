a3ec0d1b61753449a03f42989c7709574c629034
Isaac <zihao599@gmail.com>
2026-06-22 21:51:56 -0700

    dp 53

--- Day 144 53 Maximum Subarray.py
+++ Day 144 53 Maximum Subarray.py
@@ -32,3 +32,16 @@ class Solution:
         return res
 #time complexity: O(n^2)
 #space complexity: O(1)
+
+#dp
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [[0] * 2 for _ in range(n)]
+        dp[n - 1][1] = dp[n - 1][0] = nums[n - 1]
+        for i in range(n -2, -1, -1):
+            dp[i][1] = max(nums[i], nums[i] + dp[i + 1][1])
+            dp[i][0] = max(dp[i + 1][0], dp[i][1])
+        return dp[0][0]
+#time complexity: O(n)
+#space complexity: O(n)
