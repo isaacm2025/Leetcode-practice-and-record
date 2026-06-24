99c005930598be7e472b4e9954b97cb211d4e499
Isaac <zihao599@gmail.com>
2026-06-23 19:43:14 -0700

    greedy 678

--- Day 145 678 Valid Parenthesis String.py
+++ Day 145 678 Valid Parenthesis String.py
@@ -73,3 +73,22 @@ class Solution:
             return not left
 #time complexity: O(n) where n is the length of s.
 #space complexity: O(n) because we use two stacks to store the indices of '(' and '*' characters.
+
+#greedy
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftMin, leftMax = 0, 0
+        for c in s:
+            if c == '(':
+                leftMin, leftMax = leftMin + 1, leftMax + 1
+            elif c == ')':
+                leftMin, leftMax = leftMin - 1, leftMax - 1
+            else:
+                leftMin, leftMax = leftMin - 1, leftMax + 1
+            if leftMax < 0:
+                return False
+            if leftMin < 0:
+                leftMin = 0
+        return leftMin == 0
+#time complexity: O(n) where n is the length of s.
+#space complexity: O(1) because we only use two variables to keep track of the minimum and maximum number of left parentheses.
