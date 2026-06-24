e81b5f00264d614a628a7d8df8defb5cde55e2e6
Isaac <zihao599@gmail.com>
2026-06-23 20:05:18 -0700

    greedy 57

--- Day 145 57 Insert Interval.py
+++ Day 145 57 Insert Interval.py
@@ -50,3 +50,21 @@ class Solution:
         return res
 #time complexity: O(n) where n is the length of intervals.
 #space complexity: O(n) because we use a new list to store the result.
+
+#greedy
+from typing import List
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        res.append(newInterval)
+        return res
+#time complexity: O(n) where n is the length of intervals.
+#space complexity: O(n) because we use a new list to store the result.
