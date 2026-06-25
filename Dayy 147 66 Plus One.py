1d410a79374d49e54cf8764a1e7b29af86ee529d
Isaac <zihao599@gmail.com>
2026-06-25 15:16:19 -0700

    iteration 66

--- Dayy 147 66 Plus One.py
+++ Dayy 147 66 Plus One.py
@@ -32,3 +32,16 @@ class Solution:
             return self.plusOne(digits[:-1]) + [0]
 #time complexity: O(N)
 #space complexity: O(N)
+
+#iteration
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        for i in range(n- 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
+#time complexity: O(N)
+#space complexity: O(N)
