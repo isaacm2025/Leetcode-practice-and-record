ee9ae7d2871de770db15b59a53f640a52bcb5d11
Isaac <zihao599@gmail.com>
2026-06-25 15:48:51 -0700

    multiplication 43

---
+++ Day 147 43 Multiply Strings.py
@@ -0,0 +1,43 @@
+'''You are given two strings num1 and num2 that represent non-negative integers.
+
+Return the product of num1 and num2 in the form of a string.
+
+Assume that neither num1 nor num2 contain any leading zero, unless they are the number 0 itself.
+
+Note: You can not use any built-in library to convert the inputs directly into integers.
+
+Example 1:
+
+Input: num1 = "3", num2 = "4"
+
+Output: "12"
+Example 2:
+
+Input: num1 = "111", num2 = "222"
+
+Output: "24642"
+Constraints:
+
+1 <= num1.length, num2.length <= 200
+num1 and num2 consist of digits only.'''
+
+#multiplication
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if "0" in [num1, num2]:
+            return '0'
+        res = [0] * (len(num1) + len(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+        for i1 in range(len(num1)):
+            for i2 in range(len(num2)):
+                digit = int(num1[i1]) * int(num2[i2])
+                res[i1 + i2] += digit
+                res[i1 + i2 + 1] += res[i1 + i2] //10
+                res[i1 + i2] = res[i1 + i2] % 10
+        res, begin = res[::-1], 0
+        while begin < len(res) and res[begin] == 0:
+            begin += 1
+        res = map(str, res[begin:])
+        return ''.join(res)
+#time complexity: O(M*N)
+#space complexity: O(M+N)
