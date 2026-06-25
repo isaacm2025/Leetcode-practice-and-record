01e81561ca45aa1444ba8381fb62d50c4acc45e5
Isaac <zihao599@gmail.com>
2026-06-25 15:36:45 -0700

    iteration 50

--- Day 147 50 Pow(x, n).py
+++ Day 147 50 Pow(x, n).py
@@ -40,3 +40,21 @@ class Solution:
         return res if n >= 0 else 1/res
 #time complexity: O(N)
 #space complexity: O(1)
+
+#iteration
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+        if n == 0:
+            return 1
+        res = 1
+        power = abs(n)
+        while power:
+            if power & 1:
+                res *= x
+            x *= x
+            power >>= 1
+        return res if n >= 0 else 1/res
+#time complexity: O(logN)
+#space complexity: O(1)
