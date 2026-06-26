140e816fab2db6b1bc6eff9231fbc16f5d534bec
Isaac <zihao599@gmail.com>
2026-06-26 15:04:00 -0700

    bf 190

---
+++ Day 148 190 Reverse Bits.py
@@ -0,0 +1,25 @@
+'''Given a 32-bit unsigned integer n, reverse the bits of the binary representation of n and return the result.
+
+Example 1:
+
+Input: n = 00000000000000000000000000010101
+
+Output:    2818572288 (10101000000000000000000000000000)
+Explanation: Reversing 00000000000000000000000000010101, which represents the unsigned integer 21, gives us 10101000000000000000000000000000 which represents the unsigned integer 2818572288.'''
+
+#bf
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        binary = ""
+        for i in range(32):
+            if n & (1 << i):
+                binary += "1"
+            else:
+                binary += "0"
+        res = 0
+        for i, bit in enumerate(binary[:: -1]):
+            if bit == '1':
+                res |= (1 << i)
+        return res
+#time complexity: O(1)
+#space complexity: O(1)
