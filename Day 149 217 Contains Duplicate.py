a95c3335a6040df4b1d007d23d43871f28f2e04e
Isaac <zihao599@gmail.com>
2026-06-27 16:26:10 -0700

    bf 217

---
+++ Day 149 217 Contains Duplicate.py
@@ -0,0 +1,30 @@
+'''Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+Constraints:
+
+0 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+'''
+
+#bf
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+#time complexity: O(n^2)
+#space complexity: O(1)
