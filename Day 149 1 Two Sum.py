dbbcf2baedc2fdb97b919f10c9c61151ff93e621
Isaac <zihao599@gmail.com>
2026-06-27 17:21:31 -0700

    sorting 1

--- Day 149 1 Two Sum.py
+++ Day 149 1 Two Sum.py
@@ -41,3 +41,24 @@ class Solution:
         return []
 #time complexity: O(n^2) because we are going through the entire array twice
 #space complexity: O(1) because we are not using any extra space
+
+#sorting
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        A = []
+        for i, num in enumerate(nums):
+            A.append([num, i]) # create a list of lists where each inner list contains the number and its index
+        A.sort() # sort the list of lists based on the numbers
+        i, j = 0, len(nums) - 1 # initialize two pointers, one at the start and one at the end of the list
+        while i < j:
+            cur = A[i][0] + A[j][0] # calculate the sum of the numbers at the two pointers
+            if cur == target:
+                return [min(A[i][1], A[j][1]), max(A[i][1], A[j][1])] # return the indices of the two numbers in sorted order
+            elif cur < target:
+                i += 1 # if the sum is less than the target, move the left pointer to the right to increase the sum
+            else:
+                j -= 1
+        return []
+#time complexity: O(nlogn) because we are sorting the list of lists, n because of sorting, logn because of the sort function, sorting function is O(nlogn) because it uses Timsort algorithm
+#space complexity: O(n) because we are creating a new list of lists to store the numbers and their indices
