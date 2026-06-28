23562ffed881c5e188b65be592857c69dc2f23de
Isaac <zihao599@gmail.com>
2026-06-27 17:02:07 -0700

    hashmap 242

--- Day 149 242 Valid Anagram.py
+++ Day 149 242 Valid Anagram.py
@@ -25,3 +25,16 @@ class Solution:
         return sorted(s) == sorted(t) #returns True if the sorted version of both strings are equal, otherwise returns False
 #time complexity: O(nlogn + mlogm) because of sorting, n and m are the lengths of the strings, logn and logm because of the sort function, sorting function is O(nlogn) because it uses Timsort algorithm
 #space complexity: O(n + m) because we are creating a new sorted version of the strings,
+
+#hashmap
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {} # create two dictionaries to store the counts of each character in the strings
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0) # get the current count of the character in the dictionary, if it doesn't exist, return 0
+        return countS == countT # returns True if the two dictionaries are equal, otherwise returns False
+#time complexity: O(n + m) because we are going through the entire strings once,
+#space complexity: O(1) since we have at mostt 26 characters in the English alphabet, the space used by the dictionaries is constant
