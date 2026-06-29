e8eef445e53c6f66c45d20d4d2964d6db8d3ba94
Isaac <zihao599@gmail.com>
2026-06-29 13:08:08 -0700

    hashmap 15

--- Day 151 15 3Sum.py
+++ Day 151 15 3Sum.py
@@ -34,6 +34,7 @@ Constraints:
 '''
 
 #bf
+from collections import defaultdict
 from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -47,4 +48,30 @@ class Solution:
                         res.add(tuple(temp))
         return [list(i) for i in res]
 #time complexity: O(n^3)
+#space complexity: O(n)
+
+#hashmap
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+        res = []
+        for i in range(len(nums)):
+            count[nums[i]] -= 1
+            if i and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums)):
+                count[nums[j]] -= 1
+                if j - 1 > i and nums[j] == nums[j - 1]:
+                    continue
+                target = -nums[i] - nums[j]
+                if count[target] > 0:
+                    res.append([nums[i], nums[j], target])
+            for j in range(i + 1, len(nums)):
+                count[nums[j]] += 1
+        return res
+#time complexity: O(n^2)
 #space complexity: O(n)
