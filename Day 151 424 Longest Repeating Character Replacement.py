ce8611d03087d05acf88e8b280b7037293dfc02d
Isaac <zihao599@gmail.com>
2026-06-29 16:05:09 -0700

    sliding window 424

--- Day 151 424 Longest Repeating Character Replacement.py
+++ Day 151 424 Longest Repeating Character Replacement.py
@@ -35,3 +35,22 @@ class Solution:
 #time complexity: O(n^2), where n is the length of the string. 
 # The outer loop iterates through each character in the string, and the inner loop iterates through the remaining characters to calculate the frequency of each character in the substring. In the worst case, this results in a time complexity of O(n^2).
 #space complexity: O(1), since the count dictionary can have at most 26 entries (one for each uppercase English letter), which is a constant space usage.
+
+#sliding window
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        res = 0
+        left = 0
+        maxf = 0
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)
+            maxf = max(maxf, count[s[right]])
+            while (right - left + 1) - maxf > k:
+                count[s[left]] -= 1
+                left += 1
+            res = max(res, right - left + 1)
+        return res
+#time complexity: O(n), where n is the length of the string. 
+#Each character is processed at most twice, once when it is added to the count dictionary and once when it is removed.
+#space complexity: O(1), since the count dictionary can have at most 26 entries (one for each uppercase English letter), which is a constant space usage.
