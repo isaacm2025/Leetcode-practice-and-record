91abc6ef33049d2ce5d791b369dd56a4d0573894
Isaac <zihao599@gmail.com>
2026-06-30 16:34:31 -0700

    deque 239

--- Day 152 239 Sliding Window Maximum.py
+++ Day 152 239 Sliding Window Maximum.py
@@ -54,3 +54,25 @@ class Solution:
         return output
 #time complexity: O(nlogk)
 #space complexity: O(n)
+
+#deque
+from typing import List
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        dq = deque()
+        l = r = 0
+        while r < len(nums):
+            while dq and nums[dq[-1]] < nums[r]:
+                dq.pop()
+            dq.append(r)
+            if l > dq[0]:
+                dq.popleft()
+            if (r + 1) >= k:
+                output.append(nums[dq[0]])
+                l += 1
+            r += 1
+        return output
+#time complexity: O(n)
+#space complexity: O(k)
