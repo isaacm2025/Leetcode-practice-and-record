f0971236a565e5942558902a71e44eaf301f40d0
Isaac <zihao599@gmail.com>
2026-06-30 15:44:25 -0700

    hashtable 567

--- Day 152 567 Permutation in String.py
+++ Day 152 567 Permutation in String.py
@@ -33,3 +33,24 @@ class Solution:
         return False
 #time complexity: O(n^3logn)
 #space complexity: O(n)
+
+#hash table
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count1 = {}
+        for c in s1:
+            count1[c] = 1 + count1.get(c, 0)
+        need = len(count1)
+        for i in range(len(s2)):
+            count2, cur = {}, 0
+            for j in range(i, len(s2)):
+                count2[s2[j]] = 1 + count2.get(s2[j], 0)
+                if count1.get(s2[j], 0) < count2[s2[j]]:
+                    break
+                if count1.get(s2[j], 0) == count2[s2[j]]:
+                    cur += 1
+                if cur == need:
+                    return True
+        return False
+#time complexity: O(n*m)
+#space complexity: O(1) because the size of the hash table is limited to 26 letters
