8355be57326dfbb6c63d6b0f87baccc59bf7e6cd
Isaac <zihao599@gmail.com>
2026-06-30 17:12:49 -0700

    two stack 155

--- Day 152 155 Min Stack.py
+++ Day 152 155 Min Stack.py
@@ -53,3 +53,22 @@ class MinStack:
         return mini
 #time complexity: O(n)
 #space complexity: O(n)
+
+#two stacks
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+    def top(self) -> int:
+        return self.stack[-1]
+    def getMin(self) -> int:
+        return self.minStack[-1]
+#time complexity: O(1)
+#space complexity: O(n)
