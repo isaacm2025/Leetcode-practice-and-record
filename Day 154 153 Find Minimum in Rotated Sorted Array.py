58fc34262775d64ec0050e0dbbd9d5da5e146280
Isaac <zihao599@gmail.com>
2026-07-02 14:58:07 -0700

    bs 153

--- Day 154 153 Find Minimum in Rotated Sorted Array.py
+++ Day 154 153 Find Minimum in Rotated Sorted Array.py
@@ -36,3 +36,22 @@ class Solution:
         return min(nums)
 #time complexity: O(n)
 #space complexity: O(1)
+
+#bs
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            if nums[left] < nums[right]:
+                res = min(res, nums[left])
+                break
+            mid = (left + right) // 2
+            res = min(res, nums[mid])
+            if nums[mid] >= nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return res
+#time complexity: O(log n)  
+#space complexity: O(1)
