49649cecce0d83dd0a94fee66a701c93cfcff3e1
Isaac <zihao599@gmail.com>
2026-07-02 15:33:19 -0700

    bs array 981

--- Day 154 981 Time Based Key-Value Store.py
+++ Day 154 981 Time Based Key-Value Store.py
@@ -47,3 +47,25 @@ class TimeMap:
         return "" if seen == 0 else self.keyStore[key][seen][-1]
 #time complexity: O(n) for get(), O(1) for set()
 #space complexity: O(n * m) where n is the number of keys and m is the number of timestamps for each key
+
+#bs(array)
+class TimeMap:
+    def __init__(self):
+        self.keyStore = {}
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keyStore:
+            self.keyStore[key] = []
+        self.keyStore[key].append([value, timestamp])
+    def get(self, key: str, timestamp: int) -> str:
+        res, values = "", self.keyStore.get(key, [])
+        left, right = 0, len(values) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if values[mid][1] <= timestamp:
+                res = values[mid][0]
+                left = mid + 1
+            else:
+                right = mid - 1
+        return res
+#time complexity: O(log n) for get(), O(1) for set()
+#space complexity: O(n * m) where n is the number of keys and m is the number of timestamps for each key
