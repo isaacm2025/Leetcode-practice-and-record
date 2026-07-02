95d49140d6c1a50a8102719f06e69c3eb7b4ad3a
Isaac <zihao599@gmail.com>
2026-07-02 15:10:17 -0700

    bs 33

--- Day 154 33 Search in Rotated Sorted Array.py
+++ Day 154 33 Search in Rotated Sorted Array.py
@@ -27,7 +27,7 @@ All values of nums are unique.
 nums is an ascending array that is possibly rotated.
 '''
 
-#bs
+#bf
 from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
@@ -36,4 +36,26 @@ class Solution:
                 return i
         return -1
 #time complexity: O(n)
+#space complexity: O(1)
+
+#bs
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
+#time complexity: O(log n)
 #space complexity: O(1)
