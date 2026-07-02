1e584182f8a9512d0f71bd2773cacf85c35fafb4
Isaac <zihao599@gmail.com>
2026-07-01 17:43:54 -0700

    staircase search 74

--- Day 153 74 Search a 2D Matrix.py
+++ Day 153 74 Search a 2D Matrix.py
@@ -39,3 +39,20 @@ class Solution:
         return False
 #time complexity: O(m*n)
 #space complexity: O(1)
+
+#staircase search
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        row, col = 0, n - 1
+        while row < m and col >= 0:
+            if matrix[row][col] > target:
+                col -= 1
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                return True
+        return False
+#time complexity: O(m+n)
+#space complexity: O(1)
+            
