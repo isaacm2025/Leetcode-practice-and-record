0f1f3a7112ef6a2ba0fe101aed9dcbe2c0aef990
Isaac <zihao599@gmail.com>
2026-07-01 17:27:06 -0700

    recursion bs 704

---
+++ Day 153 704 Binary Search.py
@@ -0,0 +1,47 @@
+'''You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+
+Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+
+Your solution must run in 
+O
+(
+l
+o
+g
+n
+)
+O(logn) time.
+
+Example 1:
+
+Input: nums = [-1,0,2,4,6,8], target = 4
+
+Output: 3
+Example 2:
+
+Input: nums = [-1,0,2,4,6,8], target = 3
+
+Output: -1
+Constraints:
+
+1 <= nums.length <= 10000.
+-10000 < nums[i], target < 10000
+All the integers in nums are unique.
+'''
+
+#recursive bs
+from typing import List
+class Solution:
+    def bs(self, nums: List[int], target: int, l: int, r: int) -> int:
+        if l > r:
+            return -1
+        mid = l + (r - l) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[mid] < target:
+            return self.bs(nums, target, mid + 1, r)
+        return self.bs(nums, target, l, mid - 1)
+    def search(self, nums: List[int], target: int) -> int:
+        return self.bs(nums, target, 0, len(nums) - 1)
+#time complexity: O(logn)
+#space complexity: O(logn)
