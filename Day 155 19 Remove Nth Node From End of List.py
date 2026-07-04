83aabcf3ab61e066294dd9aca96eeb9657a508b4
Isaac <zihao599@gmail.com>
2026-07-03 19:06:26 -0700

    two pointer 19

--- Day 155 19 Remove Nth Node From End of List.py
+++ Day 155 19 Remove Nth Node From End of List.py
@@ -43,3 +43,24 @@ class Solution:
         return head
 #time complexity: O(n)
 #space complexity: O(n)
+
+#two pointer
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        N = 0
+        cur = head
+        while cur:
+            N += 1
+            cur = cur.next
+        removeIndex = N - n
+        if removeIndex == 0:
+            return head.next
+        cur = head
+        for i in range(N - 1):
+            if i + 1 == removeIndex:
+                cur.next = cur.next.next
+                break
+            cur = cur.next
+        return head
+#time complexity: O(n)
+#space complexity: O(1)
