9e38761f582b0585968e6cf965d2fea3fbcab160
Isaac <zihao599@gmail.com>
2026-07-03 18:38:55 -0700

    reverse and merge 143

--- Day 155 143 Reorder Linked List.py
+++ Day 155 143 Reorder Linked List.py
@@ -56,3 +56,26 @@ class Solution:
         nodes[i].next = None
 #time complexity: O(n)
 #space complexity: O(n)
+
+#reverse and merge
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+#time complexity: O(n)
+#space complexity: O(1)
