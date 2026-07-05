38b1fb08dd5a49a45b7bbef8f841bca7534d0c21
Isaac <zihao599@gmail.com>
2026-07-04 22:32:34 -0700

    bfs 226

---
+++ Day 156 226 Invert Binary Tree.py
@@ -0,0 +1,51 @@
+'''You are given the root of a binary tree root. Invert the binary tree and return its root.
+
+Example 1:
+
+
+
+Input: root = [1,2,3,4,5,6,7]
+
+Output: [1,3,2,7,6,5,4]
+Example 2:
+
+
+
+Input: root = [3,2,1]
+
+Output: [3,1,2]
+Example 3:
+
+Input: root = []
+
+Output: []
+Constraints:
+
+0 <= The number of nodes in the tree <= 100.
+-100 <= Node.val <= 100
+'''
+
+from collections import deque
+from typing import Optional
+
+#bfs
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return root
+#time complexity O(n) where n is the number of nodes in the binary tree
+#space complexity O(n) where n is the number of nodes in the binary tree
