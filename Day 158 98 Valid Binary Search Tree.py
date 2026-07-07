55f624827d804a1cfc5f66e93264e3265ea791f4
Isaac <zihao599@gmail.com>
2026-07-06 20:14:49 -0700

    bfs 98

--- Day 158 98 Valid Binary Search Tree.py
+++ Day 158 98 Valid Binary Search Tree.py
@@ -43,3 +43,23 @@ class Solution:
         return dfs(root, float('-inf'), float('inf'))
 #time complexity: O(n)
 #space complexity: O(h) where h is the height of the tree
+
+#bfs
+from collections import deque
+from typing import Optional
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        q = deque([(root, float('-inf'), float('inf'))])
+        while q:
+            node, left, right = q.popleft()
+            if not (left <node.val < right):
+                return False
+            if node.left:
+                q.append((node.left, left, node.val))
+            if node.right:
+                q.append((node.right, node.val, right))
+        return True
+#time complexity: O(n)
+#space complexity: O(h) where h is the height of the tree
