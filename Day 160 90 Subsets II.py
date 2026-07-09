b205d5e73945ce92da1062cd70a93887c45ad670
Isaac <zihao599@gmail.com>
2026-07-08 20:34:32 -0700

    backtracking 90

--- Day 160 90 Subsets II.py
+++ Day 160 90 Subsets II.py
@@ -36,3 +36,20 @@ class Solution:
 #time complexity: O(n*2^n) where n is the length of the input array. The number of subsets of a set of size n is 2^n, and for each subset, we may take O(n) time to copy it to the result list.
 #space complexity: O(n) for the recursion stack and the subset list, where n is the length of the input array. The result set will take O(2^n) space to store all unique subsets, but this is not counted towards the space complexity since it is part of the output. The conversion of the set to a list will also take O(2^n) space
     
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        def backtrack(i, subset):
+            res.append(subset[::])
+            for j in range(i, len(nums)):
+                if j > i and nums[j] == nums[j - 1]:
+                    continue
+                subset.append(nums[j])
+                backtrack(j + 1, subset)
+                subset.pop()
+        backtrack(0, [])
+        return res
+#time complexity: O(n*2^n) where n is the length of the input array. The number of subsets of a set of size n is 2^n, and for each subset, we may take O(n) time to copy it to the result list.
+#space complexity: O(n) for the recursion stack and the subset list, where n is the length of the input array. The result list will take O(2^n) space to store all unique subsets, but this is not counted towards the space complexity since it is part of the output.
