2c3e4fa85d5ffe01064c7c50dd44cb24d1beaaaa
Isaac <zihao599@gmail.com>
2026-07-09 15:38:14 -0700

    optimal 79

--- Day 161 79 Word Search.py
+++ Day 161 79 Word Search.py
@@ -35,6 +35,7 @@ Constraints:
 board and word consists of only lowercase and uppercase English letters.'''
 
 #backtracking hashset
+from typing import List
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
@@ -58,3 +59,24 @@ class Solution:
         return False
 #time complexity: O(m * 4^n) where m is the number of cells in the board and n is the length of the word. In the worst case, we might explore all 4 directions for each character in the word.
 #space complexity: O(n) where n is the length of the word. The space is used by the recursion stack and the path set to keep track of visited cells.
+
+#optimal
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or r >= rows or c >= cols or word[i] != board[r][c] or board[r][c] == '#'):
+                return False
+            board[r][c] = '#'
+            res = (dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1))
+            board[r][c] = word[i]
+            return res
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+        return False
+#time complexity: O(m * 4^n) where m is the number of cells in the board and n is the length of the word. In the worst case, we might explore all 4 directions for each character in the word.
+#space complexity: O(n) where n is the length of the word. The space is used by the recursion stack.
