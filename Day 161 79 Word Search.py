9c1c738120b4e197c25455cdf44b39d7266b7949
Isaac <zihao599@gmail.com>
2026-07-09 13:07:06 -0700

    hashset backtracking 79

---
+++ Day 161 79 Word Search.py
@@ -0,0 +1,60 @@
+'''Given a 2-D grid of characters board and a string word, return true if the word is present in the grid, otherwise return false.
+
+For the word to be present it must be possible to form it with a path in the board with horizontally or vertically neighboring cells. The same cell may not be used more than once in a word.
+
+Example 1:
+
+
+
+Input: 
+board = [
+  ["A","B","C","D"],
+  ["S","A","A","T"],
+  ["A","C","A","E"]
+],
+word = "CAT"
+
+Output: true
+Example 2:
+
+
+
+Input: 
+board = [
+  ["A","B","C","D"],
+  ["S","A","A","T"],
+  ["A","C","A","E"]
+],
+word = "BAT"
+
+Output: false
+Constraints:
+
+1 <= board.length, board[i].length <= 5
+1 <= word.length <= 10
+board and word consists of only lowercase and uppercase English letters.'''
+
+#backtracking hashset
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        path = set()
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (min(r, c) < 0 or r >= rows or c >= cols or word[i] != board[r][c] or (r, c) in path):
+                return False
+            path.add((r, c))
+            res = (dfs(r + 1, c, i + 1) or
+                   dfs(r - 1, c, i + 1) or
+                   dfs(r, c + 1, i + 1) or
+                   dfs(r, c - 1, i + 1))
+            path.remove((r, c))
+            return res
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+        return False
+#time complexity: O(m * 4^n) where m is the number of cells in the board and n is the length of the word. In the worst case, we might explore all 4 directions for each character in the word.
+#space complexity: O(n) where n is the length of the word. The space is used by the recursion stack and the path set to keep track of visited cells.
