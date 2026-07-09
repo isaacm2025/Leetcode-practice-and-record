136ea2fb6d5a129c27d2e990a7f6f3c9eb00750d
Isaac <zihao599@gmail.com>
2026-07-08 20:09:48 -0700

    backtracking 46

--- Day 160 46 Permutations.py
+++ Day 160 46 Permutations.py
@@ -31,3 +31,22 @@ class Solution:
         return res
 #time complexity: O(n^2 *n!) where n is the length of the input array. The number of permutations of a set of size n is n!, and for each permutation, we may take O(n^2) time to generate it. The O(n^2) factor comes from the fact that we are inserting an element into a list of size n, which takes O(n) time, and we are doing this for each of the n elements in the list.
 #space complexity: O(n*n!) for the result list, where n is the length of the input array. The result list will take O(n*n!) space to store all permutations, but this is not counted towards the space complexity since it is part of the output.
+
+#backtracking
+from typing import List
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.nums = nums
+        self.backtrack(nums, 0)
+        return self.res
+    def backtrack(self, nums: List[int], idx: int):
+        if idx == len(nums):
+            self.res.append(nums[:])
+            return 
+        for i in range(idx, len(nums)):
+            nums[idx], nums[i] = nums[i], nums[idx]
+            self.backtrack(nums, idx + 1)
+            nums[idx], nums[i] = nums[i], nums[idx]
+#time complexity: O(n*n!) where n is the length of the input array. The number of permutations of a set of size n is n!, and for each permutation, we may take O(n) time to copy it to the result list.
+#space complexity: O(n! * n) for the result list, where n is the length of the input array. The result list will take O(n! * n) space to store all permutations, but this is not counted towards the space complexity since it is part of the output. The recursion stack will take O(n) space, where n is the length of the input array.
