9d255a9a6be95cd15c8000a0240707a31fc30963
Isaac <zihao599@gmail.com>
2026-07-09 17:27:57 -0700

    hashset backtracking 51

---
+++ Day 161 51 N-Queens.py
@@ -0,0 +1,58 @@
+'''The n-queens puzzle is the problem of placing n queens on an n x n chessboard so that no two queens can attack each other.
+
+A queen in a chessboard can attack horizontally, vertically, and diagonally.
+
+Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+Each solution contains a unique board layout where the queen pieces are placed. 'Q' indicates a queen and '.' indicates an empty space.
+
+You may return the answer in any order.
+
+Example 1:
+
+
+
+Input: n = 4
+
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There are two different solutions to the 4-queens puzzle.
+
+Example 2:
+
+Input: n = 1
+
+Output: [["Q"]]
+Constraints:
+
+1 <= n <= 8'''
+
+#hashset backtracking
+from typing import List
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDiag = set()
+        negDiag = set()
+        res = []
+        board = [["."] * n for i in range(n)]
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                backtrack(r + 1)
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+        backtrack(0)
+        return res
+#time complexity: O(n!) where n is the size of the chessboard. In the worst case, we might explore all possible placements of queens on the board.
+#space complexity: O(n^2) for the recursion stack and the sets used to track columns and diagonals.
