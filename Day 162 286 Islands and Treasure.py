f859093f58ee8c232a2a35cd223c07d0cd6f59df
Isaac <zihao599@gmail.com>
2026-07-10 22:44:49 -0700

    multi bfs 286

--- Day 162 286 Islands and Treasure.py
+++ Day 162 286 Islands and Treasure.py
@@ -78,3 +78,33 @@ class Solution:
                     grid[r][c] = bfs(r, c)
 #time complexity: O((m * n)^2)
 #space complexity: O(m * n) for the queue and visited array used in BFS.
+
+#multisource bfs
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        queue = deque()
+
+        def addCell(r, c):
+            if (min(r, c) < 0 or r == rows or c == cols or (r, c) in visit or grid[r][c] == -1):
+                return
+            visit.add((r, c))
+            queue.append([r, c])
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    queue.append([r, c])
+                    visit.add((r, c))
+        dist = 0
+        while queue:
+            for i in range(len(queue)):
+                r, c = queue.popleft()
+                grid[r][c] = dist
+                addCell(r + 1, c)
+                addCell(r - 1, c)
+                addCell(r, c + 1)
+                addCell(r, c - 1)
+            dist += 1
+#time complexity: O(m * n) where m is the number of rows and n is the number of columns in the grid. Each cell is processed at most once.
+#space complexity: O(m * n) for the queue and visited set used in BFS.
