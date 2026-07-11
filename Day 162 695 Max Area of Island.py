e51b647b60527a66749ea7ebb24edce2c2947902
Isaac <zihao599@gmail.com>
2026-07-10 22:10:39 -0700

    bfs 695

--- Day 162 695 Max Area of Island.py
+++ Day 162 695 Max Area of Island.py
@@ -26,6 +26,7 @@ Constraints:
 '''
 
 #dfs
+from collections import deque
 from typing import List
 class Solution:
     def maAreaOfisland(self, grid: List[List[int]]) -> int:
@@ -43,3 +44,33 @@ class Solution:
         return maxArea
 #time complexity: O(M * N) where M is the number of rows and N is the number of columns in the grid. In the worst case, we may have to visit every cell in the grid.
 #space complexity: O(M * N) in the worst case, where M is the number of rows and N is the number of columns in the grid. This is because we may have to store all the cells in the grid in the call stack during the DFS traversal.
+
+#bfs
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        maxArea = 0
+        def bfs(r, c):
+            queue = deque()
+            grid[r][c] = 0
+            queue.append((r, c))
+            res= 1
+            while queue:
+                row, col = queue.popleft()
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if nr < 0 or nc < 0 or nr >= rows or nc >= cols or grid[nr][nc] == 0:
+                        continue
+                    queue.append((nr, nc))
+                    grid[nr][nc] = 0
+                    res += 1
+            return res
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] ==1:
+                    maxArea = max(maxArea, bfs(r, c))
+        return maxArea
+#time complexity: O(M * N) where M is the number of rows and N is the number of columns in the grid. In the worst case, we may have to visit every cell in the grid.
+#space complexity: O(M * N) in the worst case, where M is the number of rows and N is the number of columns in the grid. This is because we may have to store all the cells in the grid in the queue during the BFS traversal.
