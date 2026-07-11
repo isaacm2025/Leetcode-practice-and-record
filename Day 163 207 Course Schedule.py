1a365d7fd926f8f63129111e53702aa700563a25
Isaac <zihao599@gmail.com>
2026-07-11 12:46:01 -0700

    dfs 207

---
+++ Day 163 207 Course Schedule.py
@@ -0,0 +1,58 @@
+'''You are given an array prerequisites where prerequisites[i] = [a, b] indicates that you must take course b first if you want to take course a.
+
+The pair [0, 1], indicates that must take course 1 before taking course 0.
+
+There are a total of numCourses courses you are required to take, labeled from 0 to numCourses - 1.
+
+Return true if it is possible to finish all courses, otherwise return false.
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[0,1]]
+
+Output: true
+Explanation: First take course 1 (no prerequisites) and then take course 0.
+
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[0,1],[1,0]]
+
+Output: false
+Explanation: In order to take course 1 you must take course 0, and to take course 0 you must take course 1. So it is impossible.
+
+Constraints:
+
+1 <= numCourses <= 1000
+0 <= prerequisites.length <= 1000
+prerequisites[i].length == 2
+0 <= a[i], b[i] < numCourses
+All prerequisite pairs are unique.'''
+
+#dfs
+from typing import List
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        visiting = set()
+        def dfs(crs):
+            if crs in visiting:
+                return False
+            if preMap[crs] == []:
+                return True
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
+            preMap[crs] =[]
+            return True
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
+#time complexity: O(N + P) where N is the number of courses and P is the number of prerequisites. 
+# This is because we are performing a DFS on each course, and each course can have at most P prerequisites, so the total time complexity is O(N + P).
+#space complexity: O(N + P) where N is the number of courses and P is the number of prerequisites. 
+# This is because we are storing the prerequisites in a dictionary, which takes up space proportional to the number of courses and prerequisites, and we are also using a set to keep track of the courses that are currently being visited in the DFS, which can take up space proportional to the number of courses
