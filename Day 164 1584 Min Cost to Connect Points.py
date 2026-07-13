f43671743d09536e318c8abcbcae0b2ae590f9be
Isaac <zihao599@gmail.com>
2026-07-12 22:46:33 -0700

    optimal prims 1584

---
+++ Day 164 1584 Min Cost to Connect Points.py
@@ -0,0 +1,44 @@
+'''You are given a 2-D integer array points, where points[i] = [xi, yi]. Each points[i] represents a distinct point on a 2-D plane.
+
+The cost of connecting two points [xi, yi] and [xj, yj] is the manhattan distance between the two points, i.e. |xi - xj| + |yi - yj|.
+
+Return the minimum cost to connect all points together, such that there exists exactly one path between each pair of points.
+
+Example 1:
+
+
+
+Input: points = [[0,0],[2,2],[3,3],[2,4],[4,2]]
+
+Output: 10
+Constraints:
+
+1 <= points.length <= 1000
+-1,000,000 <= xi, yi <= 1,000,000
+All pairs (xi, yi) are distinct.'''
+
+#optimal prim's algorithm
+import heapq
+from typing import List
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n, node = len(points), 0
+        dist = [100000000] * n
+        visit = [False] * n
+        res, edges = 0, 0
+        while edges < n - 1:
+            visit[node] = True
+            nextNode = -1
+            for i in range(n):
+                if visit[i]:
+                    continue
+                curDist = (abs(points[i][0] - points[node][0]) + (abs(points[i][1] - points[node][1])))
+                dist[i] = min(dist[i], curDist)
+                if nextNode == -1 or dist[i] < dist[nextNode]:
+                    nextNode = i
+            res += dist[nextNode]
+            node = nextNode
+            edges += 1
+        return res
+#time complexity: O(n^2) where n is the number of points. We have a while loop that runs n times, and inside the loop, we have a for loop that iterates over all n points to find the next node with the minimum distance. Therefore, the overall time complexity is O(n^2).
+#space complexity: O(n) where n is the number of points. We use two additional arrays, dist and visit, both of size n, to keep track of the minimum distances and visited nodes, respectively. Therefore, the overall space complexity is O(n).
