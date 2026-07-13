0fc22c4f1706c259b830e25a7c1835464f0f57e3
Isaac <zihao599@gmail.com>
2026-07-13 16:53:57 -0700

    bellman ford 787

---
+++ Day 165 787 Cheapest Flights Within K Stops.py
@@ -0,0 +1,39 @@
+'''There are n airports, labeled from 0 to n - 1, which are connected by some flights. You are given an array flights where flights[i] = [from_i, to_i, price_i] represents a one-way flight from airport from_i to airport to_i with cost price_i. You may assume there are no duplicate flights and no flights from an airport to itself.
+
+You are also given three integers src, dst, and k where:
+
+src is the starting airport
+dst is the destination airport
+src != dst
+k is the maximum number of stops you can make (not including src and dst)
+Return the cheapest price from src to dst with at most k stops, or return -1 if it is impossible.
+
+Example 1:
+
+
+
+Input: n = 4, flights = [[0,1,200],[1,2,100],[1,3,300],[2,3,100]], src = 0, dst = 3, k = 1
+
+Output: 500
+Explanation:
+The optimal path with at most 1 stop from airport 0 to 3 is shown in red, with total cost 200 + 300 = 500.
+Note that the path [0 -> 1 -> 2 -> 3] costs only 400, and thus is cheaper, but it requires 2 stops, which is more than k.'''
+
+#bellman ford algorithm
+from collections import defaultdict
+from typing import List
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float('inf')] * n
+        prices[src] = 0
+        for i in range(k + 1):
+            tmpPrices = prices.copy()
+            for s, d, p in flights:
+                if prices[s] == float('inf'):
+                    continue
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+            prices = tmpPrices
+        return -1 if prices[dst] == float('inf') else prices[dst]
+#time complexity: O(n + (m*k)) where n is the number of airports, m is the number of flights, and k is the maximum number of stops. The algorithm iterates through the flights k + 1 times, and for each iteration, it checks all flights to update the prices.
+#space complexity: O(n) where n is the number of airports. The algorithm uses an array of size n to store the minimum prices for each airport.
