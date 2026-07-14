c35a490fa129741d5a978fd7bd811309b6d4486b
Isaac <zihao599@gmail.com>
2026-07-14 15:57:40 -0700

    two pointer 647

--- Day 166 647 Palindromic Substrings.py
+++ Day 166 647 Palindromic Substrings.py
@@ -49,3 +49,21 @@ class Solution:
         return res
 #time complexity: O(n^2)
 #space complexity: O(n^2)
+
+#two pointer
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        for i in range(len(s)):
+            res += self.countPali(s, i, i)  # odd length
+            res += self.countPali(s, i, i + 1)  # even length
+        return res
+    def countPali(self, s, l, r):
+        res = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            res += 1
+            l -= 1
+            r += 1
+        return res
+#time complexity: O(n^2)
+#space complexity: O(1)
