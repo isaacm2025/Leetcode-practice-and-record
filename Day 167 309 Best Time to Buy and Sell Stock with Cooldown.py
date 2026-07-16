beae0432e3b008d84ac82e549c24255fa5f6c973
Isaac <zihao599@gmail.com>
2026-07-15 21:40:17 -0700

    dp op 309

---
+++ Day 167 309 Best Time to Buy and Sell Stock with Cooldown.py
@@ -0,0 +1,42 @@
+'''You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+
+You may buy and sell one NeetCoin multiple times with the following restrictions:
+
+After you sell your NeetCoin, you cannot buy another one on the next day (i.e., there is a cooldown period of one day).
+You may only own at most one NeetCoin at a time.
+You may complete as many transactions as you like.
+
+Return the maximum profit you can achieve.
+
+Example 1:
+
+Input: prices = [1,3,4,0,4]
+
+Output: 6
+Explanation: Buy on day 0 (price = 1) and sell on day 1 (price = 3), profit = 3-1 = 2. Then buy on day 3 (price = 0) and sell on day 4 (price = 4), profit = 4-0 = 4. Total profit is 2 + 4 = 6.
+
+Example 2:
+
+Input: prices = [1]
+
+Output: 0
+Constraints:
+
+1 <= prices.length <= 5000
+0 <= prices[i] <= 1000
+'''
+
+#dp op
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp1_buy, dp1_sell, dp2_buy = 0, 0, 0
+        for i in range(n - 1, -1, -1):
+            dp_buy = max(dp1_sell-prices[i], dp1_buy)
+            dp_sell = max(dp2_buy+prices[i], dp1_sell)
+            dp2_buy = dp1_buy
+            dp1_buy, dp1_sell = dp_buy, dp_sell
+        return dp1_buy
+#time complexity: O(n)
+#space complexity: O(1)
