cb1eaa011a0cfa03e0571489e8085bfc7080c287
Isaac <zihao599@gmail.com>
2026-07-15 19:56:10 -0700

    op dp 416

--- Day 167 416 Partition Equal Subset Sum.py
+++ Day 167 416 Partition Equal Subset Sum.py
@@ -55,3 +55,18 @@ class Solution:
         return dp[target]
 #time complexity: O(n*target)
 #space complexity: O(target)
+
+#optimal dp
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+        target = sum(nums) // 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+        for num in nums:
+            for j in range(target, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
+        return dp[target]
+#time complexity: O(n*target)
+#space complexity: O(target)
