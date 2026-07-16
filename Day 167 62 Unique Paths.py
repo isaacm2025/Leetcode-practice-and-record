c48ae0f0f4a89145f58b1abc737da0a56e9880a0
Isaac <zihao599@gmail.com>
2026-07-15 20:01:45 -0700

    dp 62

---
+++ Day 167 62 Unique Paths.py
@@ -0,0 +1,33 @@
+'''There is an m x n grid where you are allowed to move either down or to the right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that can be taken from the top-left corner of the grid (grid[0][0]) to the bottom-right corner (grid[m - 1][n - 1]).
+
+You may assume the output will fit in a 32-bit integer.
+
+Example 1:
+
+
+
+Input: m = 3, n = 6
+
+Output: 21
+Example 2:
+
+Input: m = 3, n = 3
+
+Output: 6
+Constraints:
+
+1 <= m, n <= 100'''
+
+#dp
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * (n+1) for _ in range(m + 1)]
+        dp[m - 1][n - 1] = 1
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                dp[i][j] += dp[i + 1][j] + dp[i][j + 1]
+        return dp[0][0]
+#time complexity: O(m*n)
+#space complexity: O(m*n)
