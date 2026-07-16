bf47aa754204bf678fc5e5891bb28e60730388c5
Isaac <zihao599@gmail.com>
2026-07-15 20:04:20 -0700

    space op 62

--- Day 167 62 Unique Paths.py
+++ Day 167 62 Unique Paths.py
@@ -31,3 +31,16 @@ class Solution:
         return dp[0][0]
 #time complexity: O(m*n)
 #space complexity: O(m*n)
+
+#dp space optimization
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n -2 , -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        return row[0]
+#time complexity: O(m*n)
+#space complexity: O(n)
