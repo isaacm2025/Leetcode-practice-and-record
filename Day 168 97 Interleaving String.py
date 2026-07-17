5ff264ebd6158ad1cb83610d431f2ac712f2134f
Isaac <zihao599@gmail.com>
2026-07-16 22:58:41 -0700

    dp optimal 97

--- Day 168 97 Interleaving String.py
+++ Day 168 97 Interleaving String.py
@@ -51,3 +51,28 @@ class Solution:
         return dp[0][0]
 #time complexity: O(n * m) where n is the length of s1 and m is the length of s2
 #space complexity: O(n * m) where n is the length of s1 and m is the length of s2
+
+#dp optimal
+class Solution:
+    def isInteleave(self, s1: str, s2: str, s3: str) -> bool:
+        m, n = len(s1) ,len(s2)
+        if m + n != len(s3):
+            return False
+        if n < m:
+            s1, s2 = s2, s1
+            m, n = n, m
+        dp = [False for _ in range(n + 1)]
+        dp[n] = True
+        for i in range(m, -1, -1):
+            nextDp = True if i == m else False
+            for j in range(n, -1, -1):
+                res = False if j < n else nextDp
+                if i < m and s1[i] == s3[i + j] and dp[j]:
+                    res = True
+                if j < n and s2[j] == s3[i + j] and nextDp:
+                    res = True
+                dp[j] = res
+                nextDp = dp[j]
+        return dp[0]
+#time complexity: O(n * m) where n is the length of s1 and m is the length of s2
+#space complexity: O(n) where n is the length of s2
