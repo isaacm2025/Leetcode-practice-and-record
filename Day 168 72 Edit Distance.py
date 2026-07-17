26337e1c22ccc48a600c8a2ac60b8fffb8ae86ab
Isaac <zihao599@gmail.com>
2026-07-16 23:25:39 -0700

    dp 72

---
+++ Day 168 72 Edit Distance.py
@@ -0,0 +1,50 @@
+'''You are given two strings word1 and word2, each consisting of lowercase English letters.
+
+You are allowed to perform three operations on word1 an unlimited number of times:
+
+Insert a character at any position
+Delete a character at any position
+Replace a character at any position
+Return the minimum number of operations to make word1 equal word2.
+
+Example 1:
+
+Input: word1 = "monkeys", word2 = "money"
+
+Output: 2
+Explanation:
+monkeys -> monkey (remove s)
+monkey -> money (remove k)
+
+Example 2:
+
+Input: word1 = "neatcdee", word2 = "neetcode"
+
+Output: 3
+Explanation:
+neatcdee -> neetcdee (replace a with e)
+neetcdee -> neetcde (remove last e)
+neetcde -> neetcode (insert o)
+
+Constraints:
+
+0 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.'''
+
+#dp
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[float('inf')] * (len(word2) + 1) for i in range(len(word1) + 1)]
+        for j in range(len(word2) + 1):
+            dp[len(word1)][j] = len(word2) - j
+        for i in range(len(word1) + 1):
+            dp[i][len(word2)] = len(word1) - i
+        for i in range(len(word1) - 1, -1, -1):
+            for j in range(len(word2) - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+        return dp[0][0]
+#time complexity: O(m * n) where m is the length of word1 and n is the length of word2
+#space complexity: O(m * n) where m is the length of word1 and n is the length of word2 for the dp array
