870a40cd2484550efea0e7f2847f7bc2ef8e035c
Isaac <zihao599@gmail.com>
2026-07-17 23:40:48 -0700

    greedy 45

--- Day 169 45 Jump Game II.py
+++ Day 169 45 Jump Game II.py
@@ -37,3 +37,19 @@ class Solution:
         return dp[0]
 #time complexity: O(n^2) where n is the length of nums
 #space complexity: O(n) where n is the length of nums
+
+#greedy
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        res = 0
+        l = r = 0
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
+#time complexity: O(n) where n is the length of nums
+#space complexity: O(1)
