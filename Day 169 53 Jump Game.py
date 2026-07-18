580989c5f98773bcabc75c1eed38796f8e140b15
Isaac <zihao599@gmail.com>
2026-07-17 23:31:47 -0700

    dp 53

---
+++ Day 169 53 Jump Game.py
@@ -0,0 +1,39 @@
+'''You are given an integer array nums where each element nums[i] indicates your maximum jump length at that position.
+
+Return true if you can reach the last index starting from index 0, or false otherwise.
+
+Example 1:
+
+Input: nums = [1,2,0,1,0]
+
+Output: true
+Explanation: First jump from index 0 to 1, then from index 1 to 3, and lastly from index 3 to 4.
+
+Example 2:
+
+Input: nums = [1,2,1,0,1]
+
+Output: false
+Constraints:
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 1000
+'''
+
+#dp
+
+from typing import List
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [False] * n
+        dp[-1] = True
+        for i in range(n - 2, -1, -1):
+            end = min(n, i + nums[i] + 1)
+            for j in range(i + 1, end):
+                if dp[j]:
+                    dp[i] = True
+                    break
+        return dp[0]
+#time complexity: O(n^2) where n is the length of nums
+#space complexity: O(n) where n is the length of nums
