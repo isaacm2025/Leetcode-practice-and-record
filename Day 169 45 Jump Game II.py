4d8b491194e1a9a02f95694ca11b2c35679a8c96
Isaac <zihao599@gmail.com>
2026-07-17 23:38:02 -0700

    dp 45

---
+++ Day 169 45 Jump Game II.py
@@ -0,0 +1,39 @@
+'''You are given an array of integers nums, where nums[i] represents the maximum length of a jump towards the right from index i. For example, if you are at nums[i], you can jump to any index i + j where:
+
+j <= nums[i]
+i + j < nums.length
+You are initially positioned at nums[0].
+
+Return the minimum number of jumps to reach the last position in the array (index nums.length - 1). You may assume there is always a valid answer.
+
+Example 1:
+
+Input: nums = [2,4,1,1,1,1]
+
+Output: 2
+Explanation: Jump from index 0 to index 1, then jump from index 1 to the last index.
+
+Example 2:
+
+Input: nums = [2,1,2,1,0]
+
+Output: 2
+Constraints:
+
+1 <= nums.length <= 1000
+0 <= nums[i] <= 100'''
+
+#dp
+from typing import List
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1000000] * n
+        dp[-1] = 0
+        for i in range(n - 2, -1, -1):
+            end = min(n, i + nums[i] + 1)
+            for j in range(i + 1, end):
+                dp[i] = min(dp[i], dp[j] + 1)
+        return dp[0]
+#time complexity: O(n^2) where n is the length of nums
+#space complexity: O(n) where n is the length of nums
