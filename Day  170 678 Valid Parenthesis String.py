ce449988af7aa42fe0119ab3d1c5e8c00cfd0f01
Isaac <zihao599@gmail.com>
2026-07-18 21:23:21 -0700

    greedy 678

--- Day  170 678 Valid Parenthesis String.py
+++ Day  170 678 Valid Parenthesis String.py
@@ -69,3 +69,25 @@ class Solution:
         return not left
 #time complexity: O(n)
 #space complexity: O(n)
+
+#greedy
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftMin, leftMax = 0, 0
+        for c in s:
+            if c == "(":
+                leftMin += 1
+                leftMax += 1
+            elif c == ")":
+                leftMin -= 1
+                leftMax -= 1
+            else:
+                leftMin -= 1
+                leftMax += 1
+            if leftMax < 0:
+                return False
+            if leftMin < 0:
+                leftMin = 0
+        return leftMin == 0
+#time complexity: O(n)
+#space complexity: O(1)
