4292ef1c64ef9eb002d926dde3fa516804e15be0
Isaac <zihao599@gmail.com>
2026-07-18 21:17:45 -0700

    stack 678

--- Day  170 678 Valid Parenthesis String.py
+++ Day  170 678 Valid Parenthesis String.py
@@ -45,3 +45,27 @@ class Solution:
         return dp[0]
 #time complexity: O(n^2)
 #space complexity: O(n)
+
+#stack
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left = []
+        star = []
+        for i, c in enumerate(s):
+            if c == '(':
+                left.append(i)
+            elif c == '*':
+                star.append(i)
+            else:
+                if not left and not star:
+                    return False
+                if left:
+                    left.pop()
+                else:
+                    star.pop()
+        while left and star:
+            if left.pop() > star.pop():
+                return False
+        return not left
+#time complexity: O(n)
+#space complexity: O(n)
