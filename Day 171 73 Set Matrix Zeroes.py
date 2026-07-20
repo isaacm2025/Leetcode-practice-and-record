caa24ff42e1296ae91e410a9f7c5375d70fa3733
Isaac <zihao599@gmail.com>
2026-07-19 22:56:04 -0700

    sp op 73

--- Day 171 73 Set Matrix Zeroes.py
+++ Day 171 73 Set Matrix Zeroes.py
@@ -56,3 +56,29 @@ class Solution:
                     matrix[r][c] = 0
 #time complexity: O(m * n) since we are iterating through the matrix
 #space complexity: O(m + n) since we are using two additional arrays to keep track
+
+#optimal
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else: 
+                        rowZero = True
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0    
+#time complexity: O(m * n) since we are iterating through the matrix
+#space complexity: O(1) since we are not using any additional space
