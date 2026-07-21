1810c169df831559bc0e4bf34b77e17970bd0556
Isaac <zihao599@gmail.com>
2026-07-20 17:46:31 -0700

    fast and slow pointer 202

--- Day 172 202 Non-Cyclical Number.py
+++ Day 172 202 Non-Cyclical Number.py
@@ -54,3 +54,28 @@ class Solution:
         return output
 #time complexity: O(logn)
 #space complexity: O(logn)
+
+#fast and slow pointer
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        slow, fast = n, self.sumOfSquares(n)
+        power = lam = 1
+        while slow != fast:
+            if power == lam:
+                slow = fast
+                power *= 2
+                lam = 0
+            fast = self.sumOfSquares(fast)
+            lam += 1
+        return True if fast == 1 else False
+    def sumOfSquares(self, n: int) -> int:
+        output = 0
+        while n:
+            digit = n % 10
+            digit = digit * digit
+            output += digit
+            n //= 10
+        return output
+#time complexity: O(logn)
+#space complexity: O(1)
+    
