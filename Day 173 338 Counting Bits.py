1f0bbdd654ecf23734ffcee6038456ef6d9955bd
Isaac <zihao599@gmail.com>
2026-07-21 17:26:25 -0700

    bit manipulation 338

---
+++ Day 173 338 Counting Bits.py
@@ -0,0 +1,33 @@
+'''Given an integer n, count the number of 1's in the binary representation of every number in the range [0, n].
+
+Return an array output where output[i] is the number of 1's in the binary representation of i.
+
+Example 1:
+
+Input: n = 4
+
+Output: [0,1,1,2,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+
+Constraints:
+
+0 <= n <= 1000'''
+
+#bit manipulation
+from typing import List
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0] * (n + 1)
+        for i in range(1, n + 1):
+            num = i
+            while num != 0:
+                res[i] += 1
+                num &= num - 1
+        return res
+#time complexity: O(nlogn)
+#space complexity: O(n)
