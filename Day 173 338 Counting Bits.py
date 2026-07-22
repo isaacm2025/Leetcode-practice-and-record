9596f8fb244a7beaa36f580a5a2270865a4e8547
Isaac <zihao599@gmail.com>
2026-07-21 17:30:12 -0700

    dp 338

--- Day 173 338 Counting Bits.py
+++ Day 173 338 Counting Bits.py
@@ -31,3 +31,17 @@ class Solution:
         return res
 #time complexity: O(nlogn)
 #space complexity: O(n)
+
+
+#dp
+class Solution:
+    def countBitss(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset *= 2
+            dp[i] = 1 + dp[i - offset]
+        return dp
+#time complexity: O(n)
+#space complexity: O(n)
