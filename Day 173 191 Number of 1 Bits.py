294a28da811e60f94621255358552422d95f172b
Isaac <zihao599@gmail.com>
2026-07-21 17:21:32 -0700

    optimal 191

--- Day 173 191 Number of 1 Bits.py
+++ Day 173 191 Number of 1 Bits.py
@@ -24,3 +24,14 @@ class Solution:
         return res
 #time complexity: O(1)
 #space complexity: O(1)
+
+#optimal
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            res += 1
+            n &= n - 1
+        return res
+#time complexity: O(1)
+#space complexity: O(1)
