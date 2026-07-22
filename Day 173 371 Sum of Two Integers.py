1c374a0b7190b279073de0acd52c54253573b001
Isaac <zihao599@gmail.com>
2026-07-21 18:07:36 -0700

    bit mani 371

--- Day 173 371 Sum of Two Integers.py
+++ Day 173 371 Sum of Two Integers.py
@@ -18,6 +18,19 @@ Constraints:
 #bf
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        return a + b
+        return sum([a, b])
+#time complexity: O(1)
+#space complexity: O(1)
+
+#bit manipulation
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        maxInt = 0x7FFFFFFF
+        while b != 0:
+            carry = (a & b) << 1 #it means (a & b) is the carry, and we need to shift it left by 1 to add it to the next bit
+            a = (a ^ b) & mask #it means (a ^ b) is the sum without carry, and we need to mask it to 32 bits
+            b = carry & mask #it means we need to mask the carry to 32 bits
+        return a if a <= maxInt else ~(a ^ mask) #if a is less than or equal to maxInt, we return a, otherwise we return the negative of (a ^ mask) which is the two's complement of a, ~ means bitwise NOT, so ~(a ^ mask) is the two's complement of a, which is the negative of a
 #time complexity: O(1)
 #space complexity: O(1)
