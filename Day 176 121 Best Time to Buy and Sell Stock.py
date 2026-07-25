bfdc4d080bfd82061c857e47409e9323b613b683
Isaac <zihao599@gmail.com>
2026-07-24 22:41:37 -0700

    two pointer 121

--- Day 176 121 Best Time to Buy and Sell Stock.py
+++ Day 176 121 Best Time to Buy and Sell Stock.py
@@ -37,3 +37,18 @@ class Solution:
         return res
 #time complexity: O(n^2)
 #space complexity: O(1)
+
+#two pointer
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        res = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                res = max(res, prices[r] - prices[l])
+            else:
+                l = r
+            r += 1
+        return res
+#time complexity: O(n)
+#space complexity: O(1)
