adf8ab72ee4ad78bad81a62d27c131ab1a7d4da6
Isaac <zihao599@gmail.com>
2026-07-24 22:59:41 -0700

    hashtable 567

---
+++ Day 176 567 Permutation in String.py
@@ -0,0 +1,42 @@
+'''You are given two strings s1 and s2.
+
+Return true if s2 contains a permutation of s1, or false otherwise. That means if a permutation of s1 exists as a substring of s2, then return true.
+
+Both strings only contain lowercase letters.
+
+Example 1:
+
+Input: s1 = "abc", s2 = "lecabee"
+
+Output: true
+Explanation: The substring "cab" is a permutation of "abc" and is present in "lecabee".
+
+Example 2:
+
+Input: s1 = "abc", s2 = "lecaabee"
+
+Output: false
+Constraints:
+
+1 <= s1.length, s2.length <= 1000'''
+
+#hash table
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count1 = {}
+        for c in s1:
+            count1[c] = count1.get(c, 0) + 1
+        need = len(count1)
+        for i in range(len(s2)):
+            count2, cur = {}, 0
+            for j in range(i, len(s2)):
+                count2[s2[j]] = 1 + count2.get(s2[j], 0)
+                if count1.get(s2[j], 0) < count2[s2[j]]:
+                    break
+                if count1.get(s2[j], 0) == count2[s2[j]]:
+                    cur += 1
+                if cur == need:
+                    return True
+        return False
+#time complexity: O(n * m) where n is the length of s2 and m is the length of s1
+#space complexity: O(m) where m is the length of s1
