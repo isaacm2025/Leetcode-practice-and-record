fb3b46b925b8ab6c6338a95839c8c0ce421f6792
Isaac <zihao599@gmail.com>
2026-07-26 18:45:48 -0700

    iterative bs 704

--- Day 178 704 Binary Search.py
+++ Day 178 704 Binary Search.py
@@ -28,13 +28,27 @@ Constraints:
 -10000 < nums[i], target < 10000
 All the integers in nums are unique.'''
 
-#itrattion
+#itration
 from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
+        while l < r:
+            mid = l + ((r - l) // 2)
+            if nums[mid] >= target:
+                r = mid
+            elif nums[mid] < target:
+                l = mid + 1
+        return l if (l < len(nums) and nums[l] == target) else -1
+#time complexity: O(logn)
+#space complexity: O(1)
+
+#iteration bs
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
         while l <= r:
-            mid = (l + r) // 2
+            mid = l + ((r - l) // 2)
             if nums[mid] > target:
                 r = mid - 1
             elif nums[mid] < target:
