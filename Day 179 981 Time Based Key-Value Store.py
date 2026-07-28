ae3226a83c6e9354da396017070b0f60c3301734
Isaac <zihao599@gmail.com>
2026-07-27 22:59:27 -0700

    bs 981

--- Day 179 981 Time Based Key-Value Store.py
+++ Day 179 981 Time Based Key-Value Store.py
@@ -48,3 +48,25 @@ class TimeMap:
         return "" if seen == 0 else self.keyStore[key][seen][-1]
 #time complexity: O(n) for get(), O(1) for set()
 #space complexity: O(n * m)
+
+#bs
+from collections import defaultdict
+
+from sortedcontainers import SortedDict
+
+class TimeMap:
+    def __init__(self):
+        self.m = defaultdict(SortedDict)
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.m[key][timestamp] = value
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.m:
+            return ""
+        timestamps = self.m[key]
+        idx = timestamps.bisect_right(timestamp) - 1
+        if idx >= 0:
+            closest_time = timestamps.iloc[idx]
+            return self.m[key][closest_time]
+        return ""
+#time complexity: O(log n) for get(), O(log n) for set()
+#space complexity: O(n * m)
