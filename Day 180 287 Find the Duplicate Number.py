2828b41fe2b72b29a4c2328d0a84d453a4ca6081
Isaac <zihao599@gmail.com>
2026-07-28 22:58:05 -0700

    hashset 287

---
+++ Day 180 287 Find the Duplicate Number.py
@@ -0,0 +1,41 @@
+'''You are given an array of integers nums containing n + 1 integers. Each integer in nums is in the range [1, n] inclusive.
+
+There is exactly one repeated integer in nums, and every other integer appears at most once.
+
+Return the repeated integer.
+
+Example 1:
+
+Input: nums = [1,2,3,2,2]
+
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3,4,4]
+
+Output: 4
+Follow-up: Can you solve the problem without modifying the array nums and using 
+O
+(
+1
+)
+O(1) extra space?
+
+Constraints:
+
+1 <= n <= 10,000
+nums.length == n + 1
+1 <= nums[i] <= n
+'''
+#hashset
+from typing import List
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
+        return -1
+#time complexity: O(n)
+#space complexity: O(n)
