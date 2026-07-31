2b6623e6896b6dfef243e7ca966455f43663643f
Isaac <zihao599@gmail.com>
2026-07-30 21:47:12 -0700

    bfs 100

--- Day 182 100 Same Binary Tree.py
+++ Day 182 100 Same Binary Tree.py
@@ -46,3 +46,26 @@ class Solution:
             return False
 #time complexity: O(n)
 #space complexity: O(n) in worst case, O(log n) in best case
+
+#bfs
+from collections import deque
+from typing import Optional
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        q1 = deque([p])
+        q2 = deque([q])
+        while q1 and q2:
+            for _ in range(len(q1)):
+                node1 = q1.popleft()
+                node2 = q2.popleft()
+                if node1 is None and node2 is None:
+                    continue
+                if node1 is None or node2 is None or node1.val != node2.val:
+                    return False
+                q1.append(node1.left)
+                q1.append(node1.right)
+                q2.append(node2.left)
+                q2.append(node2.right)
+        return True
+#time complexity: O(n)
+#space complexity: O(n)
