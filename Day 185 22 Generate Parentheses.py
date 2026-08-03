e27d0984eb26ae9ce3bfd5abf73edecf29085147
Isaac <zihao599@gmail.com>
2026-08-02 23:48:56 -0700

    dp 22

---
+++ Day 185 22 Generate Parentheses.py
@@ -0,0 +1,32 @@
+'''You are given an integer n. Return all well-formed parentheses strings that you can generate with n pairs of parentheses.
+
+Example 1:
+
+Input: n = 1
+
+Output: ["()"]
+Example 2:
+
+Input: n = 3
+
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+You may return the answer in any order.
+
+Constraints:
+
+1 <= n <= 7'''
+
+#dp
+from typing import List
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = [[] for _ in range(n + 1)]
+        res[0] = ['']
+        for i in range(1, n + 1):
+            for j in range(i):
+                for left in res[j]:
+                    for right in res[i - j - 1]:
+                        res[i].append('(' + left + ')' + right)
+        return res[n]
+#time complexity: O(4^n / sqrt(n)) because the number of valid parentheses
+#space complexity: O(n) because we store the result in a list of size n + 1
