5a5e2b7b4fe34ac1c8bcb2421240c267752681e2
Isaac <zihao599@gmail.com>
2026-08-04 22:17:01 -0700

    bfs 200

--- Day 187 200 Number of Islands.py
+++ Day 187 200 Number of Islands.py
@@ -47,3 +47,32 @@ class Solution:
         return islands
 #time complexity: O(M * N) where M is the number of rows and N is the number of columns in the grid. We visit each cell once.
 #space complexity: O(M * N) in the worst case, where the grid is filled with land and the recursion stack goes as deep as the number of cells in the grid.
+
+#bfs
+from typing import List
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = "0"
+            q.append((r, c))
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr +row, dc + col
+                    if nr < 0 or nc <0 or nr >= ROWS or nc >= COLS or grid[nr][nc] == '0':
+                        continue
+                    grid[nr][nc] = '0'
+                    q.append((nr, nc))
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    islands += 1
+                    bfs(r, c)
+        return islands
+#time complexity: O(M * N) where M is the number of rows and N is the number of columns in the grid. We visit each cell once.
+#space complexity: O(min(M, N)) where M is the number of rows and N is the number of columns in the grid. In the worst case, the queue can contain all the cells in a row or a column
