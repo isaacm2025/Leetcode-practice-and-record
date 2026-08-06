ba9c4b10c5f068727c3a42e897f58f079e6e66ef
Isaac <zihao599@gmail.com>
2026-08-05 22:37:50 -0700

    bfs 261

--- Day 188 261 Graph Valid Tree.py
+++ Day 188 261 Graph Valid Tree.py
@@ -53,3 +53,30 @@ class Solution:
         return dfs(0, -1) and len(visit) == n
 #time complexity: O(N + E) where N is the number of nodes and E is the number of edges
 #space complexity: O(N + E) where N is the number of nodes and E is the number of edges
+
+#bfs
+from typing import List
+from collections import deque
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > n - 1:
+            return False
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        visit = set()
+        queue = deque([(0, -1)])
+        visit.add(0)
+        while queue:
+            node, parent = queue.popleft()
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if nei in visit:
+                    return False
+                visit.add(nei)
+                queue.append((nei, node))
+        return len(visit) == n
+#time complexity: O(N + E) where N is the number of nodes and E is the number of edges
+#space complexity: O(N + E) where N is the number of nodes and E is the number of edges
