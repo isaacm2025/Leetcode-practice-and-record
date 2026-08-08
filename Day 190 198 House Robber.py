f2e32dc5cd107c041c400e86e0267bfea22eb521
Isaac <zihao599@gmail.com>
2026-08-07 23:11:21 -0700

    space op 198

--- Day 190 198 House Robber.py
+++ Day 190 198 House Robber.py
@@ -34,3 +34,15 @@ class Solution:
         return dfs(0)
 #time complexity: O(2^n) where n is the length of nums
 #space complexity: O(n) where n is the length of nums
+
+#dp space optimized
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+#time complexity: O(n) where n is the length of nums
+#space complexity: O(1) where n is the length of nums
