7e7df19716f8d8b4e8437ab4a5f3cffd24e70f6e
Isaac <zihao599@gmail.com>
2026-08-09 21:00:30 -0700

    hashsett 416

--- Day 192 416 Partition Equal Subset Sum.py
+++ Day 192 416 Partition Equal Subset Sum.py
@@ -48,3 +48,23 @@ class Solution:
         return (dp & (1 << target) != 0) # check if the bit at position target is set
 #time complexity: O(n * target)
 #space complexity: O(target)
+
+#hashset
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+        dp = set()
+        dp.add(0)
+        target = sum(nums) // 2
+        for i in range(len(nums) -1, -1, -1):
+            nextDp = set()
+            for t in dp:
+                if (t + nums[i] == target):
+                    return True
+                nextDp.add(t + nums[i])
+                nextDp.add(t)
+            dp = nextDp
+        return False
+#time complexity: O(n * target)
+#space complexity: O(target)
