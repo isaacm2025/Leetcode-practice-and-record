f359b5905e004bb39d9c8307c51cb0345792aee3
Isaac <zihao599@gmail.com>
2026-08-09 20:37:21 -0700

    dp 300

---
+++ Day 192 300 Longest Increasing Subsequence.py
@@ -0,0 +1,41 @@
+'''Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from the given sequence by deleting some or no elements without changing the relative order of the remaining characters.
+
+For example, "cat" is a subsequence of "crabt".
+Example 1:
+
+Input: nums = [9,1,4,2,3,3,7]
+
+Output: 4
+Explanation: The longest increasing subsequence is [1,2,3,7], which has a length of 4.
+
+Example 2:
+
+Input: nums = [0,3,1,3,2,3]
+
+Output: 4
+Constraints:
+
+1 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000'''
+
+#dp
+from typing import List
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [-1] * n
+
+        def dfs(i):
+            if dp[i] != -1:
+                return dp[i]
+            LIS = 1
+            for j in range(i + 1, n):
+                if nums[i] < nums[j]:
+                    LIS = max(LIS, 1 + dfs(j))
+            dp[i] = LIS
+            return LIS
+        return max(dfs(i) for i in range(n))   
+#time complexity: O(n^2)
+#space complexity: O(n)
