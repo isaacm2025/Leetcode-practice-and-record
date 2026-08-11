70995d7942b3556472fdf827ea60fdbea3c822ff
Isaac <zihao599@gmail.com>
2026-08-10 22:39:15 -0700

    space op 115

--- Day 193 115 Distinct Subsequences.py
+++ Day 193 115 Distinct Subsequences.py
@@ -53,3 +53,21 @@ class Solution:
 #time complexity: O(n * m)
 #space complexity: O(n * m)
             
+
+#dp space optimized
+from typing import List
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        dp = [0] * (n + 1)
+        nextDp = [0] * (n + 1)
+        dp[n] = nextDp[n] = 1
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                nextDp[j] = dp[j]
+                if s[i] == t[j]:
+                    nextDp[j] += dp[j + 1]
+            dp = nextDp[:]
+        return dp[0]
+#time complexity: O(n * m)
+#space complexity: O(n)
