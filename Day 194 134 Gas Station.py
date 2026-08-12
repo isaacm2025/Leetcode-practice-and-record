91a3a36f3317b588434d9810831824e9260fd629
Isaac <zihao599@gmail.com>
2026-08-11 22:41:20 -0700

    greedy 134

--- Day 194 134 Gas Station.py
+++ Day 194 134 Gas Station.py
@@ -58,3 +58,19 @@ class Solution:
         return -1
 #time complexity: O(n^2) where n is the length of gas
 #space complexity: O(1)
+
+#greedy
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        total = 0
+        res = 0
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+            if total < 0:
+                total = 0
+                res = i + 1
+        return res
+#time complexity: O(n) where n is the length of gas
+#space complexity: O(1)
