aa1dd1e7690cc2ed74d7d3a7ef415323c719d20c
Isaac <zihao599@gmail.com>
2026-08-11 21:26:44 -0700

    10 dp

---
+++ Day 194 10 Regular Expression Matching.py
@@ -0,0 +1,54 @@
+'''You are given an input string s consisting of lowercase english letters, and a pattern p consisting of lowercase english letters, as well as '.', and '*' characters.
+
+Return true if the pattern matches the entire input string, otherwise return false.
+
+'.' Matches any single character
+'*' Matches zero or more of the preceding element.
+
+Example 1:
+
+Input: s = "aa", p = ".b"
+
+Output: false
+Explanation: Regardless of which character we choose for the '.' in the pattern, we cannot match the second character in the input string.
+
+
+Example 2:
+
+Input: s = "nnn", p = "n*"
+
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'n'. We choose 'n' to repeat three times.
+
+
+Example 3:
+
+Input: s = "xyz", p = ".*z"
+
+Output: true
+Explanation: The pattern ".*" means zero or more of any character, so we choose ".." to match "xy" and "z" to match "z".
+
+
+Constraints:
+
+1 <= s.length <= 20
+1 <= p.length <= 20
+Each appearance of '*', will be preceded by a valid character or '.'.'''
+
+#dp
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for i in range(len(s) + 1)]
+        dp[len(s)][len(p)] = True
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) -1, -1, -1):
+                match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+                if (j + 1 < len(p) and p[j + 1] == '*'):
+                    dp[i][j] = dp[i][j + 2]
+                    if match:
+                        dp[i][j] = dp[i][j] or dp[i + 1][j]
+                elif match:
+                    dp[i][j] = dp[i + 1][j + 1]
+        return dp[0][0]
+#time complexity: O(m*n) where m is the length of s and n is the length of p
+#space complexity: O(m*n) where m is the length of s and n is the length of p
