50e482ad04ae052ae0ea9e8508e69fbbb4b1da26
Isaac <zihao599@gmail.com>
2026-08-12 12:41:42 -0700

    greedy 435

--- Day 195 435 Non-overlapping Intervals.py
+++ Day 195 435 Non-overlapping Intervals.py
@@ -51,3 +51,19 @@ class Solution:
         return n - maxNonOverlap
 #time complexity: O(n^2)
 #space complexity: O(n)
+
+#greedy
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        res = 0
+        prevEnd = intervals[0][1]
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
+                res += 1
+                prevEnd = min(prevEnd, end)
+        return res
+#time complexity: O(nlogn)
+#space complexity: O(1)
