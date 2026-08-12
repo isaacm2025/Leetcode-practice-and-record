0fc898f029db05de234ebf5f5db0226a048f1591
Isaac <zihao599@gmail.com>
2026-08-12 11:52:57 -0700

    stack 678

---
+++ Day 195 678 Valid Parenthesis String.py
@@ -0,0 +1,51 @@
+'''You are given a string s which contains only three types of characters: '(', ')' and '*'.
+
+Return true if s is valid, otherwise return false.
+
+A string is valid if it follows all of the following rules:
+
+Every left parenthesis '(' must have a corresponding right parenthesis ')'.
+Every right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+A '*' could be treated as a right parenthesis ')' character or a left parenthesis '(' character, or as an empty string "".
+Example 1:
+
+Input: s = "((**)"
+
+Output: true
+Explanation: One of the '*' could be a ')' and the other could be an empty string.
+
+Example 2:
+
+Input: s = "(((*)"
+
+Output: false
+Explanation: The string is not valid because there is an extra '(' at the beginning, regardless of the extra '*'.
+
+Constraints:
+
+1 <= s.length <= 100'''
+
+#stack
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left = []
+        star = []
+        for i, ch in enumerate(s):
+            if ch == '(':
+                left.append(i)
+            elif ch == '*':
+                star.append(i)
+            else:
+                if not left and not star:
+                    return False
+                if left:
+                    left.pop()
+                else:
+                    star.pop()
+        while left and star:
+            if left.pop() > star.pop():
+                return False
+        return not left
+#time complexity: O(n)
+#space complexity: O(n)
