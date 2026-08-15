13fa789690135dd6229c2fc16d2e3e20188b8ef5
Isaac <zihao599@gmail.com>
2026-08-14 18:03:43 -0700

    iteration 66

--- Day 197 66 Plus One.py
+++ Day 197 66 Plus One.py
@@ -32,3 +32,18 @@ class Solution:
             return self.plusOne(digits[:-1]) + [0]
 #time complexity: O(n)
 #space complexity: O(n)
+
+#iterative
+from typing import List
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        return [1] + digits
+#time complexity: O(n)
+#space complexity: O(n) because of the new list created when all digits are 9
