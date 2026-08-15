d5048ca5c257cbc82d6e9cc623cc1d93f93b4b76
Isaac <zihao599@gmail.com>
2026-08-14 18:50:52 -0700

    sorting 136

--- Day 197 136 Single Number.py
+++ Day 197 136 Single Number.py
@@ -57,3 +57,18 @@ class Solution:
         return list(seen)[0]
 #time complexity: O(n)
 #space complexity: O(n)
+
+#sorting
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                i += 2
+            else:
+                return nums[i]
+        return nums[-1]
+#time complexity: O(nlogn)
+#space complexity: O(1)
