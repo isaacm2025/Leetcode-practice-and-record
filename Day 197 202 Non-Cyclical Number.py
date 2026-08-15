c106e17302740f1aead3d027655ec13b42bc1436
Isaac <zihao599@gmail.com>
2026-08-14 17:55:43 -0700

    two pointers 202

--- Day 197 202 Non-Cyclical Number.py
+++ Day 197 202 Non-Cyclical Number.py
@@ -53,3 +53,23 @@ class Solution:
         return output
 #time complexity: O(logn)
 #space complexity: O(logn)
+
+#two pointers
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        slow, fast = n, self.sumOfSquares(n)
+        while slow != fast:
+            fast = self.sumOfSquares(fast)
+            fast = self.sumOfSquares(fast)
+            slow = self.sumOfSquares(slow)
+        return True if fast == 1 else False
+    def sumOfSquares(self, n: int) -> int:
+        output = 0
+        while n:
+            digit = n % 10
+            digit = digit ** 2
+            output += digit
+            n //= 10
+        return output
+#time complexity: O(logn)
+#space complexity: O(1)
