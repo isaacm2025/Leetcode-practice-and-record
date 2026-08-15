fec7a613fc5f9a6707545ff321ad5043048b1d04
Isaac <zihao599@gmail.com>
2026-08-14 18:15:06 -0700

    interation 50

--- Day 197 50 Pow(x, n).py
+++ Day 197 50 Pow(x, n).py
@@ -56,3 +56,21 @@ class Solution:
         return res if n >= 0 else 1 / res
 #time complexity: O(logn)
 #space complexity: O(logn) because of the recursion stack
+
+#binary exponentiation iterative
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+        if n == 0:
+            return 1
+        res = 1
+        power = abs(n)
+        while power:
+            if power & 1:
+                res *= x
+            x *= x
+            power >>= 1
+        return res if n >= 0 else 1 / res
+#time complexity: O(logn)
+#space complexity: O(1)
