13ca934e49f03d8c2024e50d9c5084c37895331c
Isaac <zihao599@gmail.com>
2026-08-15 23:40:43 -0700

    optimal bit mani 371

--- Day 198 371 Sum of Two Integers.py
+++ Day 198 371 Sum of Two Integers.py
@@ -20,3 +20,16 @@ class Solution:
         return sum([a, b])
 #time complexity: O(1)
 #space complexity: O(1)
+
+#optimal
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        maxInt = 0x7FFFFFFF
+        while b!=0:
+            carry = (a & b) << 1
+            a = (a ^ b) & mask
+            b = carry & mask
+        return a if a <= maxInt else ~(a ^ mask) # it means if a is greater than maxInt, then return the negative value of a, otherwise return a as it is.
+#time complexity: O(1)
+#space complexity: O(1)
