aabbc75cbd12071a08e554a4bb462d239c002849
Isaac <zihao599@gmail.com>
2026-08-17 12:11:45 -0700

    hashset 128

--- Day 200 128 Longest Consecutive Sequence.py
+++ Day 200 128 Longest Consecutive Sequence.py
@@ -59,3 +59,18 @@ class Solution:
         return res
 #time complexity: O(nlogn)
 #space: O(1) or O(n)
+
+#hashset
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+        for num in numSet:
+            if (num - 1) not in numSet:
+                length = 1
+                while (num + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+#time O(n)
+#space O(n)
