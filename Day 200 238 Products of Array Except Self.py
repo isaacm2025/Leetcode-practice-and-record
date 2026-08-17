51b92b5e19466e70dc8e75d60a6e6f92a4ccfe03
Isaac <zihao599@gmail.com>
2026-08-17 11:34:32 -0700

    optimal 238

--- Day 200 238 Products of Array Except Self.py
+++ Day 200 238 Products of Array Except Self.py
@@ -41,3 +41,19 @@ class Solution:
         return res
 #time com: O(n^2)
 #space complexity: O(1)
+
+#optimal
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+#time com: O(n)
+#space com: O(n)
