c13ef291a228802591bed1c5fec3bc266f4ca48a
Isaac <zihao599@gmail.com>
2026-08-16 23:13:09 -0700

    hashmap 242

--- Day 199 242 Valid Anagram.py
+++ Day 199 242 Valid Anagram.py
@@ -25,3 +25,16 @@ class Solution:
         return sorted(s) == sorted(t)
 #time complexity: O(nlogn)
 #space complexity: O(n)
+
+#hashmap
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+#time complexity: O(n)
+#space complexity: O(n)
