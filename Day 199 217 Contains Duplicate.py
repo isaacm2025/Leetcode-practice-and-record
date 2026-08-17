9aa311133a0f77951913893f49bf00f51eb7240b
Isaac <zihao599@gmail.com>
2026-08-16 22:58:23 -0700

    hashset 217

--- Day 199 217 Contains Duplicate.py
+++ Day 199 217 Contains Duplicate.py
@@ -39,3 +39,16 @@ class Solution:
         return False
 #time complexity: O(nlogn)
 #space complexity: O(1)
+
+#hashset
+from typing import List
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True # return True if a duplicate is found
+            seen.add(num) # add the number to the set if it's not a duplicate
+        return False
+#time complexity: O(n)
+#space complexity: O(n)
