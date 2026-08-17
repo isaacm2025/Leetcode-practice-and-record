ac40a7a2c434bb4607295ec0c9e0a6aae088c61a
Isaac <zihao599@gmail.com>
2026-08-16 23:26:41 -0700

    hashmap 1

--- Day 199 1 Two Sum.py
+++ Day 199 1 Two Sum.py
@@ -41,3 +41,18 @@ class Solution:
         return []
 #time complexity: O(n^2)
 #space complexity: O(1)
+
+#hashmap
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}
+        for i, n in enumerate(nums):
+            indices[n] = i
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+        return []
+#time complexity: O(n)
+#space complexity: O(n)
