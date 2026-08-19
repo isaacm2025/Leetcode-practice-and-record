8043875b62d7e1ce0a7076c7e76525e8cef2fce0
Isaac <zihao599@gmail.com>
2026-08-18 23:43:06 -0700

    sliding window 424

---
+++ Day 201 424 Longest Repeating Character Replacement.py
@@ -0,0 +1,40 @@
+'''You are given a string s consisting of only uppercase english characters and an integer k. You can choose up to k characters of the string and replace them with any other uppercase English character.
+
+After performing at most k replacements, return the length of the longest substring which contains only one distinct character.
+
+Example 1:
+
+Input: s = "XYYX", k = 2
+
+Output: 4
+Explanation: Either replace the 'X's with 'Y's, or replace the 'Y's with 'X's.
+
+Example 2:
+
+Input: s = "AAABABB", k = 1
+
+Output: 5
+Constraints:
+
+1 <= s.length <= 100,000
+0 <= k <= s.length
+s consists of only uppercase english characters.'''
+
+#sliding window
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        charSet = set(s)
+        for c in charSet:
+            count = l = 0
+            for r in range(len(s)):
+                if s[r] == c:
+                    count += 1
+                while (r - l + 1) - count > k:
+                    if s[l] == c:
+                        count -= 1
+                    l += 1
+                res = max(res, r - l + 1)
+        return res
+#time: O(n * m) where m is the number of distinct characters in s
+#space: O(m) where m is the number of distinct characters in s
