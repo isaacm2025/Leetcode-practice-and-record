8ef9652a7ba464b1bb6e41f43e23319958f47589
Isaac <zihao599@gmail.com>
2026-08-18 23:07:31 -0700

    bf 11

---
+++ Day 201 11 Container With Most Water.py
@@ -0,0 +1,43 @@
+'''You are given an integer array heights where heights[i] represents the height of the 
+i
+t
+h
+i 
+th
+  bar.
+
+You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+
+
+Example 1:
+
+
+
+Input: height = [1,7,2,5,4,7,3,6]
+
+Output: 36
+Explanation: The bars at indices 1 and 7 have heights 7 and 6. The container has width 7 - 1 = 6 and height min(7, 6) = 6, so it can store 6 * 6 = 36 units of water. This is the maximum possible area.
+
+
+Example 2:
+
+Input: height = [2,2,2]
+
+Output: 4
+
+Constraints:
+
+2 <= height.length <= 100,000
+0 <= height[i] <= 10,000'''
+
+#bf
+from typing import List
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        res = 0
+        for i in range(len(heights)):
+            for j in range(i + 1, len(heights)):
+                res = max(res, min(heights[i], heights[j]) * (j - i))
+        return res
+#time: O(n^2)
+#space: O(1)
