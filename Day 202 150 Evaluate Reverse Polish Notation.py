228a971d843fbcb5856ff9306f89aa53d5d12a66
Isaac <zihao599@gmail.com>
2026-08-19 23:26:38 -0700

    stack 150

--- Day 202 150 Evaluate Reverse Polish Notation.py
+++ Day 202 150 Evaluate Reverse Polish Notation.py
@@ -39,3 +39,25 @@ class Solution:
         return dfs()
 #time complexity: O(n)
 #space complexity: O(n)
+
+#stack
+from typing import List
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
+#time complexity: O(n)
+#space complexity: O(n)
