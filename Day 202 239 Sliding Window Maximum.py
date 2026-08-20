827eb6a44ef8124e063e53a0df585092d3bea06c
Isaac <zihao599@gmail.com>
2026-08-19 22:56:58 -0700

    heap 239

--- Day 202 239 Sliding Window Maximum.py
+++ Day 202 239 Sliding Window Maximum.py
@@ -35,3 +35,20 @@ class Solution:
         return res
 #time complexity: O(n*k)
 #space complexity: O(n)
+
+#heap
+import heapq
+from typing import List
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        res = []
+        for i in range(len(nums)):
+            heapq.heappush(heap, (-nums[i], i))
+            if i >= k - 1:
+                while heap[0][1] <= i - k:
+                    heapq.heappop(heap)
+                res.append(-heap[0][0])
+        return res
+#time complexity: O(nlogk)
+#space complexity: O(k)
