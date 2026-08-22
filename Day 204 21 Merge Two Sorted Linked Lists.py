824ab5180fe66195bbfca37268c6c71fdef3500b
Isaac <zihao599@gmail.com>
2026-08-21 23:34:18 -0700

    21 iteration

---
+++ Day 204 21 Merge Two Sorted Linked Lists.py
@@ -0,0 +1,49 @@
+'''You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+
+The new list should be made up of nodes from list1 and list2.
+
+Example 1:
+
+
+
+Input: list1 = [1,2,4], list2 = [1,3,5]
+
+Output: [1,1,2,3,4,5]
+Example 2:
+
+Input: list1 = [], list2 = [1,2]
+
+Output: [1,2]
+Example 3:
+
+Input: list1 = [], list2 = []
+
+Output: []
+Constraints:
+
+0 <= The length of the each list <= 100.
+-100 <= Node.val <= 100'''
+
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = node = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
+            else:
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+        node.next = list1 or list2
+        return dummy.next
+#time complexity: O(n + m) where n and m are the lengths of the two lists
+#space complexity: O(1)
