67848164b96d9252b29874b35bf8725697280541
Isaac <zihao599@gmail.com>
2026-08-23 23:38:19 -0700

    104 dfs

---
+++ Day 206 104 Maximum Depth of Binary Tree.py
@@ -0,0 +1,35 @@
+'''Given the root of a binary tree, return its depth.
+
+The depth of a binary tree is defined as the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+
+
+
+Input: root = [1,2,3,null,null,4]
+
+Output: 3
+Example 2:
+
+Input: root = []
+
+Output: 0
+Constraints:
+
+0 <= The number of nodes in the tree <= 100.
+-100 <= Node.val <= 100'''
+
+# Definition for a binary tree node.
+#dfs
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+#time complexity is O(n) where n is the number of nodes in the tree
+#space complexity is O(h) where h is the height of the tree
