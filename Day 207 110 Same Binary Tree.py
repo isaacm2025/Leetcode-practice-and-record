583998e3e7da4975c003a4d41bcbcb4cc4e40960
Isaac <zihao599@gmail.com>
2026-08-24 22:14:17 -0700

    iteration 110

--- Day 207 110 Same Binary Tree.py
+++ Day 207 110 Same Binary Tree.py
@@ -47,3 +47,19 @@ class Solution:
             return False
 #time complexity: O(n)
 #space complexity: best case O(logn), worst case O(n)
+
+#iterative
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        stack = [(p, q)]
+        while stack:
+            node1, node2 = stack.pop()
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            stack.append((node1.right, node2.right))
+            stack.append((node1.left, node2.left))
+        return True
+#time complexity: O(n)
+#space complexity: O(n)
