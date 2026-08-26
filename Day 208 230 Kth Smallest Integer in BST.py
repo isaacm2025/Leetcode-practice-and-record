09c78cee1edf854ccae33f733daf9806eaf8127b
Isaac <zihao599@gmail.com>
2026-08-25 21:36:01 -0700

    230 morris

--- Day 208 230 Kth Smallest Integer in BST.py
+++ Day 208 230 Kth Smallest Integer in BST.py
@@ -61,3 +61,30 @@ class Solution:
         return arr[k - 1]
 #time complexity: O(n)
 #space complexity: O(n)
+
+#morris Traversal
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        curr = root
+        while curr:
+            if not curr.left:
+                k -= 1
+                if k == 0:
+                    return curr.val
+                curr = curr.right
+            else:
+                prev = curr.left
+                while prev.right and prev.right != curr:
+                    prev = prev.right
+                if not prev.right:
+                    prev.right = curr
+                    curr = curr.left
+                else:
+                    prev.right = None
+                    k -= 1
+                    if k == 0:
+                        return curr.val
+                    curr = curr.right
+        return -1
+#time complexity: O(n)
+#space complexity: O(1)
