22d6aa5ad555a60943ea669e56331ba062eb0bf5
Isaac <zihao599@gmail.com>
2026-08-25 21:06:56 -0700

    bfs 1448

--- Day 208 1448 Count Good Nodes in Binary Tree.py
+++ Day 208 1448 Count Good Nodes in Binary Tree.py
@@ -41,3 +41,22 @@ class Solution:
         return dfs(root, root.val)
 #time complexity: O(n)
 #space complexity: O(n)
+
+#bfs
+from collections import deque
+class Solution:
+    def goodNodes(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        q = deque()
+        q.append((root, -float('inf')))
+        while q:
+            node, maxVal = q.popleft()
+            if node.val >= maxVal:
+                res += 1
+            if node.left:
+                q.append((node.left, max(maxVal, node.val)))
+            if node.right:
+                q.append((node.right, max(maxVal, node.val)))
+        return res
+#time complexity: O(n)
+#space complexity: O(n)
