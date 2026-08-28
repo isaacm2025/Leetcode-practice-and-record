21093885e260150a826c7d0832f9f9a79c532dac
Isaac <zihao599@gmail.com>
2026-08-28 11:26:53 -0700

    optimal 79

--- Day 221 79 Word Search.py
+++ Day 221 79 Word Search.py
@@ -57,3 +57,24 @@ class Solution:
         return False
 #time complexity: O(m * 4^n) because we have to explore 4 directions for each cell and the maximum length of the word is n
 #space complexity: O(n) for the recursion stack and the path set
+
+#optimal backtrack
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (min(r, c) < 0 or r >= ROWS or c >= COLS or word[i] != board[r][c]):
+                return False
+            board[r][c] = '#'
+            res = (dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1))
+            board[r][c] = word[i]
+            return res
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+        return False
+#time complexity: O(m * 4^n) because we have to explore 4 directions for each cell and the maximum length of the word is n
+#space complexity: O(n) for the recursion stack
