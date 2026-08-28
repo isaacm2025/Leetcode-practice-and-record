a3738a6b44d497d9bb34a30f3e68fa241bcb5911
Isaac <zihao599@gmail.com>
2026-08-28 11:49:31 -0700

    iteration 17

--- Day 221 17 Letter Combinations of a Phone Number.py
+++ Day 221 17 Letter Combinations of a Phone Number.py
@@ -50,3 +50,29 @@ class Solution:
         return res
 #time complexity: O(n * 4^m) where n is the number of digits that map to 3 letters and m is the number of digits that map to 4 letters
 #space complexity: O(n) for the recursion stack and the result list
+
+#iteration
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        res = [""]
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        for digit in digits:
+            temp = []
+            for curStr in res:
+                for c in digitToChar[digit]:
+                    temp.append(curStr + c)
+            res = temp
+        return res
+#time complexity: O(n * 4^m) where n is the number of digits that map to 3 letters and m is the number of digits that map to 4 letters
+#space complexity: O(n) for the result list
