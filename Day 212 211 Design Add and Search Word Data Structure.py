6afbb5d2fce98565a91df1f4ac7f38634b942945
Isaac <zihao599@gmail.com>
2026-08-29 21:51:28 -0700

    211 dfs

--- Day 212 211 Design Add and Search Word Data Structure.py
+++ Day 212 211 Design Add and Search Word Data Structure.py
@@ -45,8 +45,43 @@ class WordDictionary:
                 if w[i] == word[i] or word[i] == '.':
                     i += 1
                 else:
-                    breaks
+                    break
             if i == len(w):
                 return True
         return False
-#time com
+#time complexity: O(1) for addWord, O(n*m) for search where n is the number of words and m is the length of the word being searched
+#space complexity: O(n*m) where n is the number of words and m is the average length of the words
+
+#dfs(trie)
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+        return dfs(0, self.root)
+#time complexity: O(n) for addWord, O(m) for search where n is the length of the word being added and m is the length of the word being searched
+#space complexity: O(n) for addWord, O(m) for search where n is the length of the word being added and m is the length of the word being searched
