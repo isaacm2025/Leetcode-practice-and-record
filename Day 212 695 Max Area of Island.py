ce681362ba06a1f5ad6edb5ace1530642f553b88
Isaac <zihao599@gmail.com>
2026-08-29 22:40:24 -0700

    bfs 695

--- Day 212 695 Max Area of Island.py
+++ Day 212 695 Max Area of Island.py
@@ -43,3 +43,35 @@ class Solution:
         return Area
 #time complexity: O(n*m) where n is the number of rows and m is the number of columns
 #space complexity: O(n*m) where n is the number of rows and m is the number of columns (in the worst case, the entire grid is filled with land and the recursion stack will be of size n*m)
+
+#bfs
+from typing import List
+from collections import deque
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        area = 0
+
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = 0
+            q.append((r, c))
+            count = 1
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if (nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] == 0):
+                        continue
+                    grid[nr][nc] = 0
+                    q.append((nr, nc))
+                    count += 1
+            return count
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    area = max(area, bfs(r, c))
+        return area
+#time complexity: O(n*m) where n is the number of rows and m is the number of columns
+#space complexity: O(min(n, m)) where n is the number of rows and m is the number of columns (in the worst case, the entire grid is filled with land and the queue will be of size min(n, m))
