986550ce6932e14ad84ab6f5232038bd0e00c70d
Isaac <zihao599@gmail.com>
2026-08-30 23:50:58 -0700

    dfs 210

--- Day 213 210 Course Schedule II.py
+++ Day 213 210 Course Schedule II.py
@@ -53,3 +53,26 @@ class Solution:
         return output[::-1] 
 #time complexity: O(V + E), where V is the number of courses and E is the number of prerequisites.
 #space complexity: O(V + E), where V is the number of courses and E is the number of prerequisites.
+
+#dfs
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = [[] for i in range(numCourses)]
+        indegree = [0] * numCourses
+        for nxt, pre in prerequisites:
+            adj[pre].append(nxt)
+            indegree[nxt] += 1
+        output = []
+        def dfs(node):
+            output.append(node)
+            indegree[node] -= 1
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    dfs(nei)
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                dfs(i)
+        return output if len(output) == numCourses else []
+#time complexity: O(V + E), where V is the number of courses and E is the number of prerequisites.
+#space complexity: O(V + E), where V is the number of courses and E is the number of prerequisites.
