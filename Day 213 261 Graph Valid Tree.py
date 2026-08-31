a6af11eb313c4d9557a0ea5911f00ca343a2fb1b
Isaac <zihao599@gmail.com>
2026-08-30 23:55:58 -0700

    dfs 261

---
+++ Day 213 261 Graph Valid Tree.py
@@ -0,0 +1,45 @@
+'''Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+
+
+Example 1:
+
+
+
+Input: n = 5, edges = [[0,1],[0,2],[0,3],[1,4]]
+
+Output: true
+
+Example 2:
+
+
+
+Input: n = 5, edges = [[0,1],[1,2],[2,3],[1,3],[1,4]]
+
+Output: false
+'''
+
+#dfs
+from collections import defaultdict
+from typing import List
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        adj = [[] for i in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        visited = set()
+        def dfs(node, parent):
+            if node in visited:
+                return False
+            visited.add(node)
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if not dfs(nei, node):
+                    return False
+            return True
+        return dfs(0, -1) and len(visited) == n
+#time complexity: O(V + E), where V is the number of nodes and E is the number of edges.
+#space complexity: O(V + E), where V is the number of nodes and E is the number of edges.
