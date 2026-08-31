d7f12de9c6a9195cde802e8b28820bb99dd04e83
Isaac <zihao599@gmail.com>
2026-08-30 23:40:48 -0700

    topological sort 207

--- Day 213 207 Course Schedule.py
+++ Day 213 207 Course Schedule.py
@@ -57,3 +57,29 @@ class Solution:
         return True
 #time complexity: O(V + E) where V is the number of courses and E is the number of prerequisites
 #space complexity: O(V + E) where V is the number of courses and E is the number of prerequisites
+
+#topological sort
+from typing import List
+from collections import deque
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = [0] * numCourses
+        adj = [[] for i in range(numCourses)]
+        for src, dst in prerequisites:
+            indegree[dst] += 1
+            adj[src].append(dst)
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        finish = 0
+        while q:
+            node = q.popleft()
+            finish += 1
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        return finish == numCourses
+#time complexity: O(V + E) where V is the number of courses and E is the number of prerequisites
+#space complexity: O(V + E) where V is the number of courses and E is the number of prerequisites
