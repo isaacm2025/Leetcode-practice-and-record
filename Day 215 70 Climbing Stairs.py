c52088c7aed7b11c6571b38cb1bfdfb6c2219470
Isaac <zihao599@gmail.com>
2026-09-01 10:54:43 -0700

    dp 70

--- Day 215 70 Climbing Stairs.py
+++ Day 215 70 Climbing Stairs.py
@@ -36,3 +36,16 @@ class Solution:
         return dfs(0)
 #time complexity: O(2^n) where n is the number of steps
 #space complexity: O(n) where n is the number of steps
+
+#dp
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+#time complexity: O(n) where n is the number of steps
+#space complexity: O(n) where n is the number of steps
