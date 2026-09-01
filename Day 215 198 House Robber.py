e4f73fd7106ab98ae4437ec40d4ee83e5acf2196
Isaac <zihao599@gmail.com>
2026-09-01 11:16:55 -0700

    dp 198

--- Day 215 198 House Robber.py
+++ Day 215 198 House Robber.py
@@ -37,3 +37,18 @@ class Solution:
         return dfs(0)
 #time complexity: O(2^n) where n is the length of the input array
 #space complexity: O(n) where n is the length of the input array
+
+#dp
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums)
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if memo[i] != -1:
+                return memo[i]
+            memo[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            return memo[i]
+        return dfs(0)
+#time complexity: O(n) where n is the length of the input array
+#space complexity: O(n) where n is the length of the input array
