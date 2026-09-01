d624602f85a7e4a398e03028f0a3d45646e8e037
Isaac <zihao599@gmail.com>
2026-09-01 11:19:13 -0700

    dp sp op 198

--- Day 215 198 House Robber.py
+++ Day 215 198 House Robber.py
@@ -52,3 +52,15 @@ class Solution:
         return dfs(0)
 #time complexity: O(n) where n is the length of the input array
 #space complexity: O(n) where n is the length of the input array
+
+#dp sp op
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+#time complexity: O(n) where n is the length of the input array
+#space complexity: O(1)
