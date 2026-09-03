71fb6547728815f0eebdd40bd6f83aaf88644026
Isaac <zihao599@gmail.com>
2026-09-02 19:46:10 -0700

    bfs 322

--- Day 216 322 Coin Change.py
+++ Day 216 322 Coin Change.py
@@ -52,3 +52,30 @@ class Solution:
         return -1 if minCoins >= 1e9 else minCoins
 #time complexity: O(n*t) where n is the number of coins and t is the amount
 #space complexity: O(t) 
+
+#bfs
+from typing import List
+from collections import deque
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        q = deque([0])
+        seen = [False] * (amount + 1)
+        seen[0] = True
+        res = 0
+        while q:
+            res += 1
+            for _ in range(len(q)):
+                cur = q.popleft()
+                for coin in coins:
+                    nxt = cur + coin
+                    if nxt == amount:
+                        return res
+                    if nxt > amount or seen[nxt]:
+                        continue
+                    seen[nxt] = True
+                    q.append(nxt)
+        return -1
+#time complexity: O(n*t) where n is the number of coins and t is the amount
+#space complexity: O(t)
