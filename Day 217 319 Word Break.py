b79a17c596cedc28be98006f4fad060cc5a3fe14
Isaac <zihao599@gmail.com>
2026-09-03 17:29:26 -0700

    recursion 319

---
+++ Day 217 319 Word Break.py
@@ -0,0 +1,46 @@
+'''Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of dictionary words.
+
+You are allowed to reuse words in the dictionary an unlimited number of times. You may assume all dictionary words are unique.
+
+Example 1:
+
+Input: s = "neetcode", wordDict = ["neet","code"]
+
+Output: true
+Explanation: Return true because "neetcode" can be split into "neet" and "code".
+
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen","ape"]
+
+Output: true
+Explanation: Return true because "applepenapple" can be split into "apple", "pen" and "apple". Notice that we can reuse words and also not use all the words.
+
+Example 3:
+
+Input: s = "catsincars", wordDict = ["cats","cat","sin","in","car"]
+
+Output: false
+Constraints:
+
+1 <= s.length <= 200
+1 <= wordDict.length <= 100
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+'''
+
+#recursion
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def dfs(i):
+            if i == len(s):
+                return True
+            for w in wordDict:
+                if ((i + len(w)) <= len(s) and s[i:i + len(w)] == w):
+                    if dfs(i + len(w)):
+                        return True
+            return False
+        return dfs(0)
+#time complexity: O(t * m^n) where t is the number of words in the dictionary, m is the average length of the words, and n is the length of the string s. This is because for each character in s, we may check all words in wordDict.
+#space complexity: O(n) where n is the length of the string s. This is because we are using a recursive function that can go as deep as the length of s in the worst case.
