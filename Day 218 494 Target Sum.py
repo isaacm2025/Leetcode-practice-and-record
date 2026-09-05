89ff39a6d217ba6e111957ac3bf5bb3fd4ed77e1
Isaac <zihao599@gmail.com>
2026-09-04 20:46:23 -0700

    dp 494

--- Day 218 494 Target Sum.py
+++ Day 218 494 Target Sum.py
@@ -34,3 +34,18 @@ class Solution:
         return backtrack(0, 0)
 #time complexity: O(2^n), n is the length of nums
 #space complexity: O(n), n is the length of nums
+
+#dp
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = {}
+        def backtrack(i, total):
+            if i == len(nums):
+                return 1 if total == target else 0
+            if (i, total) in dp:
+                return dp[(i, total)]
+            dp[(i, total)] = backtrack(i + 1, total + nums[i]) + backtrack(i + 1, total - nums[i])
+            return dp[(i, total)]
+        return backtrack(0, 0)
+#time complexity: O(n * sum), n is the length of nums, sum is the sum of nums
+#space complexity: O(n * sum), n is the length of nums, sum is the sum of nums
