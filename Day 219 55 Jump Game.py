ad408d71dd02c87166e22bbe20dd28f72c4698b7
Isaac <zihao599@gmail.com>
2026-09-05 20:48:18 -0700

    dp 55

--- Day 219 55 Jump Game.py
+++ Day 219 55 Jump Game.py
@@ -34,3 +34,20 @@ class Solution:
         return dfs(0)
 #time complexity: O(n!)
 #space complexity: O(n)
+
+#dp
+from typing import List
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [False] * n
+        dp[-1] = True
+        for i in range(n - 2, -1, -1):
+            end = min(n, i + nums[i] + 1)
+            for j in range(i + 1, end):
+                if dp[j]:
+                    dp[i] = True
+                    break
+        return dp[0]
+#time complexity: O(n^2)
+#space complexity: O(n)
