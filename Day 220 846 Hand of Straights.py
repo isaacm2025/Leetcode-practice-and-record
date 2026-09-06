20c55f006360e15ba4babfaa83a267e3f9edc74c
Isaac <zihao599@gmail.com>
2026-09-06 10:33:45 -0700

    846 hashmap

--- Day 220 846 Hand of Straights.py
+++ Day 220 846 Hand of Straights.py
@@ -42,3 +42,26 @@ class Solution:
         return True
 #time complexity: O(nlogn)
 #space complexity: O(n)
+
+#hashmap
+from collections import Counter
+from typing import List
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize:
+            return False
+        count = Counter(hand)
+        for num in hand:
+            start = num
+            while count[start - 1]:
+                start -= 1
+            while start <= num:
+                while count[start]:
+                    for i in range(start, start + groupSize):
+                        if not count[i]:
+                            return False
+                        count[i] -= 1
+                start += 1
+        return True
+#time complexity: O(nlogn)
+#space complexity: O(n)
