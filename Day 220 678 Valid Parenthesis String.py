f10f86a3a4d3c200bfabe788e42d78ce72f19519
Isaac <zihao599@gmail.com>
2026-09-06 10:51:47 -0700

    dp 678

---
+++ Day 220 678 Valid Parenthesis String.py
@@ -0,0 +1,52 @@
+'''You are given a string s which contains only three types of characters: '(', ')' and '*'.
+
+Return true if s is valid, otherwise return false.
+
+A string is valid if it follows all of the following rules:
+
+Every left parenthesis '(' must have a corresponding right parenthesis ')'.
+Every right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+A '*' could be treated as a right parenthesis ')' character or a left parenthesis '(' character, or as an empty string "".
+Example 1:
+
+Input: s = "((**)"
+
+Output: true
+Explanation: One of the '*' could be a ')' and the other could be an empty string.
+
+Example 2:
+
+Input: s = "(((*)"
+
+Output: false
+Explanation: The string is not valid because there is an extra '(' at the beginning, regardless of the extra '*'.
+
+Constraints:
+
+1 <= s.length <= 100
+'''
+
+#dp
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        n = len(s)
+        memo= [[None] * (n + 1) for _ in range(n + 1)]
+        def dfs(i, open):
+            if open <0:
+                return False
+            if i == n:
+                return open == 0
+            if memo[i][open] is not None:
+                return memo[i][open]
+            if s[i] == '(':
+                result = dfs(i + 1, open + 1)
+            elif s[i] == ')':
+                result = dfs(i + 1, open - 1)
+            else:
+                result = dfs(i + 1, open + 1) or dfs(i + 1, open - 1) or dfs(i + 1, open)
+            memo[i][open] = result
+            return result
+        return dfs(0, 0)
+#time complexity: O(n^2)
+#space complexity: O(n^2)
