1d661444ba3f8271901b39e51fc806215dd3cfc4
Isaac <zihao599@gmail.com>
2026-09-07 20:16:13 -0700

    two pointers 253

--- Day 221 253 Meeting Rooms II.py
+++ Day 221 253 Meeting Rooms II.py
@@ -39,3 +39,27 @@ class Solution:
         return len(minHeap)
 #time complexity: O(nlogn)
 #space complexity: O(n)
+
+#two pointers
+from typing import List
+class Interval(object):
+    def __init__(self, s = 0, e = 0):
+        self.start = s
+        self.end = e
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+        res = count = 0
+        s = e = 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                count += 1
+                s += 1
+            else:
+                count -= 1
+                e += 1
+            res = max(res, count)
+        return res
+#time complexity: O(nlogn)
+#space complexity: O(n)
