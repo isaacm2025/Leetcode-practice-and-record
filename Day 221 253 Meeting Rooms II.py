4b08418bed4845ca528991a4cf9f601f1ace2e51
Isaac <zihao599@gmail.com>
2026-09-07 20:13:39 -0700

    MinHeap 253

---
+++ Day 221 253 Meeting Rooms II.py
@@ -0,0 +1,41 @@
+'''Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), find the minimum number of rooms required to schedule all meetings without any conflicts.
+
+Note: (0,8),(8,10) is NOT considered a conflict at 8.
+
+Example 1:
+
+Input: intervals = [(0,40),(5,10),(15,20)]
+
+Output: 2
+Explanation:
+room1: (0,40)
+room2: (5,10),(15,20)
+
+Example 2:
+
+Input: intervals = [(4,9)]
+
+Output: 1
+Constraints:
+
+0 <= intervals.length <= 100,000
+0 <= intervals[i].start < intervals[i].end <= 1,000,000'''
+
+#minHeap
+from typing import List
+import heapq
+class Interval(object):
+    def __init__(self, s = 0, e = 0):
+        self.start = s
+        self.end = e
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key = lambda x: x.start)
+        minHeap = []
+        for interval in intervals:
+            if minHeap and minHeap[0] <= interval.start:
+                heapq.heappop(minHeap)
+            heapq.heappush(minHeap, interval.end)
+        return len(minHeap)
+#time complexity: O(nlogn)
+#space complexity: O(n)
