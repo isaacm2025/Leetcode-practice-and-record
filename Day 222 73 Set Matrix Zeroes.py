cea6f208eeda9ceb2344e3744682a8fa4e315dc5
Isaac <zihao599@gmail.com>
2026-09-08 21:19:32 -0700

    iteration 73

--- Day 222 73 Set Matrix Zeroes.py
+++ Day 222 73 Set Matrix Zeroes.py
@@ -58,3 +58,20 @@ class Solution:
                 matrix[r][c] = mark[r][c]
 #time complexity: O(m*n)*(m+n)
 #space complexity: O(m*n)
+
+#iteration
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rows, cols = [False] * ROWS, [False] * COLS
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    rows[r] = True
+                    cols[c] = True
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rows[r] or cols[c]:
+                    matrix[r][c] = 0
+#time complexity: O(m*n)
+#space complexity: O(m+n)
