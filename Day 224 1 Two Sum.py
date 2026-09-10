043ab42ea8732274f4b2ca6413f20f6e583de216
Isaac <zihao599@gmail.com>
2026-09-10 11:42:58 -0700

    sorting 1

--- Day 224 1 Two Sum.py
+++ Day 224 1 Two Sum.py
@@ -41,3 +41,23 @@ class Solution:
         return []
 #time complexity: O(n^2)
 #space complexity: O(1)
+
+#sorting
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        A = []
+        for i, num in enumerate(nums):
+            A.append([num, i])
+        A.sort()
+        i, j = 0, len(nums) - 1
+        while i < j:
+            cur = A[i][0] + A[j][0]
+            if cur == target:
+                return[min(A[i][1], A[j][1]), max(A[i][1], A[j][1])]
+            elif cur < target:
+                i += 1
+            else:
+                j -= 1
+        return []
+#time complexity: O(nlogn)
+#space complexity: O(n)
