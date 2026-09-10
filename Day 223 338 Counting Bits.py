f5a74282265fc6c9c38bdb92d62e07a299ea8031
Isaac <zihao599@gmail.com>
2026-09-09 22:28:42 -0700

    optimal 338

--- Day 223 338 Counting Bits.py
+++ Day 223 338 Counting Bits.py
@@ -32,3 +32,13 @@ class Solution:
         return res
 #time complexity: O(n)
 #space complexity: O(n)
+
+#optimal
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
+#time complexity: O(n)
+#space complexity: O(n)
