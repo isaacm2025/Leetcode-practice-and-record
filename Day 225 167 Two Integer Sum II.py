60c239f17bdfe56dfb353cc6cc1363d274525e53
Isaac <zihao599@gmail.com>
2026-09-11 23:26:09 -0700

    BS 167

--- Day 225 167 Two Integer Sum II.py
+++ Day 225 167 Two Integer Sum II.py
@@ -37,3 +37,21 @@ class Solution:
         return []
 #time complexity: O(n^2)
 #space complexity: O(1)
+
+#BS
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            l, r = i + 1, len(numbers) - 1
+            tmp = target - numbers[i]
+            while l <= r:
+                mid = l + (r - l) // 2
+                if numbers[mid] == tmp:
+                    return [i + 1, mid + 1]
+                elif numbers[mid] < tmp:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return []
+#time complexity: O(nlogn)
+#space complexity: O(1)
