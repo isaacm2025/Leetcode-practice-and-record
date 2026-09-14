45d023cbafa6bd8ca4927e47c3ed53f32fa2609b
Isaac <zihao599@gmail.com>
2026-09-13 18:02:29 -0700

    dp 239

--- Day 227 239 Sliding Window Maximum.py
+++ Day 227 239 Sliding Window Maximum.py
@@ -52,3 +52,28 @@ class Solution:
         return output
 #time complexity: O(nlogk) where n is the length of nums and k is the size of the sliding window
 #space complexity: O(k) where k is the size of the sliding window
+
+#dp
+from typing import List
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        leftMax = [0] * n
+        rightMax = [0] * n
+        leftMax[0] = nums[0]
+        rightMax[n - 1] = nums[n - 1]
+        for i in range(1, n):
+            if i % k == 0:
+                leftMax[i] = nums[i]
+            else:
+                leftMax[i] = max(leftMax[i - 1], nums[i])
+            if (n - 1 - i) % k == 0:
+                rightMax[n - 1 - i] = nums[n - 1 - i]
+            else:
+                rightMax[n - 1 - i] = max(rightMax[n - i], nums[n - 1 - i])
+        output = [0] * (n - k + 1)
+        for i in range(n - k + 1):
+            output[i] = max(rightMax[i], leftMax[i + k -1])
+        return output
+#time complexity: O(n) where n is the length of nums
+#space complexity: O(n) where n is the length of nums
