9fc506cbd96b968490ff0c29a51e84b07633bcd8
Isaac <zihao599@gmail.com>
2026-09-14 12:09:45 -0700

    bs 875

--- Day 228 875 Koko Eating Bananas.py
+++ Day 228 875 Koko Eating Bananas.py
@@ -37,3 +37,22 @@ class Solution:
         return speed
 #time complexity: O(n * m) where n is the number of piles and m is the maximum number of bananas in a pile
 #space complexity: O(1)
+
+#bs
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+        while l <= r:
+            k = (l + r) // 2
+            totalTime = 0
+            for p in piles:
+                totalTime += (p + k - 1) // k
+            if totalTime <= h:
+                res = k
+                r = k - 1
+            else: 
+                l = k + 1
+        return res
+#time complexity: O(n * logm) where n is the number of piles and m is the maximum number of bananas in a pile
+#space complexity: O(1)
