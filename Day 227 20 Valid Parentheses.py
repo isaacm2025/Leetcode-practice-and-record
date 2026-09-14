9893e5c5e16058d292d7eb0f06c46888e416805c
Isaac <zihao599@gmail.com>
2026-09-13 18:08:59 -0700

    stack 20

--- Day 227 20 Valid Parentheses.py
+++ Day 227 20 Valid Parentheses.py
@@ -39,3 +39,20 @@ class Solution:
         return s == ''
 #time complexity: O(n^2) where n is the length of string s
 #space complexity: O(n) where n is the length of string s, because we are creating a new string in each iteration of the while loop
+
+#stack
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {")": "(", "}": "{", "]": "["}
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+#time complexity: O(n) where n is the length of string s
+#space complexity: O(n) where n is the length of string s, because we are using a stack to store the open brackets
