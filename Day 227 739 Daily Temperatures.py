9d2a86e586e8a22ca32eb66c6b914b409d6a8f98
Isaac <zihao599@gmail.com>
2026-09-13 18:38:40 -0700

    stack 739

--- Day 227 739 Daily Temperatures.py
+++ Day 227 739 Daily Temperatures.py
@@ -36,3 +36,17 @@ class Solution:
         return res
 #time complexity: O(n^2)
 #space complexity: O(n)
+
+#stack
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res
+#time complexity: O(n)
+#space complexity: O(n)
