201e1485ef5331ce94a01bae06e584f8eee572e2
Isaac <zihao599@gmail.com>
2026-09-15 21:06:40 -0700

    two pointer 141

--- Day 229 141 Linked List Cycle Detection.py
+++ Day 229 141 Linked List Cycle Detection.py
@@ -46,3 +46,20 @@ class Solution:
         return False
 #time complexity: O(n) where n is the length of the linked list
 #space complexity: O(n) where n is the length of the linked list
+
+#two pointer
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+#time complexity: O(n) where n is the length of the linked list
+#space complexity: O(1)
