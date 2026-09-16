cc875c393904b0a18f8ece8fd26c2b726cb3f576
Isaac <zihao599@gmail.com>
2026-09-16 13:11:54 -0700

    built in 146

--- Day 230 146 LRU Cache.py
+++ Day 230 146 LRU Cache.py
@@ -59,3 +59,26 @@ class LRUCache:
         self.cache.append([key, value]) 
 #time complexity: O(n) for get and put, where n is the number of items in the cache. This is because we may need to search through the list to find the key.
 #space complexity: O(n) for the cache, where n is the number of items in the cache. This is because we are storing the key-value pairs in a list.
+
+#built-in OrderedDict
+from collections import OrderedDict
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+#time complexity: O(1) for get and put, where n is the number of items in the cache. This is because we are using an OrderedDict which allows for O(1) access and insertion.
+#space complexity: O(n) for the cache, where n is the number of items in the cache. This is because we are storing the key-value pairs in an OrderedDict.
