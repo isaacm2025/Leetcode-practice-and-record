70be2eaf8b2018b23b37ca4c73844f210e76cb03
Isaac <zihao599@gmail.com>
2026-09-15 20:51:09 -0700

    iteration 206

--- Day 229 206 Reverse Linked List.py
+++ Day 229 206 Reverse Linked List.py
@@ -33,3 +33,21 @@ class Solution:
         return newHead
 #time complexity: O(n) where n is the number of nodes in the linked list
 #space complexity: O(n) where n is the number of nodes in the linked list due to the recursion stack
+
+#iteration
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+#time complexity: O(n) where n is the number of nodes in the linked list
+#space complexity: O(1)
