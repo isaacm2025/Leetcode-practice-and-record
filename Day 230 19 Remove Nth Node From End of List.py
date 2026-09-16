23f2cbfec9c502c72d429900f9bfeedb96cacca3
Isaac <zihao599@gmail.com>
2026-09-16 11:53:57 -0700

    iteration 19

--- Day 230 19 Remove Nth Node From End of List.py
+++ Day 230 19 Remove Nth Node From End of List.py
@@ -41,3 +41,28 @@ class Solution:
         return head
 #time complexity: O(n)
 #space complexity: O(n)
+
+#iteration
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        N = 0
+        cur = head
+        while cur:
+            N += 1
+            cur = cur.next
+        removeIndex = N - n
+        if removeIndex == 0:
+            return head.next
+        cur = head
+        for i in range(N - 1):
+            if (i + 1) == removeIndex:
+                cur.next = cur.next.next
+                break
+            cur = cur.next
+        return head
+#time complexity: O(n)
+#space complexity: O(1)
