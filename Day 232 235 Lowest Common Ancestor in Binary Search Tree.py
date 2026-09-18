de3808f2926ca88aaa4e8a058b2da2eaf71306f1
Isaac <zihao599@gmail.com>
2026-09-18 10:03:39 -0700

    iteration 235

--- Day 232 235 Lowest Common Ancestor in Binary Search Tree.py
+++ Day 232 235 Lowest Common Ancestor in Binary Search Tree.py
@@ -46,3 +46,17 @@ class Solution:
             return root
 #time complexity: O(n)
 #space complexity: O(n)
+
+#iteration
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        cur = root
+        while cur:
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            else:
+                return cur
+#time complexity: O(n)
+#space complexity: O(1)
