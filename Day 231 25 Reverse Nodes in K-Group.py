db44abbfce8ade4e402ae9bda0bea2d8cd78cefd
Isaac <zihao599@gmail.com>
2026-09-17 21:39:15 -0700

    iteration 25

--- Day 231 25 Reverse Nodes in K-Group.py
+++ Day 231 25 Reverse Nodes in K-Group.py
@@ -50,3 +50,35 @@ class solution:
         return head
 #time complexity: O(n) where n is the number of nodes in the linked list
 #space complexity: O(n/k) where n is the number of nodes in the linked list
+
+#iteration
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+        while True:
+            kth = self.getKthNode(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+    def getKthNode(self, curr: ListNode, k: int) -> ListNode:
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
+#time complexity: O(n) where n is the number of nodes in the linked list
+#space complexity: O(1)
