6e3a6c5acb12232aa8d08b9345da47f3f99cd78f
Isaac <zihao599@gmail.com>
2026-09-19 22:40:19 -0700

    105 hashmap dfs

---
+++ Day 233 105 Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,50 @@
+'''You are given two integer arrays preorder and inorder.
+
+preorder is the preorder traversal of a binary tree
+inorder is the inorder traversal of the same tree
+Both arrays are of the same size and consist of unique values.
+Rebuild the binary tree from the preorder and inorder traversals and return its root.
+
+Example 1:
+
+
+
+Input: preorder = [1,2,3,4], inorder = [2,1,3,4]
+
+Output: [1,2,3,null,null,null,4]
+Example 2:
+
+Input: preorder = [1], inorder = [1]
+
+Output: [1]
+Constraints:
+
+1 <= inorder.length <= 2001.
+inorder.length == preorder.length
+-1000 <= preorder[i], inorder[i] <= 1000'''
+
+#hashmap + dfs
+from typing import List
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        indices = {val: idx for idx, val in enumerate(inorder)}
+        self.pre_idx = 0
+        def dfs(l, r):
+            if l > r:
+                return None
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1
+            root = TreeNode(root_val)
+            mid = indices[root_val]
+            root.left = dfs(l, mid - 1)
+            root.right = dfs(mid + 1, r)
+            return root
+        return dfs(0, len(inorder) - 1)
+#time complexity: O(n)
+#space complexity: O(n)
+                             
