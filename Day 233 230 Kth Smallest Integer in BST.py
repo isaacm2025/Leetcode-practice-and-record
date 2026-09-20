7a2490d0d9c11e71013b3950d9df0efd9ee0309d
Isaac <zihao599@gmail.com>
2026-09-19 22:35:16 -0700

    iterative dfs 230

--- Day 233 230 Kth Smallest Integer in BST.py
+++ Day 233 230 Kth Smallest Integer in BST.py
@@ -67,3 +67,25 @@ class Solution:
         return arr[k - 1]
 #time complexity: O(n)
 #space complexity: O(n)
+
+#iterative dfs
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        stack = []
+        curr = root
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            curr = curr.right
+#time complexity: O(n)
+#space complexity: O(n)
