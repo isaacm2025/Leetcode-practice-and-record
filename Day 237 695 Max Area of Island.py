ca511026705d3276fd854712ae65a8ffc06293c4
Isaac <zihao599@gmail.com>
2026-09-23 10:24:35 -0700

    bfs 695

--- Day 237 695 Max Area of Island.py
+++ Day 237 695 Max Area of Island.py
@@ -43,3 +43,33 @@ class Solution:
         return area
 #time complexity: O(M*N) where M is the number of rows and N is the number of columns in the grid. Each cell is visited once.
 #space complexity: O(M*N) in the worst case where the grid is filled with land, and the recursion stack will go as deep as M*N.
+
+#bfs
+from collections import deque
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        area = 0
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = 0
+            q.append((r, c))
+            res = 1
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + row, dc + col
+                    if (nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS or grid[nr][nc] == 0):
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = 0
+                    res += 1
+            return res
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    area = max(area, bfs(r, c))
+        return area
+#time complexity: O(M*N) where M is the number of rows and N is the number of columns in the grid. Each cell is visited once.
+#space complexity: O(min(M, N)) where M is the number of rows and N is the number of columns in the grid. This is because in the worst case, the queue will contain all the cells in the grid, but the maximum size of the queue will be limited by the smaller of the two dimensions.
