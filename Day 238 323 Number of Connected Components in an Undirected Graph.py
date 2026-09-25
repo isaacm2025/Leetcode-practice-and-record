d5599c9954883b20221155b2a87fee94eb176570
Isaac <zihao599@gmail.com>
2026-09-24 21:47:22 -0700

    dfs 323

---
+++ Day 238 323 Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,57 @@
+'''You have an undirected graph of n nodes labeled from 0 to n - 1. You are given an integer n and an array edges where edges[i] = [aᵢ, bᵢ] indicates that there is an edge between aᵢ and bᵢ in the graph.
+
+Return the number of connected components in the graph.
+
+
+Example 1:
+
+
+
+Input:
+n = 5, edges = [[0,1],[1,2],[3,4]]
+
+Output: 2
+
+Example 2:
+
+
+
+Input:
+n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
+
+Output: 1
+
+Constraints:
+
+1 <= n <= 2000
+1 <= edges.length <= 5000
+edges[i].length == 2
+0 <= aᵢ < n
+0 <= bᵢ < n
+aᵢ != bᵢ
+There are no repeated edges.
+'''
+
+#dfs
+from typing import List
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        visit = [False] * n
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+        def dfs(node):
+            for nei in adj[node]:
+                if not visit[nei]:
+                    visit[nei] = True
+                    dfs(nei)
+        res = 0
+        for i in range(n):
+            if not visit[i]:
+                visit[i] = True
+                dfs(i)
+                res += 1
+        return res
+#time complexity: O(n + e), where n is the number of nodes and e is the number of edges
+#space complexity: O(n + e), for the adjacency list and the visited array
